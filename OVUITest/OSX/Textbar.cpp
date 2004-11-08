e1bf246ed56c8acf3ea95ed751197406806ee20e
gugod <gugod@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-11-08 01:12:49 +0000

    r9063@ib:  gugod | 2004-11-07T23:35:21.738793Z  * fontsize on control aren't set to give size ?  * try to specify font at the same time.
     r9064@ib:  gugod | 2004-11-07T23:41:51.868243Z
     this +fonsize is useless.
     
     r9065@ib:  gugod | 2004-11-08T00:59:37.442698Z
     RegionCode.
     
     r9066@ib:  gugod | 2004-11-08T01:03:30.100400Z
     . 
     
     r9067@ib:  gugod | 2004-11-08T01:03:40.497630Z
     test cases.
     
     r9068@ib:  gugod | 2004-11-08T01:10:06.192829Z
     Move the test strings into a sepearte file, not required to compile
     everytime when I change the string.
     
     r9069@ib:  gugod | 2004-11-08T01:11:41.452338Z
     Revert all the changes. So hard to tweak it.

--- OVUITest/OSX/Textbar.cpp
+++ OVUITest/OSX/Textbar.cpp
@@ -6,41 +6,14 @@
 #include <string.h>
 #include <unistd.h>
 
-char *str[] = 
-{
-    "Quick",
-    "Quick Fox",
-    "Quick Fox Jump",
-    "Quick Fox Jump Over",
-    "Quick Fox Jump Over Lazy",
-    "Quick Fox Jump Over Lazy Sleeping",
-    "Quick Fox Jump Over Lazy Sleeping Rabbit",
-
-    "一",
-    "一二",
-    "一二三",
-    "一二三四",
-    "一二三四五",
-
-    "a. 一",
-    "a. 一 b. 二",
-    "a. 一 b. 二 c. 三",
-    "a. 一 b. 二 c. 三 d. 四",
-    "a. 一 b. 二 c. 三 d. 四 e. 五",
-    "a. 一 b. 二 c. 三 d. 四 e. 五 f. 六",
-    "a. 一 b. 二 c. 三 d. 四 e. 五 f. 六 g. 七",
-    "a. 一 b. 二 c. 三 d. 四 e. 五 f. 六 g. 七 h. 八",
-
-    "\0",
-};
-
 int main(void) {
     VXTextBar bar;
-    for(int t = 0; str[t][0] != 0 ; t++) {
-	murmur("%s",str[t]);
+    char line[1024];
+    while(fgets(line,1023,stdin)) {
+	murmur("%s",line);
 	bar.hide();
         bar.clear();
-	bar.append((char*)str[t],ovEncodingUTF8);
+	bar.append((char*)line,ovEncodingUTF8);
 	bar.update();
 	bar.show();
 	murmur("----------------");
