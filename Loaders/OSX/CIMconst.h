abf89a2f756199b7c30879ecc96339daaa3171a7
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-11-29 06:56:44 +0000

    r194@DHCP-22153:  lukhnos | 2004-11-29T06:40:34.642722Z  Added /Loaders and /Loaders/OSX

---
+++ Loaders/OSX/CIMconst.h
@@ -0,0 +1,15 @@
+
+#ifndef __CIMConst_h
+#define __CIMConst_h
+
+#define cimVersion  0x00010000
+#define cimScript   2		// smTradChinese
+#define cimLanguage	19		// langTradChinese
+// #define cimBaseResourceID	(15872+cimScript*512)
+// #define cimBaseResourceID 16384
+#define cimBaseResourceID	16896
+
+
+#define cimVendorCode 'lkns'		// used in CIM.r
+
+#endif  // of #ifndef __CIMConst_h
