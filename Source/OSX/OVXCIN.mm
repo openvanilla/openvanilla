7939e2dc9c11b17a639414abc540be832bac8fcf
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-12-01 17:00:52 +0000

    r381@b6s:  b6s | 2004-12-01T17:00:42.584335Z  TODO: implement a getline() for char[]

--- Source/OSX/OVXCIN.mm
+++ Source/OSX/OVXCIN.mm
@@ -5,6 +5,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -23,27 +24,23 @@ vector<string> OVXCIN::find(string key)
 
 void OVXCIN::read(char *fname, int shiftselkey)
 {
-	ifstream in(fname, ios_base::binary);
+	ifstream in(fname);
 	
 	in.seekg(0, ios_base::end);
 	int length = in.tellg();
 	in.seekg(0);
-
-	cout<<length<<endl;
 	
 	char buffer[length];
 	in.get(buffer, length);
-	
-	ofstream out("test.txt", ios_base::binary);
-	out.write(buffer, length);
-	
-	out.close();	
+
+	// <TODO>getline() for buffer</TODO>
+
 	in.close();
 }
 
 int main(int argc, char**argv)
 {        
     OVXCIN cin;
-    cin.read(argv[0]);    
+    cin.read(argv[1]);    
 }
 
