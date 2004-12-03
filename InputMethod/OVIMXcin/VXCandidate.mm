0575c4414239f2e3e1e8fb8f9d96ba7fe4b4f7f5
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-12-03 18:34:14 +0000

    r328@dove:  lukhnos | 2004-12-03T18:33:44.268957Z  Renamed XcinCandidate to VXCandidate, then separated from OVIMXcin.mm

---
+++ InputMethod/OVIMXcin/VXCandidate.mm
@@ -0,0 +1,71 @@
+// VXCandidate.cpp
+
+#include <OpenVanilla/OpenVanilla.h>
+#include <OpenVanilla/OVUtility.h>
+#include "VXCandidate.h"
+
+void VXCandidate::prepare(NSArray *l, char *skey, OVTextBar *textbar)
+{
+    onduty=1;
+    list=l;
+    strcpy(selkey, skey);
+    perpage=strlen(selkey);
+    pos=0;
+    count=[list count];
+    
+    murmur ("prepare, selkey=%s, perpage=%d, pos=%d, count=%d",
+        selkey, perpage, pos, count);
+    
+    update(textbar);
+    textbar->show();
+}
+
+void VXCandidate::update(OVTextBar *textbar)
+{
+    char buf[256];
+    int bound=pos+perpage;
+    if (bound > count) bound=count;
+    
+    textbar->clear();
+    
+    for (int i=pos, j=0; i<bound; i++, j++)
+    {
+        sprintf (buf, "%c.", selkey[j]);
+        textbar->append(buf)->append((void*)[[list objectAtIndex: i] UTF8String])->
+            append((void*)" ");
+    }
+    
+    int totalpage=(count/perpage)+1;
+    int currentpage=(pos/perpage)+1;
+    sprintf (buf, "(%d/%d)", currentpage, totalpage);
+    textbar->append(buf);
+    textbar->update();   
+}
+
+VXCandidate* VXCandidate::pageUp()
+{
+    pos-=perpage;
+    if (pos < 0) pos=0;
+    return this;
+}
+
+VXCandidate* VXCandidate::pageDown()
+{
+    int oldpos=pos;
+    pos+=perpage;  
+    if (pos >= count) pos=0;
+    return this;
+}
+
+NSString* VXCandidate::select(char c)
+{
+    int i;
+    for (i=0; i<perpage; i++)
+        if (selkey[i]==c)
+        {
+            onduty=0;
+            return [list objectAtIndex: pos+i];
+        }
+        
+    return NULL;        
+}
