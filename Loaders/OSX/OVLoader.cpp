c216ef1a3d20b48bad6ccd22fc5ad08b121471e5
gugod <gugod@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-12-03 07:59:54 +0000

    r9734@ib:  gugod | 2004-12-03T07:59:25.753312Z  Big Tweak: indentation.

--- Loaders/OSX/OVLoader.cpp
+++ Loaders/OSX/OVLoader.cpp
@@ -20,185 +20,163 @@ class CIMContext
 {
 public:
     CIMContext() : ovcontext(NULL), onScreen(0) { add(); }
-	~CIMContext() { remove(); }
+    ~CIMContext() { remove(); }
     VXTextBar bar;
     OVIMContext *ovcontext;
     int onScreen;
-	
-	void add();
-	void remove();
+    void add();
+    void remove();
 };
 
-int listloaded=0;
-
-const int vxMaxContext=256;
-CIMContext* pool[vxMaxContext];
-
-void CIMContext::add()
-	{ for (int c=0; c<vxMaxContext; c++) if (!pool[c]) { pool[c]=this; break; } }
-void CIMContext::remove()
-	{ for (int c=0; c<vxMaxContext; c++) if (pool[c]==this) pool[c]=NULL; }
-
+const int vxMaxContext = 256;
+const char *plistfile  = "/Library/OpenVanilla/Development/OVXLoader.plist";
+const char *loaddir    = "/Library/OpenVanilla/Development/";
 
 int floatingwindowlock=0, defposx, defposy, textsize=24;
-OVService srv;
+int listloaded=0;
+UInt32 usermenu='USRM';
+
 VXConfig *sysconfig=NULL;
 VXLibraryList list;
-UInt32 usermenu='USRM';
-const char *plistfile="/Library/OpenVanilla/Development/OVXLoader.plist";
-const char *loaddir="/Library/OpenVanilla/Development/";
+
+OVService srv;
 OVInputMethod *inputmethod;
+
+CIMContext* pool[vxMaxContext];
+
+void CIMContext::add() {
+    for (int c=0; c<vxMaxContext; c++) if (!pool[c]) { pool[c]=this; break; }
+}
+
+void CIMContext::remove() {
+    for (int c=0; c<vxMaxContext; c++) if (pool[c]==this) pool[c]=NULL;
+}
 
 OVDictionary *GetGlobalConfig()
 {
-	OVDictionary *d=sysconfig->getDictionaryRoot();
-	
-	if (!d->keyExist("OVLoader")) d->newDictionary("OVLoader");
-	return d->getDictionary("OVLoader");
+    OVDictionary *d=sysconfig->getDictionaryRoot();
+
+    if (!d->keyExist("OVLoader")) d->newDictionary("OVLoader");
+    return d->getDictionary("OVLoader");
 }
 
 OVDictionary *GetLocalConfig(char *id)
 {
-	OVDictionary *d=sysconfig->getDictionaryRoot();
+    OVDictionary *d=sysconfig->getDictionaryRoot();
+    char buf[256];
 
-	char buf[256];
-	strcpy (buf, "IM-");
-	strcat (buf, id);
+    strcpy (buf, "IM-"); strcat (buf, id);
 
-	if (!d->keyExist(buf)) d->newDictionary(buf);
-	return d->getDictionary(buf);
+    if (!d->keyExist(buf)) d->newDictionary(buf);
+    return d->getDictionary(buf);
 }
 
 
 int CIMCustomInitialize(MenuRef mnu)
 {
-	murmur ("OVLoader: initializing");
+    murmur ("OVLoader: initializing");
 	
-	for (int c=0; c<vxMaxContext; c++) pool[c]=NULL;
+    for (int c=0; c<vxMaxContext; c++) pool[c]=NULL;
+    
+    sysconfig=new VXConfig(plistfile);
+    OVDictionary *global=GetGlobalConfig();
+    
+    // load every .dylib; this is to prevent a strange "load twice" bug in iTerm
+    if (!listloaded) {
+        list.addglob((char*)loaddir,(char*)".dylib");
+        listloaded=1;
+    }
 	
-	sysconfig=new VXConfig(plistfile);
-	OVDictionary *global=GetGlobalConfig();
+    char locale[256];
+    VXGetCurrentLocale(CFBundleGetBundleWithIdentifier(CFSTR(cimBundleName)),
+                       locale);
+    
+    // for the time being, we use id as menu names
+    // (and we initialize everything)
+    int i=0;
+    for (i=0; i<list.imcntr; i++) {
+        OVEncoding enc;
+        char imn[256];
+        list.impair[i].im->name(locale, imn, &enc);
+        CFStringRef imname=VXCreateCFString(imn, enc);
+        InsertMenuItemTextWithCFString(mnu, imname, i, 0, usermenu+i);
+        SetMenuItemCommandKey(mnu, i+1, FALSE, '1'+i);
+        SetMenuItemModifiers(mnu, i+1, kMenuControlModifier);
+        CFRelease(imname);
+    }
+	
+    InsertMenuItemTextWithCFString(mnu, CFSTR("-"), i++, 0, 0);
 
-
-	// load every .dylib; this is to prevent a strange "load twice" bug in iTerm
-	if (!listloaded)
-	{
-		list.addglob((char*)loaddir,(char*)".dylib");
-		listloaded=1;
-	}
-	
-	
-	char locale[256];
-	VXGetCurrentLocale(CFBundleGetBundleWithIdentifier(CFSTR(cimBundleName)), locale);
-	
-	// for the time being, we use id as menu names
-	// (and we initialize everything)
-	int i=0;
-	for (i=0; i<list.imcntr; i++)
-	{
-		OVEncoding enc;
-		char imn[256];
-		list.impair[i].im->name(locale, imn, &enc);
-		CFStringRef imname=VXCreateCFString(imn, enc);
-		InsertMenuItemTextWithCFString(mnu, imname, i, 0, usermenu+i);
-		
-		SetMenuItemCommandKey(mnu, i+1, FALSE, '1'+i);
-		SetMenuItemModifiers(mnu, i+1, kMenuControlModifier);
-		
-		CFRelease(imname);
-
-//		char id[256];
-//		list.impair[i].im->identifier(id);
-//		OVDictionary *local=GetLocalConfig(id);
-//		list.impair[i].im->initialize(global, local, &srv, (char*)loaddir);
-//		delete local;
-	}
-	
-	InsertMenuItemTextWithCFString(mnu, CFSTR("-"), i++, 0, 0);
-
-	CFBundleRef bundle=CFBundleGetBundleWithIdentifier(CFSTR(cimBundleName));
+    CFBundleRef bundle=CFBundleGetBundleWithIdentifier(CFSTR(cimBundleName));
     CFStringRef mstr;
-	mstr=CFBundleCopyLocalizedString(bundle, CFSTR("Preferences"), NULL, NULL);
-	InsertMenuItemTextWithCFString(mnu, mstr, i++, 0, 'PREF');
-	CFRelease(mstr);
-	mstr=CFBundleCopyLocalizedString(bundle, CFSTR("About"), NULL, NULL);
-	InsertMenuItemTextWithCFString(mnu, mstr, i++, 0, 'ABUT');
-	CFRelease(mstr);
+    mstr=CFBundleCopyLocalizedString(bundle, CFSTR("Preferences"), NULL, NULL);
+    InsertMenuItemTextWithCFString(mnu, mstr, i++, 0, 'PREF');
+    CFRelease(mstr);
+    mstr=CFBundleCopyLocalizedString(bundle, CFSTR("About"), NULL, NULL);
+    InsertMenuItemTextWithCFString(mnu, mstr, i++, 0, 'ABUT');
+    CFRelease(mstr);
 	
+    if (!global->keyExist("floatingWindowLock")) {
+        global->setInt("floatingWindowLock", 0);
+        global->setInt("floatingWindowLockPosX", 20);
+        global->setInt("floatingWindowLockPosY", 760);
+    }
+    
+    if (!global->keyExist("textSize")) global->setInt("textSize", 24);
+    textsize=global->getInt("textSize");
+    floatingwindowlock=global->getInt("floatingWindowLock");
+    defposx=global->getInt("floatingWindowLockPosX");
+    defposy=global->getInt("floatingWindowLockPosY");	
+    
+    // look for the input method we want to use
+    char currentim[256];
+    if (global->keyExist("currentIM")) {
+        global->getString("currentIM", currentim);
+    }
 	
-	// get floating window settings
-	if (!global->keyExist("floatingWindowLock")) 
-	{
-		global->setInt("floatingWindowLock", 0);
-		global->setInt("floatingWindowLockPosX", 20);
-		global->setInt("floatingWindowLockPosY", 760);
-	}
+    int pos=list.findPos(currentim);
+    
+    if (pos==-1) pos=0;
+    inputmethod=list.impair[pos].im;
+    
+    if (inputmethod) {
+        OVDictionary *local=GetLocalConfig(currentim);
+        inputmethod->initialize(global, local, &srv, (char*)loaddir);
+        list.impair[pos].inited=1;
+        delete local;
+        
+        inputmethod->identifier(currentim);
+        global->setString("currentIM", currentim);
 	
-	if (!global->keyExist("textSize")) global->setInt("textSize", 24);
-	textsize=global->getInt("textSize");
+        int pos=list.findPos(currentim);
+        SetItemMark(mnu, pos+1, checkMark);
+    }
 	
-	floatingwindowlock=global->getInt("floatingWindowLock");
-	defposx=global->getInt("floatingWindowLockPosX");
-	defposy=global->getInt("floatingWindowLockPosY");	
-	
-	// look for the input method we want to use
-	char currentim[256];
-	if (global->keyExist("currentIM"))
-	{
-		global->getString("currentIM", currentim);
-	}
-	
-	int pos=list.findPos(currentim);
-
-	if (pos==-1) pos=0;
-	inputmethod=list.impair[pos].im;
-	
-	if (inputmethod)
-	{
-		OVDictionary *local=GetLocalConfig(currentim);
-		inputmethod->initialize(global, local, &srv, (char*)loaddir);
-		list.impair[pos].inited=1;
-		delete local;
-
-		inputmethod->identifier(currentim);
-		global->setString("currentIM", currentim);
-		
-		int pos=list.findPos(currentim);
-		SetItemMark(mnu, pos+1, checkMark);
-	}
-	
-	sysconfig->write();	
-	delete global;
+    sysconfig->write();	
+    delete global;
     return 1;
 }
 
 void CIMCustomTerminate()
 {    
     fprintf (stderr, "OVXSimpleLoader: Terminating\n");
-    if (inputmethod)
-	{
-/*		OVDictionary *global=GetGlobalConfig();
-		OVDictionary *local=GetLocalConfig(imid);
-		inputmethod->terminate(global, local, &srv); 
-		sysconfig->write();
-		delete global;
-		delete local;		 */
-	}
-	
-	delete sysconfig;
+    if (inputmethod) {
+        // XXX: Not implemented: Free each element in XVLibraryList List
+    }
+    
+    delete sysconfig;
 }
 
 void *CIMCustomOpen()
 {
     CIMContext *c=new CIMContext;
 
-	c->bar.setFontSize(textsize);
-	if (floatingwindowlock) 
-	{
-		c->bar.unlock();
-		c->bar.setPosition(defposx, defposy);
-		c->bar.lock(); 
-	}
+    c->bar.setFontSize(textsize);
+    if (floatingwindowlock) {
+        c->bar.unlock();
+        c->bar.setPosition(defposx, defposy);
+        c->bar.lock(); 
+    }
 
     if (inputmethod) c->ovcontext=inputmethod->newContext();
     return c;
@@ -218,63 +196,54 @@ int CIMCustomActivate(void *data, CIMInputBuffer *buf)
     CIMContext *c=(CIMContext*)data;    
     if (!c->ovcontext) return 0;
 
-	if (sysconfig->changed())
-	{
-		sysconfig->read();
+    if (sysconfig->changed()) {
+        sysconfig->read();
 	
-		char buf[256];
-		inputmethod->identifier(buf);
-		OVDictionary *global=GetGlobalConfig();
+        char buf[256];
+        inputmethod->identifier(buf);
+        OVDictionary *global=GetGlobalConfig();
 	
-		c->bar.setFontSize(global->getInt("textSize"));
+        c->bar.setFontSize(global->getInt("textSize"));
 		
-		floatingwindowlock=global->getInt("floatingWindowLock");
-		defposx=global->getInt("floatingWindowLockPosX");
-		defposy=global->getInt("floatingWindowLockPosY");
-
-		if (floatingwindowlock) 
-		{
-			c->bar.unlock();
-			c->bar.setPosition(defposx, defposy);
-			c->bar.lock(); 
-		}
-		else c->bar.unlock();
-		
-		OVDictionary *local=GetLocalConfig(buf);
-		inputmethod->update(global, local);
-		c->onScreen=0;
-		c->ovcontext->clear();
-		delete global;
-		delete local;
-	}
-
-
-    if (c->onScreen)
-    {
-        c->onScreen=0;
+        floatingwindowlock=global->getInt("floatingWindowLock");
+        defposx=global->getInt("floatingWindowLockPosX");
+        defposy=global->getInt("floatingWindowLockPosY");
         
-        if (buf->length()) 
-        {
+        if (floatingwindowlock) {
+            c->bar.unlock();
+            c->bar.setPosition(defposx, defposy);
+            c->bar.lock(); 
+        } else {
+            c->bar.unlock();
+        }
+        OVDictionary *local=GetLocalConfig(buf);
+        inputmethod->update(global, local);
+        c->onScreen=0;
+        c->ovcontext->clear();
+        delete global;
+        delete local;
+    }
+
+    if (c->onScreen) {
+        c->onScreen=0;
+        if (buf->length()) {
             c->bar.show();
             c->onScreen=0;
-        }
-        else
-        {
+        } else {
             c->bar.hide()->clear();
             c->ovcontext->clear();    
         }
     }    
 
-	if (floatingwindowlock) 
-	{
-		c->bar.clear();
-		c->bar.show();
-		c->bar.lock();
-		c->bar.append((char*)"(選字窗)");
-		c->bar.update();
-	}
-	
-	c->ovcontext->activate(&srv);
+    if (floatingwindowlock) {
+        c->bar.clear();
+        c->bar.show();
+        c->bar.lock();
+        c->bar.append((char*)"(選字窗)");
+        c->bar.update();
+    }
+    
+    c->ovcontext->activate(&srv);
     return 1;
 }
 
@@ -283,36 +252,30 @@ int CIMCustomDeactivate(void *data, CIMInputBuffer *buf)
     CIMContext *c=(CIMContext*)data;    
     if (!c->ovcontext) return 0;
 
-    if (c->bar.onScreen())
-    {
+    if (c->bar.onScreen()) {
         c->onScreen=1;
-		if (floatingwindowlock)
-		{
-			c->bar.unlock();
-			c->bar.hide();		
-		}
+        if (floatingwindowlock) {
+            c->bar.unlock();
+            c->bar.hide();		
+        }
     }
 
-	if (floatingwindowlock)
-	{
-		int newx, newy;
-		c->bar.getPosition(&newx, &newy);
-		if ((newx != defposx) || (newy != defposy))
-		{
-			murmur("old: (%d,%d), new: (%d,%d)", defposx, defposy, newx, newy);
-			OVDictionary *global=GetGlobalConfig();
-			defposx=newx;
-			defposy=newy;
-			global->setInt("floatingWindowLock", floatingwindowlock);
-			global->setInt("floatingWindowLockPosX", defposx);
-			global->setInt("floatingWindowLockPosY", defposy);
-			sysconfig->write();
-		}
-	}
+    if (floatingwindowlock) {
+        int newx, newy;
+        c->bar.getPosition(&newx, &newy);
+        if ((newx != defposx) || (newy != defposy)) {
+            murmur("old: (%d,%d), new: (%d,%d)", defposx, defposy, newx, newy);
+            OVDictionary *global=GetGlobalConfig();
+            defposx=newx;
+            defposy=newy;
+            global->setInt("floatingWindowLock", floatingwindowlock);
+            global->setInt("floatingWindowLockPosX", defposx);
+            global->setInt("floatingWindowLockPosY", defposy);
+            sysconfig->write();
+        }
+    }
 
-
-	c->ovcontext->deactivate(&srv);
-
+    c->ovcontext->deactivate(&srv);
     return 1;
 }
 
@@ -322,38 +285,37 @@ int CIMCustomHandleInput(void *data, CIMInputBuffer *buf, unsigned char charcode
     CIMContext *c=(CIMContext*)data;    
     if (!c->ovcontext) return 0;
 
-	VXBuffer vxb;
-	VXKeyCode key;
+    VXBuffer vxb;
+    VXKeyCode key;
 
-	vxb.bind(buf);
-	key.set(charcode, keycode, modifiers);
+    vxb.bind(buf);
+    key.set(charcode, keycode, modifiers);
 
-    fprintf (stderr, "received keycode=%d, buffer position=(%d,%d)\n",
-        charcode, pnt->h, pnt->v);
+    murmur("received keycode=%d, buffer position=(%d,%d)\n",
+           charcode, pnt->h, pnt->v);
         
     c->bar.setPosition(pnt->h, pnt->v);
-	return c->ovcontext->keyEvent(&key, &vxb, &c->bar, &srv);
+    return c->ovcontext->keyEvent(&key, &vxb, &c->bar, &srv);
 }
 
 int CIMCustomMenuHandler(void *data, UInt32 command, MenuRef mnu, 
     CIMInputBuffer *buf)
 {
-	char sbuf[512];
+    char sbuf[512];
     switch (command)
     {
-		case 'PREF':
-			fprintf (stderr, "launching application to edit %s\n", plistfile);
-			sprintf (sbuf, "open %s", plistfile);
-			system(sbuf);
-			return 1;
-			
-		case 'ABUT':
-			ShowAbout();
-			return 1;
-	}
+    case 'PREF':
+        fprintf (stderr, "launching application to edit %s\n", plistfile);
+        sprintf (sbuf, "open %s", plistfile);
+        system(sbuf);
+        return 1;
+	
+    case 'ABUT':
+        ShowAbout();
+        return 1;
+    }
     
-    if (command >= usermenu && command <= usermenu+list.imcntr)
-    {
+    if (command >= usermenu && command <= usermenu+list.imcntr) {
     	int newpos=command-usermenu;
     	
     	char buf[256];
@@ -362,40 +324,36 @@ int CIMCustomMenuHandler(void *data, UInt32 command, MenuRef mnu,
     	SetItemMark(mnu, oldpos+1, 0);
     	SetItemMark(mnu, newpos+1, checkMark);
     	
-		OVInputMethod *newim=list.impair[newpos].im;
+        OVInputMethod *newim=list.impair[newpos].im;
 		
-		if (!list.impair[newpos].inited)
-		{
-			char buf[256];
-			newim->identifier(buf);
-			
-			OVDictionary *global=GetGlobalConfig();
-			OVDictionary *local=GetLocalConfig(buf);
-			newim->initialize(global, local, &srv, (char*)loaddir);
-			list.impair[newpos].inited=1;
-			delete local;
-			delete global;
-		}
+        if (!list.impair[newpos].inited) {
+            char buf[256];
+            newim->identifier(buf);
+            
+            OVDictionary *global=GetGlobalConfig();
+            OVDictionary *local=GetLocalConfig(buf);
+            newim->initialize(global, local, &srv, (char*)loaddir);
+            list.impair[newpos].inited=1;
+            delete local;
+            delete global;
+        }
 		
     	newim->identifier(buf);
     	murmur ("user wants to switch IM, newimpos=%d, new im id=%s", newpos, buf);
     	
-		// kill all existing context
-		for (int i=0; i<vxMaxContext; i++)
-		{
-			if (pool[i])
-			{
-				inputmethod->deleteContext(pool[i]->ovcontext);
-				pool[i]->ovcontext=newim->newContext();
-			}
-		}
-		
-		inputmethod=newim;
-		
+        // kill all existing context
+        for (int i=0; i<vxMaxContext; i++) {
+            if (pool[i]) {
+                inputmethod->deleteContext(pool[i]->ovcontext);
+                pool[i]->ovcontext=newim->newContext();
+            }
+        }
+
+        inputmethod=newim;
     	OVDictionary *global=GetGlobalConfig();
     	global->setString("currentIM", buf);
-		delete global;
-		sysconfig->write();
+        delete global;
+        sysconfig->write();
     }
     
     
