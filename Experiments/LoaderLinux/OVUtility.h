ea4599f0c77ca6fba6bf4b49d79c5d98082a1ce8
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-12-07 03:57:13 +0000

    r721@dove:  lukhnos | 2004-12-07T03:57:04.378680Z  Added /Experiments/LoaderLinux. This is a self-contained test.

---
+++ Experiments/LoaderLinux/OVUtility.h
@@ -0,0 +1,45 @@
+/* @(#)OVUtility.h
+ */
+
+#ifndef _OVUTILITY_H
+#define _OVUTILITY_H 1
+
+#include <stdio.h>
+
+/**
+ * Debugging routine.
+ * This function accept the same argument as in printf(),
+ * and print message to stderr. But it doesn't print anything
+ * unless OVDEBUG is set at compile time.
+ */
+inline void murmur(const char* format,...) {
+#ifdef OVDEBUG
+    va_list args;
+    va_start (args, format);
+    vfprintf (stderr,format,args);
+    va_end (args);
+    fprintf (stderr,"\n");
+#endif
+}
+
+const int ovMaxPoolSize=256;
+
+class OVAutoDeletePool
+{
+public:
+    OVAutoDeletePool() : p(0) {}
+    ~OVAutoDeletePool() { for (int i=0; i<p; i++) if (pool[i]) delete pool[i]; }
+    OVObject* add(OVObject *o)
+    {
+        if (!o) return o;
+        if (p==ovMaxPoolSize) return o; // note: no exception
+        return pool[p++]=o;
+    }
+        
+protected:
+    int p;
+    OVObject *pool[ovMaxPoolSize];
+};
+
+#endif /* _OVUTILITY_H */
+
