d0c38d1ba8d64bb7c72b8af7323970626829e84c
gugod <gugod@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-12-08 09:08:26 +0000

    r4155@ib:  gugod | 2004-12-08T08:09:27.636060Z  even simpler. (with GNU make)

--- Mk/ov.osx.mk
+++ Mk/ov.osx.mk
@@ -1,10 +1,3 @@
 
-VXUtility.o: ../../Loaders/OSX/VXUtility.cpp
-	$(CPP) $(CFLAGS) -c $<
-
-VXCIN.o: ../../InputMethod/OVIMXcin/VXCIN.mm
-	$(CPP) $(CFLAGS) -c $<
-
-VXCandidate.o: ../../InputMethod/OVIMXcin/VXCandidate.mm
-	$(CPP) $(CFLAGS) -c $<
+VPATH+= ../../Loaders/OSX:../../InputMethod/OVIMXcin
 
