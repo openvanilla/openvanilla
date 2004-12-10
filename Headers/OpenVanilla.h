f69a08697aff345de85e0fbe8854cc7d31a104a1
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-12-10 12:49:15 +0000

    r803@dove:  lukhnos | 2004-12-10T12:49:07.601383Z  Checked out older version of OpenVanilla.h

--- Headers/OpenVanilla.h
+++ Headers/OpenVanilla.h
@@ -1,131 +1,200 @@
+// OpenVanilla.h
+// Copyright (c) 2004 by Lukhnos D. Liu (lukhnos@openvanilla.org)
+// and Kang-ming Liu (gugod@gugod.org)
+
+
+#ifndef __OpenVanilla_h
+#define __OpenVanilla_h
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+const unsigned int ovVersion=0x0000ffff;
+
+class OVObject
+{
+public:
+    virtual ~OVObject() {}
+};
+
+typedef int OVEncoding;
+typedef int OVLanguage;
+typedef int OVException;
+
+enum
+{
+    ovEncodingUTF8=0,
+//  ovEncodingUTF16Auto=1,
+    ovEncodingUTF16BE=2,     
+//  ovEncodingUTF16LE=3,
+    ovEncodingBig5=16,
+    ovEncodingBig5HKSCS=16,
+    ovEncodingEUC_CN=24,
+    ovEncodingNon8BitEncodingMask=7
+};
+
+enum
+{
+    ovLangAll=0,
+    ovLangTradChinese=1,
+    ovLangSimpChinese=2,
+    ovLangJapanese=3,
+    ovLangKorean=4,
+	ovLangWestern=255,
+};
+
+enum
+{
+    ovException=0,
+    ovExceptionMemory=1,
+    ovExceptionBuffer=2,
+    ovExceptionNotSupported=3
+};
+
+class OVTextBar : public OVObject
+{
+public:
+    // functions that update the content of the text bar
+    virtual OVTextBar* clear() { return this; }
+
+    // receive a generic type pointer, the length is the number of CODE POINTS
+    // (for byte type, length of total bytes; for UTF-16 type, surrogates incl'd)
+    virtual OVTextBar* append(void *s, OVEncoding e=ovEncodingUTF8, int len=0)
+        { return this; }
+
+    virtual OVTextBar* hide() { return 0; }
+    virtual OVTextBar* show() { return 0; }
+    virtual OVTextBar* update() { return 0; }
+    virtual int onScreen() { return 0; }
+};
 
 
 class OVBuffer : public OVObject
 {
 public:
-    virtual OVBuffer* clear()=0;
-    virtual OVBuffer* send()=0; 
-    virtual OVBuffer* update(int cursorpos=-1, int hilightfrom=-1, 
-        int hilightto=-1)=0;
-    virtual OVBuffer* append (const void *s, OVEncoding e=oveUTF8, int len=0)=0;
+    virtual OVBuffer* clear() { return this; }
+    
+    virtual OVBuffer* send(OVLanguage lang=ovLangAll) { return this; }
+    virtual OVBuffer* update(OVLanguage lang=ovLangAll)
+            { return update(-1, -1, -1, lang); }
+    virtual OVBuffer* update(int cursorpos, int hilightfrom, 
+        int hilightto, OVLanguage lang=ovLangAll)
+            { return this; }
+    virtual OVBuffer* append (void *s, OVEncoding e=ovEncodingUTF8, int len=0)
+        { return this; }
+	
 	virtual OVBuffer* appendChar(char c)
         { char buf[2]; buf[0]=c; buf[1]=0; return append(buf); }
-    virtual int length()=0;
+	
+    // returns the number of GRAPHEMES (ie. actual chars displayed) in the buffer
+    virtual int length() { return 0; }    
+};
+
+
+class OVDictionary : public OVObject
+{
+public:
+    virtual int keyExist(const void *key, OVEncoding e=ovEncodingUTF8, int keylen=0)
+        { return 0; }
+        
+    virtual int getInt(const void *key, OVEncoding e=ovEncodingUTF8, int keylen=0)
+        { return 0; }
+    virtual int setInt(const void *key, int value, OVEncoding e=ovEncodingUTF8,
+        int keylen=0) { return 0; }
+            	
+	virtual int getString(const void *key, void *str, OVEncoding e=ovEncodingUTF8,
+		int keylen=0, int maxlen=0) { return 0; }
+	virtual int setString(const void *key, const void *value, 
+        OVEncoding e=ovEncodingUTF8, int keylen=0, int valuelen=0) { return 0; }
+		
+    virtual int newDictionary(const void *key, OVEncoding e=ovEncodingUTF8, 
+        int keylen=0) { return 0; }
+    virtual OVDictionary* getDictionary(const void *key, 
+        OVEncoding e=ovEncodingUTF8, int keylen=0)   { return NULL; }
 };
 
 
 class OVService : public OVObject
 {
 public:
-    virtual OVService* beep()=0;
-    virtual OVService* clearTextMsg()=0;
-    virtual OVService* showTextMsg(const void *str, OVEncoding enc=oveUTF8, 
-        int len=0)=0;
+    virtual void beep()  {}
 };
 
 
-class OVCandidateBox : public OVObject
+class OVKeyCode : public OVObject
 {
 public:
-    virtual int setSelectKey(const char *selkey);
-    virtual OVCandidateBox* clear();
-    virtual OVCandidateBox* append(const void *str, OVEncoding enc=oveUTF8,
-        int len=0)=0;
-    virtual OVCandidateBox* show();
-    virtual OVCandidateBox* finalize();
+    virtual int code() { return 0; }
+    virtual int upper() { return toupper(code()); }
+    virtual int lower() { return tolower(code()); }
+    
+    virtual int isCode(int argc, ...)
+    {
+        int r=0;
+        va_list ap;
+        va_start(ap, argc);
+        for (int i=0; i<argc; i++) 
+            { int j=va_arg(ap, int); if (j==code()) { r=1; break; } }
+        va_end(ap);
+        return r;
+    }
+
+    virtual int isShift() { return 0; }
+    virtual int isCapslock() { return 0; }
+    virtual int isCtrl() { return 0; }
+    virtual int isAlt() { return 0; }
+    virtual int isOpt() { return 0; }
+    virtual int isCommand() { return 0; }
+    virtual int isPrintable() { return isprint(code()); }
+    virtual int isAlpha() { return isalpha(code()); }
+    virtual int isNumeric() { return (code() <= '9' && code() >= '0'); }
 };
-
-
-enum {
-    oveUTF8=1,
-    oveUTF16BE=2,
-    oveUTF16LE=3,
-    oveUTF16Auto=4,
-    oveBig5=8,
-    oveBig5HKSCS=8,
-    oveEUC_CN=9
-};
-
-class OVList : public OVObject
-{
-public:
-    virtual int count()=0;
-    virtual int getInt(int index)=0;
-    virtual int getString(int index, char *str, OVEncoding enc=oveUTF8, int 
-        maxlen=0)=0;
-};
-
-class OVDictionary : public OVObject
-{
-public:
-    virtual int keyExist(const char *key, OVEncoding enc=oveUTF8)=0;
-    virtual int setInt(const char *key, int value, OVEncoding enc=oveUTF8)=0;
-    virtual int getInt(const char *key, OVEncoding enc=oveUTF8)=0;
-    virtual char* setString(const char *key, const char *value, 
-        OVEncoding enc=oveUTF8);
-    virtual int getString(const char *key, char *str, OVEncoding enc=oveUTF8, 
-        int maxlen=0)=0;
-    virtual int newDictionary(const char *key, int replaceIfExist=0);
-   
-    // these objects must be deleted by the caller
-    OVList* getList(const char *key, OVEncoding enc=oveUTF8)=0;
-    OVDictionary* getDictionary(char *key, OVEncoding enc=oveUTF8)=0;
-
-    // we implement this two functions
-    virtual int getIntDefault(const char *key, int defvalue, 
-        OVEncoding enc=oveUTF8)=0;
-    virtual int getStringDefault(const char *key, char *str, 
-        const void *defvalue, OVEncoding enc=oveUTF8, int maxlen=0)=0;
-};
-
 
 class OVIMContext : public OVObject
 {
 public:
-    virtual int keyEvent(OVKeyCode *key, OVBuffer *buf, 
-        OVCandidateBox *candi, OVService *srv)=0;
+    virtual int activate(OVService*) { return 1; }
+    virtual int deactivate(OVService*) { return 1; }
+    virtual int clear() { return 1; }
+    virtual int keyEvent(OVKeyCode*, OVBuffer*, OVTextBar*, OVService*) 
+        { return 0; }        
 };
 
+const int ovMaxIdentifierLength=32;
+const int ovMaxNameLength=128;
 
-class OVComponent : public OVObject
+class OVInputMethod : public OVObject
 {
 public:
-    virtual const char *identifier()=0;
-    virtual const char *localizedName(const char *locale, OVEncoding *enc)=0;
+    virtual int identifier(char* s)
+        { *s=0; return 0; }
+    virtual int name(char *locale, void *s, OVEncoding *enc)
+        { *(char*)s=0; *enc=ovEncodingUTF8; return 0; }
+
+    virtual int initialize(OVDictionary* globalconfig, OVDictionary* localconfig,
+        OVService* srv, char* path)
+            { return 1; }
+    virtual int terminate(OVDictionary*, OVDictionary*, OVService*)
+        { return 1; }    
+    virtual int update(OVDictionary*, OVDictionary*) { return 1; }
     
-    virtual int initialize(OVDictionary *loaderPref,
-                           OVDictionary *modulePref,
-                           const char *modulePath,
-                           const char *userPath,
-                           const char *seperator)=0;
-    virtual void terminate(OVDictionary *modulePref)=0;
-    virtual void update(OVDictionary *loaderPref, 
-                        OVDictionary *modulePref)=0;
+    virtual OVIMContext *newContext() { return new OVIMContext; }
+    virtual void deleteContext(OVIMContext* s) { delete s; }
 };
-                           
-class OVInputMethod : public OVComponent
+
+
+enum 
 {
-public:
-    virtual OVIMContext *newConext()=0;
-    virtual void deleteContext(OVIMContext*)=0;
-    
-    virtual const char *getCandidateSelectKeys();
-    virtual const char *getValidKeys();
+    ovkEscape=27, 
+    ovkSpace=32,
+    ovkReturn=13, ovkMacEnter=3,
+    ovkDelete=127, ovkBackspace=8,
+    ovkUp=30, ovkDown=31, ovkLeft=28, ovkRight=29,
+    ovkHome=1, ovkEnd=4,
+    ovkPageUp=11, ovkPageDown=12,
+    ovkTab='\t'
 };
 
-class OVKeyFilter : public OVComponent
-{
-public:
-    virtual int keyEvent(OVKeyCode *key, OVBuffer *buf, OVCandidateBox *candi,
-        OVService *srv)=0;
-};
-
-class OVBufferFilter : public OVComponent
-{
-public:
-    virtual OVEncoding bufferEncoding()=0;
-    virtual int process(void *data, int datalen, void *output, int maxoutputlen,
-        OVCandidateBox *candi);
-};
-
-
+#endif  // of #ifndef __OpenVanilla_h
