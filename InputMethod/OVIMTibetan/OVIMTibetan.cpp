a5dc1500ed74b07c6d7d8a9e2af2ad6b6f3432ef
zonble <zonble@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-12-24 18:17:06 +0000

    Reformat the codes of OVIMTibetan to be in a united coding style.

--- InputMethod/OVIMTibetan/OVIMTibetan.cpp
+++ InputMethod/OVIMTibetan/OVIMTibetan.cpp
@@ -28,40 +28,75 @@ char buf[ebMaxKeySeq];
 int len;
 };
 
-char ConsonantKeys[37] = {
+#define SYMBOL_NUM 6
+#define CONSONAT_NUM 37
+#define VOWEL_NUM 8
+
+char SymbolKeys[SYMBOL_NUM] = 
+{
+	'!', '|', '~', '@', '{', '}'
+};
+
+unsigned short SymbolChars[SYMBOL_NUM] =
+{
+	0x0F00,0x0F0D,0x0F04,0x0F05,0x0F3C,0x0F3D
+};
+
+char ConsonantKeys[CONSONAT_NUM] = 
+{
 	'k','K','g','G','c','C','j','N','n','t',
 	'T','d','D','p','P','b','B','m','x','X',
 	'D','w','z','Z','\'','y','r', 'l', 'S','s',
 	'h','A', 'v', 'q', 'Q', 'B', 'V'
 };
-unsigned short ConsonantChars[37] = {
+
+unsigned short ConsonantChars[CONSONAT_NUM] = 
+{
 	0x0F40,0x0F41,0x0F42,0x0F44,0x0F45,0x0F46,0x0F47,0x0F49,0x0F53,0x0F4F,
 	0x0F50,0x0F51,0x0F52,0x0F54,0x0F55,0x0F56,0x0F57,0x0F58,0x0F59,0x0F5A,
 	0x0F5B,0x0F5D,0x0F5F,0x0F5E,0x0F60,0x0F61,0x0F62,0x0F63,0x0F64,0x0F66,
 	0x0F67,0x0F68,0x0F4C,0x0F4A,0x0F4B,0x0F65,0x0F4E
 };
 
-char VowelKeys[8] = {
+char VowelKeys[VOWEL_NUM] = 
+{
 	'a','i','u','o',
 	'e','E','O','I'
 };
-unsigned short VowelChars[8] = {
+
+unsigned short VowelChars[VOWEL_NUM] = 
+{
 	0,0x0F72,0x0F74,0x0F7C,
 	0x0F7A,0x0F7B,0x0F7D,0x0F80
 }; 
 
-int isConsonantKey(int key){
+short isSymbolKey(int key)
+{
 	int i;
-	for(i=0; i< 37; i++){
+	for(i=0; i< SYMBOL_NUM; i++)
+	{
+		if(key == SymbolKeys[i])
+			return i;
+	}
+	return -1;
+}
+
+short isConsonantKey(int key)
+{
+	int i;
+	for(i=0; i< CONSONAT_NUM; i++)
+	{
 		if(key == ConsonantKeys[i])
 			return i;
 	}
 	return -1;
 }
 
-int isVowelKey(int key){
+short isVowelKey(int key)
+{
 	int i;
-	for(i=0; i< 8; i++){
+	for(i=0; i< VOWEL_NUM; i++)
+	{
 		if(key == VowelKeys[i])
 			return i;
 	}
@@ -109,7 +144,13 @@ public:
             return 1;   // key processed
         }
 		
-		if (key->isCode(1, ovkSpace)) //Space 輸出逗點
+		if (key->isCode(4, ovkUp, ovkDown, ovkLeft, ovkRight)) //Lock when composing
+		{ 
+			if(!strlen(keyseq.buf)) return 0;
+			return 1;
+		}
+		
+		if (key->isCode(1, ovkSpace)) //Space to keyin Tibetan common.
         {
 			i = 0x0F0B;
 			buf->append(&i, ovEncodingUTF16Auto, 1)->send()->clear();
@@ -122,24 +163,28 @@ public:
 		{
 			if(!strlen(keyseq.buf)) return 0;
 			keyseq.remove();
-			buf->clear();
-			buf->update();
+			buf->clear()->update();
 			textbar->hide();
 			return 1;
 		}
 		
         if (key->isPrintable())
         {
-			int isVowel = -1, isConsonant = -1, isConsonantPrev = -1;
+			int isSymbol = -1, isVowel = -1, isConsonant = -1, isConsonantPrev = -1;
 			
-			if(key->code() == '.'){ // 輸入的.，送出空白
+			if(key->code() == '.') // Keyin a space.
+			{ 
 				i = 0x0F20 + (key->code() - '0');
 				buf->append((char *)" ")->send();
+				keyseq.clear();
+				textbar->hide();
 				return 1;   // key processed
 			}
 			
-			if(key->code() == 'f' || key->code() == 'F'){ // 如果是 f
-				if(keyseq.buf[0] == 'f') {
+			if(key->code() == 'f' || key->code() == 'F')	// 如果是 f
+			{ 
+				if(keyseq.buf[0] == 'f')
+				{
 					buf->send()->clear();
 					keyseq.clear();
 					textbar->hide();
@@ -152,16 +197,27 @@ public:
 				return 1;   // key processed
 			}
 			
-			if(key->code() >= '0' && key->code() <= '9'){ // 輸入的是數字，直接送出
+			if(key->code() >= '0' && key->code() <= '9')	// 輸入的是數字，直接送出
+			{ 
 				i = 0x0F20 + (key->code() - '0');
 				buf->append(&i, ovEncodingUTF16Auto, 1)->send()->clear();
 				keyseq.clear();
 				textbar->hide();
 				return 1;   // key processed
 			}
-			if((isVowel = isVowelKey(key->code())) > -1){ // 輸入的是母音
+			if((isSymbol = isSymbolKey(key->code())) > -1) // 輸入的是符號
+			{
+				i = SymbolChars[isSymbol];
+				buf->append(&i, ovEncodingUTF16Auto, 1)->send()->clear();
+				keyseq.clear();
+				textbar->hide();
+				return 1;   // key processed
+			}
+			if((isVowel = isVowelKey(key->code())) > -1)	// 輸入的是母音
+			{ 
 				if(keyseq.buf[0] &&
-				   (isConsonantKey(keyseq.buf[0]) > -1 || keyseq.buf[0] == 'f')){
+				   (isConsonantKey(keyseq.buf[0]) > -1 || keyseq.buf[0] == 'f'))
+				{
 					i = VowelChars[isVowel];
 					buf->append(&i, ovEncodingUTF16Auto, 1)->send()->clear();
 					keyseq.clear();
@@ -169,9 +225,12 @@ public:
 				}
 				return 1;   // key processed
 			}
-			if((isConsonant = isConsonantKey(key->code())) > -1){ // 輸入的是子音
-				if(keyseq.buf[0] == 'f') {
-					if(strlen(keyseq.buf) < 4){
+			if((isConsonant = isConsonantKey(key->code())) > -1) // 輸入的是子音
+			{ 
+				if(keyseq.buf[0] == 'f') 
+				{
+					if(strlen(keyseq.buf) < 4)
+					{
 						i = ConsonantChars[isConsonant];
 						if(strlen(keyseq.buf) > 1)
 							i = i + 0x50; // 加了50就會變成sub的字母囉！
@@ -183,7 +242,7 @@ public:
 					keyseq.clear();
 					textbar->hide();
 					}
-				} else if((isConsonantPrev = isConsonantKey(keyseq.buf[0])) > -1){
+				} else if((isConsonantPrev = isConsonantKey(keyseq.buf[0])) > -1) {
 					//前一個也是子音，把前一個送出
 					buf->send()->clear();
 					keyseq.clear();
