73f7b48f12fac630967ee7c2bfbb63f915c0179f
mjhsieh <mjhsieh@da5ee8da-a019-0410-8a94-01093a00bd73>
2004-12-29 12:04:52 +0000

    r627@mjhsieh:  mjhsieh | 2004-12-29 04:04:39 -0800  simplfied

--- clean.sh
+++ clean.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 /usr/bin/make clean
 
-cd Experiments/OVPrefPane ; xcodebuild clean ; cd ../..
-
 cd Loaders/OSX ; xcodebuild clean ; cd ..
 
-cd InputMethod ;
-for directory in `ls -F | grep "\/" | sed -e 's/\///'`
-do
-   make -C $directory clean
-done
-cd ..
+#cd InputMethod ;
+#for directory in `ls -F | grep "\/" | sed -e 's/\///'`
+#do
+#   make -C $directory clean
+#done
+#cd ..
+cd InputMethod ; make clean ; cd ..
+
+cd Experiments/OVPrefPane ; xcodebuild clean ; cd ../..
