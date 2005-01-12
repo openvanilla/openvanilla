454b86a5ed6e323a3ac8393f40aa878d03a9f399
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-01-12 03:08:17 +0000

    r892@dove:  lukhnos | 2005-01-12T03:07:32.647327Z  Removed Loaders/OSXFancy and Loaders/OSX; renamed Loaders/OSXColor to  Loaders/OSX

--- Loaders/OSXFancy/VXHanConvert.h
+++
@@ -1,9 +0,0 @@
-// VXHanConvert.h
-
-#ifndef __VXHanConvert_h
-#define __VXHanConvert_h
-
-unsigned short VXUCS2TradToSimpChinese(unsigned short c);
-unsigned short VXUCS2SimpToTradChinese(unsigned short c);
-
-#endif
