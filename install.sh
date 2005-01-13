0462d3c71f1e6b4ed66c719abd6ebc6457fdbaa0
zonble <zonble@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-01-13 08:26:17 +0000

    r1173@zonble:  zonble | 2005-01-13 16:19:06 +0800  Use install.sh to install everything.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+make; sudo make install
+
+cd Loaders/OSX ; make ; sudo make install; cd ../..
+
+cd InputMethod ; make ; sudo make install; cd ..
+
+cd InputMethod/OVIMXcin ; make -f Makefile.AddPlist ; sudo ./AddPlist ; cd ../..
+
+cd Experiments/OVPrefPane ; make ; sudo make install ; cd ../..
+
+cd Documents ; sudo make install ; cd ..
+
+cd InputMethodExtra/OVIMXcinExtra; sudo make install ; cd ../..
