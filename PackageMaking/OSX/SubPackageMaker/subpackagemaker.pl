682ab538d6f1d7886934fea705757117856f0bc5
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-01-14 16:18:58 +0000

    r961@dove:  lukhnos | 2005-01-14T11:08:10.209544Z  Info.plist.template for SubPackageMaker

--- PackageMaking/OSX/SubPackageMaker/subpackagemaker.pl
+++ PackageMaking/OSX/SubPackageMaker/subpackagemaker.pl
@@ -2,10 +2,13 @@
 use utf8;
 use IO::All;
 
+my $infotemplate < io "Info.plist.template";
 my $desctemplate < io "Description.plist.template";
 
 my $listfile=shift or die "usage: subpackagemaker list\n";
 open LIST, "<$listfile" or die "cannot open file $listfile\n";
+
+print "start making packages. you may be asked for sudo password once.\n";
 
 while (<LIST>) {
     chomp;
@@ -15,6 +18,12 @@ while (<LIST>) {
     @d = split /\s/;
     
     my $name=$d[1];
+    
+    my $startparam="NoRestart";
+    my $info=$infotemplate;
+    if ($d[2]) { $startparam=$d[2]; }
+    $info =~ s/REPLACESTARTPARAM/$startparam/;
+    $info > io "Info.plist";    
     
     my %title;
     my %desc;
@@ -39,7 +48,6 @@ while (<LIST>) {
     if (/\@description/) {
         while (1) {
             if (/\@files/) { last; }
-            print "desc $_\n";
             @d = split /\s/;
             if (scalar @d==1) {
                 my $l=<LIST>;
@@ -65,19 +73,15 @@ while (<LIST>) {
         if (/\@end/) { last; }
         
         push @files, $_;
-        print "add file $_\n";
     }
         
-    print "PARSED!\nname=$name\n";
-    for my $x (keys %title) {
-        print "$x=$title{$x}\n";
-        
+    print "start making package $name\n";
+    for my $x (keys %title) {        
         my $outfile="Description.plist";
         
         if (!($x eq "default")) {
             system "mkdir -p Resource/$x.lproj";
             $outfile = "Resource/$x.lproj/" . $outfile;
-            print "generating $outfile , locale=$x, content=$title{$x}!\n";
         }
         
         my $content = $desctemplate;
@@ -96,7 +100,6 @@ while (<LIST>) {
     
     open FLST, ">Filelist";
     for my $xx (@files) {
-        print "file=$xx\n";
         print FLST "$xx\n";
     }
     close FLST;
@@ -105,3 +108,7 @@ while (<LIST>) {
     system "./mksub.sh $name";
 }
 
+system "rm Info.plist";
+system "rm Description.plist";
+
+print "subpackagemaker terminates without error.\n";
