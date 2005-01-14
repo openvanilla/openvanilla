8dc0723e30c5f9520edee9defc79d29cc0a840ed
zonble <zonble@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-01-14 06:22:11 +0000

    r1204@zonble:  zonble | 2005-01-14 13:39:02 +0800  Fix the problem about the path in install.sh.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cd InputMethod ; make ; sudo make install; cd ..
 
 cd InputMethod/OVIMXcin ; make -f Makefile.AddPlist ; sudo ./SetOVXcinDefaults.sh ; cd ../..
 
-cd Utilities/OSX/OVPrefPane ; make ; sudo make install ; cd ../..
+cd Utilities/OSX/OVPrefPane ; make ; sudo make install ; cd ../../..
 
 cd Documents ; sudo make install ; cd ..
 
