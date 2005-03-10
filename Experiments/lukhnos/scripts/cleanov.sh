8bdfb6ef6a31600a0516cfce7d84fe0a8bafa201
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-03-10 03:09:14 +0000

    added trunk/Experiments/lukhnos: draft from janusng, libtabe to sqlite3 importer, phonetics table conflict statistics, and a now defunct shell script to clean up everything about OV

---
+++ Experiments/lukhnos/scripts/cleanov.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo rm -rf /Library/PreferencePanes/OVPref*
+sudo rm -rf /Library/OpenVanilla/*
+sudo rm -rf /usr/lib/libchewing*
+sudo rm -rf /usr/local/lib/libchewing*
+sudo rm -rf /Library/Components/OVLoader*
+sudo rm -rf /Library/Frameworks/ChewingPP.framework
+sudo rm -rf /Library/Receipts/OV*
+sudo rm -rf /Library/Receipts/OpenVanilla*
+rm -rf ~/Library/OpenVanilla
