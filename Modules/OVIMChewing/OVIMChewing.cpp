7c4e4bc028bdafa842d642c1903c927c7cbd257e
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-05-14 21:51:31 +0000

    r146@dove:  lukhnos | 2005-05-15 05:49:45 +0800  1. Beautify OVIMChewing.cpp  2. Using a modified version of ChewingPP, NOW WE CAN SEE THE UNDERLINES     WHEN DO YOU SHIFT-LEFT/RIGHT!  3. Begin to add safe-check codes

--- Modules/OVIMChewing/OVIMChewing.cpp
+++ Modules/OVIMChewing/OVIMChewing.cpp
@@ -1,17 +1,14 @@
-// -*- mode: c++; c-basic-offset: 4; font-lock-mode: t -*-
+// OVIMChewing.cpp
 
-#define OVDEBUG 0
+#define OV_DEBUG
 
 #include <stdio.h> 
 #include <stdlib.h>
 #include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/syslimits.h>
 #include <sys/types.h>
-#include <unistd.h>
-#include <string.h>  
-#include <pwd.h>
+#include <sys/stat.h>
+#include <string.h>
 #include <OpenVanilla/OpenVanilla.h>
 #include <OpenVanilla/OVLibrary.h>
 #include <OpenVanilla/OVUtility.h>
@@ -26,16 +23,17 @@ class OVIMChewing;
 class OVIMChewingContext : public OVInputMethodContext 
 {
 public:
-    OVIMChewingContext(OVIMChewing *p, Chewing* chew) {p=parent; im=chew;}
-    virtual ~OVIMChewingContext() {}
+    OVIMChewingContext(OVIMChewing *p, Chewing* chew) {
+        p=parent;
+        im=chew;
+    }
 	
 	virtual void start(OVBuffer *key, OVCandidate *textbar, OVService *srv) {	
 	} 
     virtual void clear() { im->Enter(); }
     virtual void end() { im->Enter(); }
      
-    virtual int keyEvent(OVKeyCode *key, OVBuffer *buf, OVCandidate *textbar,
-						 OVService *srv) {
+    virtual int keyEvent(OVKeyCode *key, OVBuffer *buf, OVCandidate *textbar, OVService *srv) {
         if(key->isCommand()) return 0;
         KeyPress(key,buf,textbar,srv);
         if(im->KeystrokeIgnore()) return 0;
@@ -44,33 +42,37 @@ public:
         return 1;
     }
   
-    void KeyPress(OVKeyCode *key, OVBuffer *buf,
-                  OVCandidate *textbar, OVService *srv) {
+protected:
+    void KeyPress(OVKeyCode *key, OVBuffer *buf, OVCandidate *textbar, OVService *srv) {
         int k = key->code();
         Capslock(key,buf,textbar,srv);
         if(k == ovkSpace) {
             key->isShift() ? im->ShiftSpace() : im->Space();
-        } else if (k == ovkLeft)   {
+        }
+        else if (k == ovkLeft)   {
             key->isShift() ? im->ShiftLeft()  : im->Left();
-        } else if (k == ovkRight)  {
+        }
+        else if (k == ovkRight)  {
             key->isShift() ? im->ShiftRight() : im->Right(); 
         }
-        else if (k == ovkDown)   { im->Down();  }
-        else if (k == 27) { im->Esc();   }
-        else if (k == ovkTab)    { im->Tab();   }
-        else if (k == ovkHome)   { im->Home();  }
-        else if (k == ovkEnd)    { im->End();   }
+        else if (k == ovkDown) { im->Down(); }
+        else if (k == ovkEsc)  { im->Esc();  }
+        else if (k == ovkTab)  { im->Tab();  }
+        else if (k == ovkHome) { im->Home(); }
+        else if (k == ovkEnd)  { im->End();  }
         else if (k == ovkDelete || k == ovkBackspace) { im->Backspace() ; }
-        else if (k == ovkReturn  || k == 3) { im->Enter(); im->CommitReady(); }
-        else {DefaultKey(key,buf,textbar,srv);}
+        else if (k == ovkReturn) { im->Enter(); im->CommitReady(); }
+        else { 
+            DefaultKey(key,buf,textbar,srv);
+        }
     }
 
-    void DefaultKey(OVKeyCode *key, OVBuffer *buf,
-                    OVCandidate *textbar, OVService *srv) {
+    void DefaultKey(OVKeyCode *key, OVBuffer *buf, OVCandidate *textbar, OVService *srv) {
         if(key->isCtrl()) {
             if((key->code() >= '0') && (key->code() <= '9')) {
                 im->CtrlNum(key->code());
-            } else if(key->isAlt()) {
+            }
+            else if(key->isAlt()) {
                 im->CtrlOption(key->code());
             }
             return;
@@ -78,11 +80,11 @@ public:
         im->Key((key->isShift())?toupper(key->code()):tolower(key->code()));
     }
 
-    void Capslock(OVKeyCode *key, OVBuffer *buf,
-                  OVCandidate *textbar, OVService *srv) {
+    void Capslock(OVKeyCode *key, OVBuffer *buf, OVCandidate *textbar, OVService *srv) {
         if(key->isCapslock()) {
             if(im->ChineseMode()) im->Capslock();
-        } else if (!im->ChineseMode()) {
+        }
+        else if (!im->ChineseMode()) {
             im->Capslock();
         }
     }
@@ -90,134 +92,113 @@ public:
     void Redraw(OVBuffer *buf, OVService *srv) {
         const char *s1,*s2,*s3;
         
-        if(im->CommitReady() ) {
+        if(im->CommitReady()) {
             const char *s = im->CommitStr();
-			s = srv->toUTF8("big5", s);
-            buf->clear()->append(s)->send();
-            //free(s);
+            buf->clear()->append(srv->toUTF8("big5", s))->send();
         }
         
         s1 = srv->toUTF8("big5", im->Buffer(0,im->CursorPos()-1));
         s2 = srv->toUTF8("big5", im->ZuinStr());
         s3 = srv->toUTF8("big5", im->Buffer(im->CursorPos()));
         
-        buf->clear()->
-            append(s1)->
-            append(s2)->
-            append(s3)->
-            update(im->CursorPos(),-1,-1);
+        int ps=-1, pe=-1, ips=im->PointStart(), ipe=im->PointEnd();
+        if (ips > -1 && ipe !=0) {
+            if (ipe > 0) { ps=ips; pe=ps+ipe; }
+            else { ps=ips+ipe; pe=ips; }
+        }        
         
-        murmur("==> %s%s%s",s1,s2,s3);
+        // murmur("ips=%d, ipe=%d, ps=%d, pe=%d\n", ips, ipe, ps, pe);
+        buf->clear()->append(s1)->append(s2)->append(s3)->update(im->CursorPos(), ps, pe);
+        // murmur("==> %s%s%s",s1,s2,s3);
     }
     
     void CandidateWindow(OVCandidate *textbar, OVService *srv) {
         if(im->Candidate()) {
-            char s[20];
-			char *ch,selkey;
+            char s[64];
+			char *ch, selkey;
             textbar->clear();
             for(int i=0; i < im->ChoicePerPage() ; i++) {
                 ch      = im->Selection(i);
                 selkey  = im->SelKey(i);
                 if(ch[0]) {
-						char b[2];
-						sprintf(b, "%c.", selkey);
-						textbar->append((char *)b);
-						const char *cha = srv->toUTF8("big5", ch);
-						textbar->append(cha)->append(" ");
+                    char b[2];
+                    sprintf(b, "%c.", selkey);
+                    textbar->append((char *)b);
+                    const char *cha = srv->toUTF8("big5", ch);
+                    textbar->append(cha)->append(" ");
                 }
                 free(ch);
             }
             sprintf(s," %d/%d",im->CurrentPage() + 1,im->TotalPage());
-            textbar->append((char *)s);
+            textbar->append((char*)s);
             textbar->update();
             textbar->show();
-        } else {
+        }
+        else {
             textbar->hide();
         }
     }
 
 protected:
     OVIMChewing *parent;
-    Chewing    *im;
+    Chewing *im;
 };
 
-char* mkHashDir() {
-    char *ptr;
-    static char hashdir[PATH_MAX];
-    
-    // this code is merged from SpaceChewingOSX, to solve SecurityAgent.app bug
-    // this will be depracated as in the future userpath will be given us
-    // by the Loader
-    if (!(ptr=getenv("HOME")))
-    {
-        struct passwd *pw = getpwuid(getuid());
-        if (pw == NULL) strcpy(hashdir, "/tmp");
-        else strcpy(hashdir, pw->pw_dir);
-    }
-    else strcpy(hashdir, ptr);
-        
-    strcat(hashdir, "/Library/OpenVanilla") ;
-    mkdir(hashdir, S_IRWXU) ;
-    strcat(hashdir, "/OVIMChewing");
-    mkdir(hashdir, S_IRWXU) ;
-    
-    return hashdir;
-}
 
-class OVIMChewing : public OVInputMethod
-{
+class OVIMChewing : public OVInputMethod {
 public:
-    Chewing *chew;
-
     OVIMChewing() {
-        murmur ("new IM instance created");
+        chew=NULL;
     }
-    
+
     virtual ~OVIMChewing() {
-        murmur ("IM instance destroyed");
         delete chew;
     }
 
-    virtual int initialize(OVDictionary* l, OVService*, const char* modulePath)
-    {
-        char *hashdir = mkHashDir();
+    virtual int initialize(OVDictionary* l, OVService* s, const char* modulePath) {
         char chewingpath[PATH_MAX];
-        strcpy (chewingpath, modulePath);
-        strcat (chewingpath, "OVIMChewing");
+        char hashdir[PATH_MAX];
         
-        murmur ("OVIMChewing: creating ChewingPP instance, chewing data path=%s, whereas userhash data=%s", chewingpath, hashdir);
+        // we can always be sure that an ending seperator '/' is given to us
+        sprintf(hashdir, "%sOVIMChewing", s->userSpacePath(identifier()));
+        mkdir(hashdir, S_IRWXU);
+        sprintf(chewingpath, "%sOVIMChewing", modulePath);
+                
+        murmur ("OVIMChewing: initialize, chewing data=%s, userhash=%s", chewingpath, hashdir);
 		
+		// BECAUSE THE {SACRILEGIOUS WORDS HERE} libchewing HAS NO 
+		// EXCEPTION HANDLING HERE (BLAME OLD C-style assert() !!)
+		// WE HAVE TO DO ERROR CHECKING OURSELVES, OTHERWISE WE ARE
+        // DOOMED IF CHEWING DATA DOESN'T EXIST. THIS MAKES OUR LIFE
+        // HARD BUT WE SHOULD TRY NOT TO COMPLAIN
         chew = new Chewing(chewingpath, hashdir);
 
-        if(!l->keyExist("keyboardLayout"))
-            l->setInteger("keyboardLayout", 0);
+        if(!l->keyExist("keyboardLayout")) l->setInteger("keyboardLayout", 0);
 		chew->SetKeyboardLayout(l->getInteger("keyboardLayout"));
         return 1;
     }
 
-    virtual int update(OVDictionary*, OVDictionary* localconfig)
-    {
+    virtual void update(OVDictionary* localconfig, OVService*) {
         chew->SetKeyboardLayout(localconfig->getInteger("keyboardLayout"));
-        return 1; 
     }
 
-    virtual const char *identifier()
-    {
+    virtual const char *identifier() {
         return "OVIMChewing";
     }
 
-    virtual const char *localizedName(const char *locale)
-    {
-        if (!strcasecmp(locale, "zh_TW")) return "OV 酷音";
-        if (!strcasecmp(locale, "zh_CN")) return "OV 酷音";
- 	    return "OV Chewing";
+    virtual const char *localizedName(const char *locale) {
+        if (!strcasecmp(locale, "zh_TW")) return "酷音";
+        if (!strcasecmp(locale, "zh_CN")) return "酷音";
+ 	    return "Chewing (smart phonetics)";
     }
 
-    virtual OVInputMethodContext* newContext()
-    {
+    virtual OVInputMethodContext* newContext() {
 	    return new OVIMChewingContext(this, chew); 
     }
+    
+protected:
+    Chewing *chew;
 };
 
-// use these two wrappers
+// the module wrapper
 OV_SINGLE_MODULE_WRAPPER(OVIMChewing);
