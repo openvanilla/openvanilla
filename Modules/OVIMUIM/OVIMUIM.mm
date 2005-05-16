9865660392e9f431124f8831e95950a9683eecb5
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-05-16 19:17:03 +0000

    r204@dove:  lukhnos | 2005-05-17 03:11:42 +0800  A fully functional OVIMUIM (anthy Hiragana mode). Some UI glitches, but  functional.

--- Modules/OVIMUIM/OVIMUIM.mm
+++ Modules/OVIMUIM/OVIMUIM.mm
@@ -66,9 +66,18 @@ public:
 
         // convert keycode to UIM keycode
         switch (k->code()) {
-            case ovkLeft:   keycode=UKey_Left; break;
-            case ovkRight:  keycode=UKey_Right; break;
-            case ovkReturn: keycode=UKey_Return; break;
+            case ovkHome:       keycode=UKey_Home; break;
+            case ovkEnd:        keycode=UKey_End; break;
+            case ovkPageUp:     keycode=UKey_Prior; break;
+            case ovkPageDown:   keycode=UKey_Next; break;
+            case ovkLeft:       keycode=UKey_Left; break;
+            case ovkRight:      keycode=UKey_Right; break;
+            case ovkUp:         keycode=UKey_Up; break;
+            case ovkDown:       keycode=UKey_Down; break;
+            case ovkReturn:     keycode=UKey_Return; break;
+            case ovkEsc:        keycode=UKey_Escape; break;
+            case ovkDelete:     keycode=UKey_Delete; break;
+            case ovkBackspace:  keycode=UKey_Backspace; break;
         }
 
         int r=uim_press_key(uc, keycode, 0);
@@ -79,42 +88,99 @@ public:
     }
     
 // UIM callbacks
+public:
     void uimClear() {
         if (!ovbuf) return;
         ovbuf->clear()->update();
-        upos=0;
-        uhighlightstart=uhighlightend=0;
+        upos=uhighlightstart=uhighlightend=0;
         ucountpos=1;
     }
     
     void uimPush(int state, const char *s) {
-        if (s) murmur("uimPush, state=%d, string=%d, strlen=%s", state, s ? strlen(s): 0, s ? s :0);
+        // UIM states
+        // UPreeditAttr_UnderLine = 1,          // draw underline (NORMAL MODE)
+        // UPreeditAttr_Reverse = 2,            // draw "candidate line" (HIGHLIGHT)
+        // UPreeditAttr_Cursor = 4,             // cursor stops here
+        // binary OR op: (2 | 4) = 6            // candidate lne & cursor stops here
+        
+        murmur("uimPush, state=%d, string=%d, strlen=%s", state, s ? strlen(s): 0, s ? s :0);
         
         int charcount=0;
         if (s) {
             NSString *ns=[NSString stringWithUTF8String:s];
             charcount=[ns length];
         }
-        if (ucountpos) upos+=charcount;
-        if (state==4) ucountpos=0;      // stop counting for cursor pos
-        murmur("upos=%d, ucountpos=%d", upos, ucountpos);
-        if (ovbuf) {
-            if (s) ovbuf->append(s);
-//            ovbuf->update(upos, uhighlightstart, uhighlightend);
+        if (state & UPreeditAttr_Reverse) {
+            if (!ovcandi->onScreen()) ovsrv->notify("漢字変換モード, ←/→ to move, ↑↓ for candidates");
+            uhighlightstart=upos;
+            uhighlightend=upos+charcount;
         }
+        if (ucountpos) upos+=charcount;
+        if (state & UPreeditAttr_Cursor) ucountpos=0;      // stop counting for cursor pos
+
+        murmur("last segment length=%d, new cursor position=%d, cursor counting=%s",
+            charcount, upos, ucountpos ? "CONTINUING" : "STOPPED");
+
+        if (s) ovbuf->append(s);
     }
     
     void uimUpdate() {
-        murmur("uimUpdate");
+        murmur("uimUpdate: updating composing (preedit buffer)");
         if (ovbuf) ovbuf->update(upos, uhighlightstart, uhighlightend);
         upos=uhighlightstart=uhighlightend=0;
         ucountpos=1;
     }
+    
     void uimCommit(const char *s) {
-        murmur("uimCommit, string=%s", s);
+        murmur("uimCommit, commit string=%s", s);
         if (ovbuf) ovbuf->clear()->append(s)->send();
     }
+    
+    void uimCandidateActivate(int nr, int display_limit) {
+        murmur("candidate activate, nr=%d, limit=%d", nr, display_limit);
+        
+        ovcandi->clear();
+        for (int i=0; i<nr; i++) {
+            uim_candidate c;
+            c=uim_get_candidate(uc, i, 0);  // the "acclerator hint" (0) doesn't seem to work
+            const char *str=uim_candidate_get_cand_str(c);
+            char buf[256];
+            sprintf(buf, "%d.\t%s", i, str);
+            ovcandi->append(buf);
+            if (i != nr-1) ovcandi->append("\n");
+            uim_candidate_free(c);
+        }
+        ovcandi->update()->show();
+        ovsrv->notify("candidate mode,  ↑↓ to move or ESC");
+    }
+    
+    void uimCandidateSelect(int index) {
+        murmur("candidate select! index=%d", index);
 
+        uim_candidate c;
+        c=uim_get_candidate(uc, index, 0);  // try replace 0
+        const char *str=uim_candidate_get_cand_str(c);
+
+        char buf[256];
+        sprintf(buf, "↑↓ candididate = %s (%d)", str, index);
+        ovsrv->notify(buf);
+
+        uim_candidate_free(c);
+    }
+    
+    void uimCandidateShiftPage(int direction) {
+        // this never seems to be called
+        murmur("uim candidate shift page! direction=%d", direction);
+        char buf[256];
+        sprintf(buf, "page shift, direction=%d", direction);
+        ovsrv->notify(buf);
+    }
+    
+    void uimCandidateDeactivate() {
+        murmur("uim candidate list deactivated");
+        ovcandi->hide()->clear()->update();
+    }
+    
 protected:
     OVBuffer *ovbuf;
     OVCandidate *ovcandi;
@@ -127,27 +193,16 @@ protected:
     int uhighlightend;
 };
 
+// UIM callback functions
 
-void OUPreeditClear(void *ptr) {
-    if (ptr) {
-        murmur("PTR not null! %x", ptr);
-        ((OVIMUIMContext*)ptr)->uimClear();
-    }
-    else murmur ("PTR null!");
-}
-
-void OUPreeditPush(void *ptr, int state, const char *s) {
-    ((OVIMUIMContext*)ptr)->uimPush(state, s);
-}
-
-void OUPreeditUpdate(void *ptr) {
-    ((OVIMUIMContext*)ptr)->uimUpdate(); 
-}
-
-void OUPreeditCommit(void *ptr, const char *s) {
-    ((OVIMUIMContext*)ptr)->uimCommit(s);
-}
-
+void OUPreeditClear(void *ptr) { if (ptr) ((OVIMUIMContext*)ptr)->uimClear(); }
+void OUPreeditPush(void *ptr, int state, const char *s) { if (ptr) ((OVIMUIMContext*)ptr)->uimPush(state, s); }
+void OUPreeditUpdate(void *ptr) { if (ptr) ((OVIMUIMContext*)ptr)->uimUpdate();  }
+void OUPreeditCommit(void *ptr, const char *s) { if (ptr) ((OVIMUIMContext*)ptr)->uimCommit(s); }
+void OUCandidateActivate(void *ptr, int nr, int display_limit) { if (ptr) ((OVIMUIMContext*)ptr)->uimCandidateActivate(nr, display_limit); }
+void OUCandidateSelect(void *ptr, int index) { if (ptr) ((OVIMUIMContext*)ptr)->uimCandidateSelect(index); }
+void OUCandidateShiftPage(void *ptr, int direction) { if (ptr) ((OVIMUIMContext*)ptr)->uimCandidateShiftPage(direction); }
+void OUCandidateDeactivate(void *ptr) { if (ptr) ((OVIMUIMContext*)ptr)->uimCandidateDeactivate(); }
 
 class OVIMUIM : public OVInputMethod
 {
@@ -155,21 +210,18 @@ public:
     virtual const char* identifier() { return "OVIMUIM"; }
     virtual OVInputMethodContext *newContext() {
         OVIMUIMContext *ovc=new OVIMUIMContext;
-        murmur("context addr=%x", ovc);
         uim_context uc;
 
         // create UIM-anthy
         murmur("uim context create");
         uc=uim_create_context(ovc, "UTF-8", "ja", "anthy", &enccvtr, OUPreeditCommit);
         if (uc) {
-            murmur("set context");
             ovc->setUIMContext(uc);
-            murmur("after set");
         }
         else return NULL;
         
         uim_set_preedit_cb(uc, OUPreeditClear, OUPreeditPush, OUPreeditUpdate);
-
+        uim_set_candidate_selector_cb(uc, OUCandidateActivate, OUCandidateSelect, OUCandidateShiftPage, OUCandidateDeactivate);
         // mode 1 is anthy's Hiragana mode
         uim_set_mode(uc, 1);
 
@@ -191,6 +243,7 @@ public:
         return 1;
     }
     virtual const char* localizedName(const char *locale) {
+        if (!strcasecmp(locale, "zh_TW")) return "日文輸入法 (UIM-anthy) - 片假名模式";
         return "UIM (anthy) - Hiragana";
     }
     
