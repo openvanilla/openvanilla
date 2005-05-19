a5f261a41c17206b8b2373d0aefa01522450f60e
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-05-19 07:26:00 +0000

    r280@dove:  lukhnos | 2005-05-19 15:24:54 +0800  Removed trunk/Modules/OVIMWordNet as it is too large

--- Modules/OVIMWordNet/killserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ps auxwww | grep Perl | grep OVSocketServer
-ps auxwww | grep Perl | grep OVSocketServer | perl -n -e 'split; print "kill $_[1]\n"'
-ps auxwww | grep Perl | grep OVSocketServer | perl -n -e 'split; system "kill $_[1]"'
