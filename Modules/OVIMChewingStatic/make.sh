61a7773fa17f15252867e196a8dcc2996bdc1043
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-05-27 20:15:12 +0000

    r433@dove:  lukhnos | 2005-05-28 04:14:00 +0800  OVIMChewingStatic makefile  To build, copy all the chewing data from libchewing 0.2.6  (/usr/local/share/chewing/* or /usr/share/chewing/*) to the  chewingdata under trunk/Modules/OVIMChewingStatic/chewingdata  This version builds a .bundle version

--- Modules/OVIMChewingStatic/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-gcc -O2 -I./ -I./libchewing -c libchewing/char.c libchewing/chewingio.c libchewing/chewingutil.c libchewing/choice.c libchewing/key2pho.c libchewing/dict.c libchewing/hanyupinying.c libchewing/hash.c libchewing/tree.c libchewing/userphrase.c libchewing/zuin.c
-
-g++ -O2 -I./ -I./libchewing/ -bundle -o OVIMChewingStatic.dylib  Chewingpp.cpp OVIMChewingStatic.cpp  *.o
-
-# sudo cp OVIMChewingStatic.dylib /Library/OpenVanilla/0.7.0/Modules/
