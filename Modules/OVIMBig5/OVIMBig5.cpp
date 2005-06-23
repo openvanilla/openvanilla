89c0c4ceb4f12f86626d2a628c548764bc04713c
zonble <zonble@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-06-23 12:09:02 +0000

    r66@zonble:  zonble | 2005-06-23 20:08:54 +0800  Porting OVIMBig5.

---
+++ Modules/OVIMBig5/OVIMBig5.cpp
@@ -0,0 +1,124 @@
+// OVIMBig5.cpp 2004 by Weizhong Yang
+// http://openvanilla.org
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <OpenVanilla/OpenVanilla.h>
+#include <OpenVanilla/OVLibrary.h>
+#include "OVIMBig5.h"
+
+unsigned int convert(unsigned int in){ //Convert Big5 to Unicode
+	int i;
+	for (i =0; i< 19582 ; i++){
+		if(map[i][0] == in)
+			return map[i][1];
+	}
+	return 0;
+}
+
+class OVIMBig5;
+
+const size_t ebMaxKeySeq=4; 
+class KeySeq {
+public:
+    KeySeq() {
+        clear();
+    }
+    void add(char c) {
+        if (len == ebMaxKeySeq) return;
+        buf[len++]=c;
+        buf[len]=0;
+    }
+    void remove() {
+        if (!len) return;
+        buf[--len]=0;
+    }
+    void clear() {
+        len=0; buf[0]=0;
+    }
+    char buf[ebMaxKeySeq];
+    size_t len;
+};
+
+class OVUnicodeContext : public OVInputMethodContext {
+public:
+    virtual void start(OVBuffer*, OVCandidate*, OVService*) {
+        clear();
+    }
+    virtual void clear() {
+	    keyseq.clear();
+    }
+	
+    virtual int keyEvent(OVKeyCode *key, OVBuffer *buf, OVCandidate *textbar,
+        OVService *srv)
+    {
+		if (key->isOpt() || key->isCommand() || key->isCtrl()) return 0;
+
+        if (key->code()==ovkReturn || key->code()==ovkSpace) {
+            if (!(strlen(keyseq.buf))) return 0;   // empty buffer, do nothing
+			//unsigned short i = strtol(keyseq.buf, (char **) NULL, 16);
+			unsigned short i = convert(strtol(keyseq.buf, (char **) NULL, 16));
+			
+			buf->clear()->append(srv->UTF16ToUTF8(&i, 1))->send()->clean();
+            textbar->clear()->hide();
+			keyseq.clear();
+            return 1;   // key processed
+        }
+
+		if (key->code()==ovkEsc) {
+			textbar->clear()->hide();
+			keyseq.clear();
+			return 1;
+		}
+		
+		if (key->code()==ovkDelete || key->code()==ovkBackspace) {
+			if(!strlen(keyseq.buf)) return 0;
+			keyseq.remove();
+			if(!strlen(keyseq.buf)) 
+				textbar->clear()->hide();
+			else
+				textbar->clear()->append((char *)"[Big5 Hex] ")->append(keyseq.buf)->update()->show();
+			buf->clear()->append(keyseq.buf)->update();
+			return 1;
+		}
+		
+		if(strlen(keyseq.buf) >= ebMaxKeySeq) return 1; // Max to 4 letters.
+		
+        if (isprint(key->code()))
+        {
+			if((key->code()>='a'&&key->code()<='f') || (key->code()>='0'&&key->code()<='9'))
+			{
+				keyseq.add(key->code());
+				buf->clear()->append(keyseq.buf)->update();
+				textbar->clear()->append((char *)"[Big5 Hex] ")->append(keyseq.buf)->update()->show();
+			}
+			return 1;   // key processed
+        }        
+        return 0;   // key not processed
+    }
+    
+protected:
+	KeySeq keyseq;
+};
+
+class OVIMBig5 : public OVInputMethod
+{
+public:
+    virtual const char* identifier() { return "OVIMBig5"; }
+    
+    virtual const char* localizedName(const char *locale) {
+        if (!strcasecmp(locale, "zh_TW"))
+            return "Big5 內碼";
+        else if (!strcasecmp(locale, "zh_CN"))
+            return "Big5 内码";
+        return "Big5 hexadecimal";        
+    }
+
+    virtual OVInputMethodContext* newContext()  {
+        return new OVUnicodeContext; 
+    }
+};
+
+OV_SINGLE_MODULE_WRAPPER(OVIMBig5);
