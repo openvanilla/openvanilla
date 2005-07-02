1e27f53f960d0bf961c1616a871923d123d92675
zonble <zonble@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-07-02 03:58:09 +0000

    r117@zonble:  zonble | 2005-07-02 11:56:58 +0800  Add a experimental Auto-Candidate function to the New Roman IM, however,  it's not finished and  it will crash your application somehow(again).

--- Modules/OVIMRomanNew/OVIMRomanNew.cpp
+++ Modules/OVIMRomanNew/OVIMRomanNew.cpp
@@ -8,6 +8,9 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include "OVSQLite3.h"
+
 
 int is_punc(char i){
 	if(i >= '!' && i <= '@'){
@@ -44,6 +47,64 @@ public:
     size_t len;
 };
 
+struct IMGCandidate
+{
+    IMGCandidate();
+    ~IMGCandidate();
+    unsigned int count;
+    char **candidates;
+};
+
+SQLite3 *db;
+IMGCandidate *candi;
+
+const char *QueryForCommand(SQLite3 *db, const char *command);
+const char *QueryForKey(SQLite3 *db, const char *tbl, const char *key);
+
+
+int showcandi(char* buf, OVCandidate* i) { //Show candidate list
+    char cmd[256];
+    char realqs[256];
+    sprintf(cmd, "select value from %s where key like ?1;", buf);
+    murmur("executing command=%s", cmd);
+
+    SQLite3Statement *sth=db->prepare(cmd);
+    if (!sth) return 0;
+    sth->bind_text(1, realqs);
+    int rows=0;
+    while (sth->step()==SQLITE_ROW) rows++;
+    murmur("query string=%s, number of candidates=%d", realqs, rows);
+    if (!rows) {
+        delete sth;
+        return 0;
+    }
+
+    candi->count=0;
+    candi->candidates=new char* [rows];
+    sth->reset();
+    while (sth->step()==SQLITE_ROW) {
+        const char *v=sth->column_text(0);
+        char *s=(char*)calloc(1, strlen(v)+1);
+        strcpy(s, v);
+        candi->candidates[candi->count++]=s;
+    }
+    delete sth;
+    
+    if (candi){
+        int candicount=candi->count;
+        char dispstr[32];
+        for (int j=0; j< 10; j++) {
+            if (j >= candicount) break;     // stop if idx exceeds candi counts
+            sprintf(dispstr, "%d.", j+1);
+            i->append(dispstr)->append(candi->candidates[j])->append("\n");
+        }
+        i->update();
+        if (!i->onScreen()) i->show();
+        return 1;
+    }
+    return 0;
+}
+
 
 class OVIMRomanNewContext : public OVInputMethodContext
 {
@@ -54,6 +115,19 @@ public:
     virtual void clear() {
 		keyseq.clear();
     } 
+    
+    virtual int initialize(OVDictionary* l, OVService* s, const char* modulePath) {
+		keyseq.clear();
+        db=new SQLite3;  // this never gets deleted, but so do we
+        char dbfile[128];
+        sprintf(dbfile, "%s/OVIM/dict.db", modulePath);
+        if (int err=db->open(dbfile)) {
+            murmur("SQLite3 error! code=%d", err);
+            return 0;
+        }
+        return 1;
+    }
+
     virtual int keyEvent(OVKeyCode* k, OVBuffer* b, OVCandidate* i, OVService* srv) {
         
         // we only send printable characters so that they can pass thru 
@@ -81,6 +155,9 @@ public:
 			char s[2];
 			sprintf(s, "%c", k->code());
 			keyseq.add(k->code());
+			if(keyseq.buf) {
+                showcandi(keyseq.buf, i);
+			}
 			b->clear()->append(keyseq.buf)->update();
 			return 1;
 		}
---
+++ Modules/OVIMRomanNew/cin-sqlconvert.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+use strict;
+
+die "USAGE: cin-sqlconvert file1 file2 ..." if (!@ARGV);
+
+for my $fn (@ARGV) {
+    open HNDL, "<$fn";
+    my @a=($fn=~/(\w+)\..+/);
+    my $tblname=lc $a[0];
+    
+    print "insert into tablelist values ('$tblname');\n";
+    print "create table $tblname (key);\n";
+    printf "create index %s_index_key on %s (key);\n", $tblname, $tblname;
+    print "begin;\n";
+    while(<HNDL>) {
+        chomp;
+        next if (/^# /);
+        next if (/^-/);
+        parse_dict($tblname);
+    }
+    print "commit;\n";
+}
+
+sub parse_dict {
+    my $tblname=shift;
+    my @a=split;
+    return if (!scalar(@a));
+    if ($a[0] =~ /^%.+/) { $a[0]=substr($a[0], 1, length($a[0])-1);  }
+    $a[0] =~ s/\'/\'\'/g;
+    if ($a[1]) { $a[1] =~ s/\'/\'\'/g; } else { $a[1]="" };
+    printf "insert into %s values ('%s\');\n", $tblname, lc $a[0];
+}
