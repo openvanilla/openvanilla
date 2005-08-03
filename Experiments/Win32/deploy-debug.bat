9fd66b00ddb1a5901019e5fb6e1f4bdc763bc240
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-08-03 17:03:09 +0000

    r1063@barabbas:  tmjiang | 2005-08-04 01:02:59 +0800  Adjust linker's library path setting for libiconv.

--- Experiments/Win32/deploy-debug.bat
+++ Experiments/Win32/deploy-debug.bat
@@ -1,4 +1,5 @@
 copy .\OVDummyLoader\libltdl3.dll %WINDIR%\system32\
+copy .\OVDummyLoader\libiconv-2.dll %WINDIR%\system32\
 copy .\OVIMEUI\Debug\OVIMEUI.dll %WINDIR%\system32\
 copy .\OVIME\Debug\ovime.ime %WINDIR%\system32\
 pause
--- Experiments/Win32/deploy-release.bat
+++ Experiments/Win32/deploy-release.bat
@@ -1,4 +1,5 @@
 copy .\OVDummyLoader\libltdl3.dll %WINDIR%\system32\
+copy .\OVDummyLoader\libiconv-2.dll %WINDIR%\system32\
 copy .\OVIMEUI\Release\OVIMEUI.dll %WINDIR%\system32\
 copy .\OVIME\Release\ovime.ime %WINDIR%\system32\
 pause
