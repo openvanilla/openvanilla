d403b609487647d12960c9489d322323178e2913
kanru <kanru@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-08-08 13:39:57 +0000

    Implement UTF16toUTF8()  /* copy from OVPhoneticLib.cpp */

--- Experiments/Win32/OVDummyLoader/OVDummyLoader.cpp
+++ Experiments/Win32/OVDummyLoader/OVDummyLoader.cpp
@@ -211,8 +211,42 @@ public:
 	    iconv_close(cd);
 	    return internal;
     }
-    virtual const char *UTF16ToUTF8(unsigned short *src, int len) {
-	    return "blabla";
+    /* copy from OVPhoneticLib.cpp: VPUTF16ToUTF8 */
+    virtual const char *UTF16ToUTF8(unsigned short *s, int l)
+    {
+	    char *b = internal;
+	    for (int i=0; i<l; i++)
+	    {
+		    if (s[i] < 0x80)
+		    {
+			    *b++=s[i];
+		    }
+		    else if (s[i] < 0x800)
+		    {
+			    *b++=(0xc0 | s[i]>>6);
+			    *b++=(0x80 | s[i] & 0x3f);
+		    }
+		    else if (s[i] < 0xd800 || s[i] > 0xdbff)
+		    {
+			    *b++ = (0xe0 | s[i]>>12);
+			    *b++ = (0x80 | s[i]>>6 & 0x3f);
+			    *b++ = (0x80 | s[i] & 0x3f);
+
+		    }
+		    else
+		    {
+			    unsigned int offset= 0x10000 - (0xd800 << 10) - 0xdc00;
+			    unsigned int codepoint=(s[i] << 10) + s[i+1]+offset;
+			    i++;
+			    *b++=(0xf0 | codepoint>>18);
+			    *b++=(0x80 | codepoint>>12 & 0x3f);
+			    *b++=(0x80 | codepoint>>6 & 0x3f);
+			    *b++=(0x80 | codepoint & 0x3F);
+		    }
+	    }
+
+	    *b=0;
+	    return internal;
     }
     virtual int UTF8ToUTF16(const char *src, unsigned short **rcvr) {
         return 0;
