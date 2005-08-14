1351cc462d063a07aa900afb85b7bf0449041e19
PCMan <PCMan@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-08-14 09:38:18 +0000

    Use new icon in the toolbar.

--- Experiments/Win32/OVIMEUI/PCMan/resource.h
+++ Experiments/Win32/OVIMEUI/PCMan/resource.h
@@ -2,8 +2,6 @@
 // Microsoft Visual C++ generated include file.
 // Used by PCMan.rc
 //
-//#define ID_STATUS_TOOLBAR               4
-//#define IDI_MAINICON                    6
 #define ID_IME_LIST_FIRST               20
 #define ID_IME_LIST_LAST                100
 #define ID_CHANGE_IME                   101
@@ -11,8 +9,8 @@
 #define ID_IME_HELP                     103
 #define ID_CHI_ENG                      106
 #define ID_FULL_HALF                    107
-//#define IDB_BITMAP1                     113
 #define IDB_STATUS_TB                   113
+#define IDR_RT_MANIFEST1                114
 #define ID_BUTTON40002                  40002
 #define ID_BUTTON40005                  40005
 #define ID_BUTTON40009                  40009
@@ -25,7 +23,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_RESOURCE_VALUE        115
 #define _APS_NEXT_COMMAND_VALUE         40015
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           108
