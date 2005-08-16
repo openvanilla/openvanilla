ff05b53025b7af82feaaa41f5f41bfc1a6f3fa64
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-08-16 08:48:10 +0000

    r15@go234:  tmjiang | 2005-08-16 16:51:42 +0800  Redirect source code references from Experiment\Win32\Modules\OVIMGeneric to Modules\OVIMGeneric

--- Experiments/Win32/Modules/OVIMGeneric/make.bat
+++ Experiments/Win32/Modules/OVIMGeneric/make.bat
@@ -1,3 +1,2 @@
-del *.*~
-cl /I.\ /I..\..\..\..\Headers /GR /EHsc /D OV_DEBUG /FeOVIMGeneric.DLL /D WIN32 /LD *.cpp ..\..\..\..\Source\*.cpp /link /DEF:..\..\OVModule.DEF
+cl /I.\ /I..\..\..\..\Headers /GR /EHsc /D OV_DEBUG /FeOVIMGeneric.DLL /D WIN32 /LD ..\..\..\..\Modules\OVIMGeneric\*.cpp ..\..\..\..\Source\*.cpp /link /DEF:..\..\OVModule.DEF
 pause
