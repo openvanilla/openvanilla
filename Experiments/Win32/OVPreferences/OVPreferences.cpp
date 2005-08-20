83a5e5856966a6689c0772c04f6482cae66ad511
PCMan <PCMan@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-08-20 14:10:10 +0000

    Add basic dialog-based GUI.

--- Experiments/Win32/OVPreferences/OVPreferences.cpp
+++ Experiments/Win32/OVPreferences/OVPreferences.cpp
@@ -1,16 +1,30 @@
 #include <wx/wx.h>
+#include <wx/snglinst.h>
+
+#include "ovprefdlg.h"
+
 
 class OVPreferencesApp : public wxApp
 {
 public:
-  virtual bool OnInit();
+	virtual bool OnInit();
 };
 
 IMPLEMENT_APP(OVPreferencesApp)
 
+static wxLocale g_Locale;
+static wxSingleInstanceChecker g_SingleInstChecker;
 bool OVPreferencesApp::OnInit()
 {
+	if( g_SingleInstChecker.Create("open_vanilla_settings:"+::wxGetUserId()) && g_SingleInstChecker.IsAnotherRunning() )
+		return false;
 
-  return true;
+	g_Locale.Init();
+	g_Locale.AddCatalog( "OVPreferences" );
+	OVPrefDlg* dlg = new OVPrefDlg(NULL, -1, _("Open Vanilla Preferences"));
+	dlg->ShowModal();
+	dlg->Destroy();
+
+	return false;
 }
 
--- Experiments/Win32/OVPreferences/resource.h
+++ Experiments/Win32/OVPreferences/resource.h
@@ -6,6 +6,7 @@
 #define IDR_WMDMLOGGER                  101
 #define IDS_LOG_SEV_INFO                201
 #define IDS_LOG_SEV_WARN                202
+#define IDB_BITMAP1                     202
 #define IDS_LOG_SEV_ERROR               203
 #define IDS_LOG_DATETIME                204
 #define IDS_LOG_SRCNAME                 205
@@ -19,7 +20,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_RESOURCE_VALUE        203
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
 #define _APS_NEXT_SYMED_VALUE           101
