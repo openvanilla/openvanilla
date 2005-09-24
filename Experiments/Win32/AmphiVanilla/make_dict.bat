53e606bb265d93ac389be9925b769b4c51afffda
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-09-24 19:10:36 +0000

    r1341@barabbas:  tmjiang | 2005-09-25 03:01:12 +0800  Remove useless "del *.*~" from some bat files.

--- Experiments/Win32/AmphiVanilla/make_dict.bat
+++ Experiments/Win32/AmphiVanilla/make_dict.bat
@@ -1,2 +1,1 @@
-del *.*~
 cl -EHsc -I..\..\..\Headers -I..\tinyxml -I.\ AVDictionary.cpp /DOV_DEBUG /DTEST_DICT /link ..\tinyxml\tinyxml.lib 
--- Experiments/Win32/AmphiVanilla/make_server.bat
+++ Experiments/Win32/AmphiVanilla/make_server.bat
@@ -1,2 +1,1 @@
-del *.*~
 cl /I.\ /I..\..\..\Headers -I..\tinyxml /GR /EHsc /MT /D OV_DEBUG /D WIN32 *.cpp ..\..\..\Source\*.cpp /link Ws2_32.lib SHFolder.lib  .\libltdl.lib .\libiconv.lib ..\tinyxml\tinyxml.lib /out:OVServer.exe /SUBSYSTEM:windows /entry:mainCRTStartup
