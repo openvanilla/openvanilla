b35fd596ceee1acd97759690a3f7a5f46ecef675
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-10-19 15:59:28 +0000

    r3236@b6s:  b6s | 2005-10-20 00:03:54 +0800  Add "$(SOURCES).cpp" into Makefile to fix the borken unit testing process.

--- Mk/ov.test.mk
+++ Mk/ov.test.mk
@@ -4,5 +4,5 @@ INC+= -I$(TESTER_HOME) -I./
 
 test:
 	$(TESTER_GEN) --error-printer -o runner.cpp $(TEST_CASES)
-	$(CPP) $(INC) -o runner runner.cpp
+	$(CPP) $(INC) -o runner runner.cpp $(SOURCES).cpp
 	./runner
