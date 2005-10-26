e92f1e9cab27b867dd88650ee4850d851963cc66
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-10-26 11:18:13 +0000

    1. Add a script "tobacco-sqlconvert_general.pl" that programmed by cls_bsd for converting general .cin files into SQLite3 database tables. The known bug of this script is that it will replace conflict keys of a word with the last one. 2. Modify the Makefile to cooperate with "tobacco-sqlconvert_general.pl."

---
+++ Modules/OVIMTobacco/tobacco-sqlconvert_general.pl
@@ -0,0 +1,127 @@
+#!/usr/bin/perl -w -CO
+use strict;
+use utf8;
+use Encode;
+
+my %char2wordHash;
+my %word2freqHash;
+my %word2charHash;
+my $fn_tsi;
+
+$fn_tsi = $ARGV[0] if defined($ARGV[0]);
+$fn_tsi = "tsi.src" unless defined($ARGV[0]);
+shift @ARGV;
+die "USAGE: $0 tsi.src 1.cin 2.cin ..." if (!@ARGV);
+print "begin;\n";
+
+# reading tsi.src
+open (HNDL, $fn_tsi) or die $!;
+while(<HNDL>) {
+    chomp;
+    if (/#?\s*(\S+)\s+(\d+)\s+(.+)/) {
+        my ($p, $f, $s)=(decode("big5-hkscs", $1), $2, decode("big5-hkscs", $3));
+
+        $word2freqHash{$p} = $f unless exists $word2freqHash{$p};
+	{
+	    my @chars = split (//,$p);
+	    $word2charHash{$p} = \@chars;
+	}
+    }
+}
+close HNDL;
+
+# reading xxx.cin
+for my $fn_cin (@ARGV) {
+    open (HNDL, "<:utf8",$fn_cin) or die $!;
+    my $table_prefix; # grep ename, for sqlite
+    my %keymap; # a for X, b for Y, .... z for Z...
+    my %charmap; # abc for XY..etc
+    my $keyname_sec = 0;
+    my $chardef_sec = 0;
+    while(<HNDL>) {
+	chomp;
+	if ($_ =~ m/^%ename\s+([^\s]+)/) {
+	    $table_prefix = $1;
+	    printf "CREATE TABLE %s_char2word_table(characters TEXT, wordID INTEGER);\n",
+		$table_prefix;
+	    printf "CREATE INDEX %s_index_characters ON %s_char2word_table(characters);\n",
+		$table_prefix, $table_prefix;
+	}
+	$keyname_sec = 1 if ($_ =~ m/^%keyname\s+begin/);
+	$keyname_sec = 0 if ($_ =~ m/^%keyname\s+end/);
+	$chardef_sec = 1 if ($_ =~ m/^%chardef\s+begin/);
+	$chardef_sec = 0 if ($_ =~ m/^%chardef\s+end/);
+	if ($keyname_sec and $_ =~ m/^(\S)\s+(\S+)/) {
+	    $keymap{$1} = $2;
+	}
+	if ($chardef_sec and $_ =~ m/^(\S+)\s+(\S+)/) {
+	    $charmap{$2} = $1; # NOTE: reverse...
+	}
+    }
+    close HNDL;
+    my $idCounter = 0;
+    my @words = keys(%word2freqHash);
+    @words = sort @words;
+    for(@words) {
+	my $word = $_;
+#	my $freq = $word2freqHash{$word};
+#	printf "insert into word_table values(%d, '%s') ON CONFLICT IGNORE;\n",
+#	    $idCounter, sprintf("%s", $word);
+#	printf "insert into generic_freq_table values(%d, %d) ON CONFLICT IGNORE;\n",
+#	    $idCounter, $freq;
+
+	my $currentCharsRef = $word2charHash{$word};
+	my $currentKeyCode = "";
+	for(@{$currentCharsRef}) {
+	    if (defined($charmap{$_})) {
+		my @currentCharCodes = split //,$charmap{$_};
+		for (@currentCharCodes) {
+		    $currentKeyCode .= $keymap{$_};
+		}
+		$currentKeyCode .= "\t";
+	    }
+	}
+	$currentKeyCode =~ s/\s*$//g;
+	printf "insert into %s_char2word_table values('%s', %d);\n",
+	    $table_prefix, sprintf("%s", $currentKeyCode), $idCounter;
+	    
+	$idCounter = $idCounter + 1;
+    }
+}
+
+print "commit;\n";
+
+# this makes the product of homophones (e.g. [A,B] x [C,D] x [E,F])
+sub produce {
+    my @s = split / /, shift;
+    my @p;    
+    my $x;
+    for $x (@s) {
+        if ($x =~ /\[(\S+)\]/) {
+            push @p, [split(/,/, $1)];
+        }
+        else {
+            push @p, [$x];
+        }
+    }
+    
+    my @r;
+    for $x (reverse @p) {
+        if (!@r) {
+            for my $y (@$x) {
+                push @r, $y;
+            }
+        }
+        else {
+            my @rr;
+            for my $y (@$x) {
+                for my $z (@r) {
+                    push @rr, "$y\t$z";
+                }
+            }
+            @r=@rr;
+        }
+    }
+    
+    @r;
+}
