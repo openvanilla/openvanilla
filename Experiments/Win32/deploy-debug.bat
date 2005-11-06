7c86d7f6830f99c1728122807ccc9ef4d006e731
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-11-06 20:49:25 +0000

    Modify project settings to make test of Tobacco easier.

--- Experiments/Win32/deploy-debug.bat
+++ Experiments/Win32/deploy-debug.bat
@@ -1,9 +1,10 @@
 copy .\AmphiVanilla\libltdl3.dll %WINDIR%\system32\
 copy .\AmphiVanilla\libiconv-2.dll %WINDIR%\system32\
 copy .\SQLite3\sqlite3.dll %WINDIR%\system32\
-copy .\tinyxml\tinyxml.dll %WINDIR%\system32\
+copy .\tinyxml\Debug\tinyxml.dll %WINDIR%\system32\
 copy .\OVIMEUI\Debug\OVIMEUI.dll %WINDIR%\system32\
 copy .\OVIME\Debug\ovime.ime %WINDIR%\system32\
 copy .\OVPreferences\Debug\OVPreferences.exe %WINDIR%\OpenVanilla\
-copy .\OVPreferences\Debug\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
+copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
+copy .\Modules\OVIMTobacco\Debug\OVIMTobacco.dll %WINDIR%\OpenVanilla\Modules\
 pause
