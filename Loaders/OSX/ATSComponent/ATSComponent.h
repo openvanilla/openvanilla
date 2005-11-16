952f28762853c359b617c09a66ad6c8bb4cd949c
vgod <vgod@da5ee8da-a019-0410-8a94-01093a00bd73>
2005-11-16 03:21:49 +0000

    r1593@mini:  vgod | 2005-11-16 02:27:06 +0800  modify ATSComponent.r for OSX i386  (This version works on osx86 ONLY)

--- Loaders/OSX/ATSComponent/ATSComponent.h
+++ Loaders/OSX/ATSComponent/ATSComponent.h
@@ -6,9 +6,10 @@
 #define __ATSComponent_h
 
 #define ATSCVERSION             0x00010000
-#define ATSCSCRIPT              2           // smTradCinese
-#define ATSCLANGUAGE            19          // langtradChinese
-#define ATSCBASERESOURCEID      16896       // (15872+script_id*512) here script_id=smTradChinese
+#define ATSCSCRIPT              0x7e          
+#define ATSCLANGUAGE            0        
+
+#define ATSCBASERESOURCEID	0xa600		// trying
 
 #define ATSCPENCILMENUID        ATSCBASERESOURCEID+1
 #define ATSCBUNDLENAME          "org.openvanilla.ovinit"
