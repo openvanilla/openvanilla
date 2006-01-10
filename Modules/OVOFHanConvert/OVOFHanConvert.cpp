a7efc0beae3d17a1bdf3c05fc5d8448a7efbed75
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-01-10 19:59:22 +0000

    r152@dove:  lukhnos | 2006-01-10 21:16:40 +0800  Module work:  * OVOFHanConvert: added license headers and an acknowledgement    (data table derived from Perl's Encode::HanConvert)

--- Modules/OVOFHanConvert/OVOFHanConvert.cpp
+++ Modules/OVOFHanConvert/OVOFHanConvert.cpp
@@ -1,13 +1,44 @@
 // OVOFHanConvert.cpp: Traditional<->Simplified Chinese Output Filter
+//
+// [FILENAME]: [DESCRIPTION]
+//
+// Copyright (c) 2004-2006 The OpenVanilla Project (http://openvanilla.org)
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of OpenVanilla nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef WIN32
-#include <OpenVanilla/OpenVanilla.h>  
-#include <OpenVanilla/OVLibrary.h>
+    #include <OpenVanilla/OpenVanilla.h>  
+    #include <OpenVanilla/OVLibrary.h>
 #else
-#include "OpenVanilla.h"
-#include "OVLibrary.h"
-#define strcasecmp stricmp
+    #include "OpenVanilla.h"
+    #include "OVLibrary.h"
+    #define strcasecmp stricmp
 #endif
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -52,7 +83,7 @@ class OVOFConvSC2TC : public OVOutputFilter
 public:
     OVOFConvSC2TC() { u16buf=NULL; }
     int initialize(OVDictionary *cfg, OVService *srv, const char *modpath) {
-        fprintf(stderr, "OVOFConvSC2TC being initialized, module path=%s\n", modpath);
+        // fprintf(stderr, "OVOFConvSC2TC being initialized, module path=%s\n", modpath);
         return 1;
     }
     const char *identifier() { return "OVOFConvSC2TC"; }
@@ -97,7 +128,7 @@ class OVOFConvTC2SC : public OVOutputFilter
 public:
     OVOFConvTC2SC() { u16buf=NULL; }
     int initialize(OVDictionary *cfg, OVService *srv, const char *modpath) {
-        fprintf(stderr, "OVOFConvTC2SC being initialized, module path=%s\n", modpath);
+        // fprintf(stderr, "OVOFConvTC2SC being initialized, module path=%s\n", modpath);
         return 1;
     }
     const char *identifier() { return "OVOFConvTC2SC"; }
