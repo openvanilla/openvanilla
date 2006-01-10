f4886c5cbda8efdf89f70984adb84d3105ae45d7
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-01-10 20:05:53 +0000

    r156@dove:  lukhnos | 2006-01-10 22:47:32 +0800  Module works:  * Moved OVKPPhrase to Experiments/  * OVIMArray license headers  * Moved array30.cin to SharedData/  * Moved and renamed ArrayShortCode.cin to SharedData/array-shortcode.cin  * Moved and renamed ArraySpecial.cin to SharedData/array-special.cin  * OVIMArray Makefile refactored

--- Modules/OVIMArray/OVIMArray.cpp
+++ Modules/OVIMArray/OVIMArray.cpp
@@ -1,9 +1,39 @@
+// OVIMArray.h: The Array Input Method
+//
+// Copyright (c) 2004-2006 The OpenVanilla Project (http://openvanilla.org)
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of OpenVanilla nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 #define OV_DEBUG
 #include "ctype.h"
-#include"OVIMArray.h"
-#include<OpenVanilla/OVLibrary.h>
-#include<OpenVanilla/OVUtility.h>
-#include<utility>
+#include "OVIMArray.h"
+#include <OpenVanilla/OVLibrary.h>
+#include <OpenVanilla/OVUtility.h>
+#include <utility>
 
 using namespace std;
 using namespace OV_Array;
@@ -295,15 +325,15 @@ void OVIMArrayContext::changeState(STATE s){
     state = s;  
 }
 
-int OVIMArray::initialize(OVDictionary *conf, OVService*, const char *path){
+int OVIMArray::initialize(OVDictionary *conf, OVService* s, const char *path) {
     char buf[PATH_MAX];
     char *cinfiles[] = { 
-        "%sOVIMArray/array30.cin",  
-        "%sOVIMArray/ArrayShortCode.cin",
-        "%sOVIMArray/ArraySpecial.cin"
+        "%sOVIMArray%sarray30.cin",  
+        "%sOVIMArray%sarray-shortcode.cin",
+        "%sOVIMArray%sarray-special.cin"
     };
     for(int i=0;i<3;i++){
-        sprintf(buf, cinfiles[i], path);
+        sprintf(buf, cinfiles[i], path, s->pathSeparator());
         murmur("OVIMArray: open cin %s", buf);
         tabs[i] = new OVCIN(buf); 
     }
