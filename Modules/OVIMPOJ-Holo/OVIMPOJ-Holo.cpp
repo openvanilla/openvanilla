6d30383ff3ba8a5cd76d428c56581e85bfee67a3
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-01-11 16:03:42 +0000

    r177@dove:  lukhnos | 2006-01-11 10:26:50 +0800  Module works:  * OVIMPOJ: Makefile, license header  * More .cin data reviews

--- Modules/OVIMPOJ-Holo/OVIMPOJ-Holo.cpp
+++ Modules/OVIMPOJ-Holo/OVIMPOJ-Holo.cpp
@@ -1,21 +1,44 @@
 // OVIMPOJHolo.cpp: prototype for POJ IM
+//
+// Copyright (c) 2004-2006 The OpenVanilla Project (http://openvanilla.org)
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of OpenVanilla nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-// Copyright (c) 2004 by Lukhnos D. Liu, Kang-ming Liu, and Pek-tiong Tan
-
-#define OV_DEBUG
+// #define OV_DEBUG
 
 #include "OVIMPOJ-Holo.h"
 #include <ctype.h>
 
 #ifdef WIN32
-	#define strcasecmp stricmp
-#endif
-
-#ifndef WIN32
-	#include <sys/syslimits.h>	//for PATH_MAX
-#else
 	#include <windows.h>
 	#define PATH_MAX MAX_PATH
+	#define strcasecmp stricmp
+#else
+	#include <sys/syslimits.h>	//for PATH_MAX
 #endif
 
 OV_SINGLE_MODULE_WRAPPER(OVIMPOJHolo);
@@ -30,17 +53,18 @@ const char* OVIMPOJHolo::identifier() {
 }
 
 const char* OVIMPOJHolo::localizedName(const char *cfge) {
-    if (!strcasecmp(cfge, "zh_TW")) return "河洛白話字漢羅 (POJ)";
-    if (!strcasecmp(cfge, "zh_CN")) return "河洛白話字汉罗 (POJ)";    
-    return "OV Holo Pe̍h-ōe-jī (POJ)";
+    if (!strcasecmp(cfge, "zh_TW")) return "Holo/台灣閩南語白話字（POJ）漢羅";
+    if (!strcasecmp(cfge, "zh_CN")) return "Holo/闽南语白话字（POJ）汉罗";    
+    return "Holo Pe̍h-ōe-jī (POJ)";
 }
 
 int OVIMPOJHolo::initialize(OVDictionary* cfg, OVService* s, const char* lp) {    
     // update configuration
     update(cfg, s); 
     
+    const char *pathsep=s->pathSeparator();
     char cinpath[PATH_MAX];
-    sprintf(cinpath, "%sOVIMPOJ-Holo/poj-holo.cin", lp);
+    sprintf(cinpath, "%s%sOVIMPOJ-Holo%spoj-holo.cin", lp, pathsep, pathsep);
     murmur ("OVIMPOJHolo: loading poj-holo.cin at %s", cinpath);
     cintab=new OVCIN(cinpath);        
 
