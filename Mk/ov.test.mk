7129e1f77261d55342808306b0505405efec63d5
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-01-16 16:24:41 +0000

    r220@dove:  lukhnos | 2006-01-17 00:24:06 +0800  Some final touches before the 0.7.2-alpha release

--- Mk/ov.test.mk
+++
@@ -1,8 +0,0 @@
-TESTER_GEN=$(TESTER_HOME)/cxxtestgen.pl
-TEST_CASES=./t/*.h
-INC+= -I$(TESTER_HOME) -I./
-
-test:
-	$(TESTER_GEN) --error-printer -o runner.cpp $(TEST_CASES)
-	$(CPP) $(INC) -o runner runner.cpp $(SOURCES).cpp
-	./runner
