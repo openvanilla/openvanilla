4ff263214ad5ab0ac608e55a144185b7790c93fd
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-01-16 13:55:19 +0000

    r203@dove:  lukhnos | 2006-01-16 19:59:51 +0800  Module work:  * OVOFDisUniHex: Support of surrogates

--- Modules/OVOFDisUniHex/OVOFDisUniHex.cpp
+++ Modules/OVOFDisUniHex/OVOFDisUniHex.cpp
@@ -1,21 +1,28 @@
+// OVOFDisUniHex.cpp: Decode Unicode
+// 2004-2006 By Weizhong Yang
+//
+// This program is Zonble-ware and adopts Zonble's License
+// -- "How could a person who doesn't even believe in law adopt any license?"
+
 #ifndef WIN32
-#include <OpenVanilla/OpenVanilla.h>
-#include <OpenVanilla/OVLibrary.h>
+    #include <OpenVanilla/OpenVanilla.h>
+    #include <OpenVanilla/OVLibrary.h>
 #else
-#include "OpenVanilla.h"
-#include "OVLibrary.h"
-#define strcasecmp stricmp
+    #include "OpenVanilla.h"
+    #include "OVLibrary.h"
+    #define strcasecmp stricmp
 #endif
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <string>
+
+using namespace std;
 
 class OVOFDisUniHex : public OVOutputFilter
 {
 public:
-    OVOFDisUniHex() {}
-    int initialize(OVDictionary *cfg, OVService *srv, const char *modpath) {
-        fprintf(stderr, "OVOFDisUniHex being initialized, module path=%s\n", modpath);
+    int initialize(OVDictionary*, OVService*, const char *) {
         return 1;
     }
     const char *identifier() { return "OVOFDisUniHex"; }
@@ -25,34 +32,38 @@ public:
 
 const char *OVOFDisUniHex::localizedName(const char *locale)
 {
-    if (!strcasecmp(locale, "zh_TW")) return "顯示 Unicode 字碼";
-    if (!strcasecmp(locale, "zh_CN")) return "显示 Unicode 字码";
-    return "Display Unicode Hex";
+    if (!strcasecmp(locale, "zh_TW")) return "顯示Unicode字碼";
+    if (!strcasecmp(locale, "zh_CN")) return "显示Unicode字码";
+    return "Unicode Decoder";
 }
 
 const char *OVOFDisUniHex::process(const char *src, OVService *srv)
 {
     unsigned short *u16p;
-    int l;
-    l=srv->UTF8ToUTF16(src, &u16p);
-    
-    if (l) {
-	char hex[1024], dec[1024], buf[1024], s[2048];
-	memset(hex, 0, 1024);
-	memset(dec, 0, 1024);
-	for(int j =0; j < l; j++){
-	   unsigned short x = u16p[j];
-	   const char *u8 = srv->UTF16ToUTF8(&(u16p[j]), 1);
-	   sprintf(buf, "%s=(U+%X) ", u8, x);
-	   strcat(hex, buf);
-	   sprintf(buf, "%s=(%d) ", u8, x);
-	   strcat(dec, buf);
+    int l=srv->UTF8ToUTF16(src, &u16p);
+    string display;
+
+	for(int j=0; j<l; j++) {
+	   char msg[512];
+	   const char *u8;
+	   unsigned short h=u16p[j], l=0;
+	   
+	   if (h >= 0xd800 && h <= 0xdc00) {
+	       l=u16p[++j];
+	       u8=srv->UTF16ToUTF8(u16p+j, 2);
+	       
+	       unsigned int u32=0xffff0000;
+	       sprintf(msg, "%s=U+%X (%d)=%X %X", u8, u32, u32, h, l);
+	   }
+	   else {
+	       u8=srv->UTF16ToUTF8(u16p+j, 1);
+	       sprintf(msg, "%s=U+%X (%d)", u8, h, h));
+	   }
+	   
+        if (j != l-1) strcat(msg, "\n");
+        display+=msg;
 	}
-	sprintf(s, "Hex: %s\nDec: %s", hex, dec);
-	srv->notify((char *)s);
-    }
-    //return src;
-    return srv->UTF16ToUTF8(u16p, l);
+	srv->notify(display.c_str());
 }
 
 OV_SINGLE_MODULE_WRAPPER(OVOFDisUniHex);
