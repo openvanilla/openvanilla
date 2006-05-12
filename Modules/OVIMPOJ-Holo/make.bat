b7f8eb12db1ee80b975363dc95e855ac47c6e38f
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-05-12 10:55:50 +0000

    Fix some out-of-dated paths for OVIMPOJ-Holo's make.bat.

--- Modules/OVIMPOJ-Holo/make.bat
+++ Modules/OVIMPOJ-Holo/make.bat
@@ -1,2 +1,2 @@
-cl /I..\..\Headers /I.\ /EHsc /GR /MT /D"WIN32" /FeOVIMPOJ-Holo.DLL /LD OVIMPOJ-Holo.cpp POJ-Holo.cpp ..\..\Source\*.cpp /link /DEF:..\..\Experiments\Win32\OVModule.def
+cl /I..\..\Framework\Headers /I..\SharedHeaders /I.\ /EHsc /GR /MT /D"WIN32" /FeOVIMPOJ-Holo.DLL /LD OVIMPOJ-Holo.cpp POJ-Holo.cpp ..\SharedSource\*.cpp /link /DEF:..\..\Experiments\Win32\OVModule.def
 pause
