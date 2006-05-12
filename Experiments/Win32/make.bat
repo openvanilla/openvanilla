2ba63f0b4a987c001c99544c6846cd3ff161775b
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-05-12 05:19:43 +0000

    Make the building process of win32 easier and notify this change in README.Win32.zh_HANT.UTF-8.txt.

--- Experiments/Win32/make.bat
+++ Experiments/Win32/make.bat
@@ -1,6 +1,18 @@
 call "c:\Program Files\Microsoft Visual Studio .NET 2003\Vc7\bin\vcvars32.bat"
-cd "C:\Documents and Settings\tmjiang.BARABBAS\My Documents\Visual Studio Projects\OpenVanilla\trunk\Experiments\Win32-nonsocket\OVIMEUI\PCMan"
+cd "tinyxml"
+call .\make.bat
+cd "..\OVIMEUI\PCMan"
 call .\make.bat
 copy OVIMEUI.* ..\..\OVIME\
 cd ..\..\
 call nmake
+cd "OVPreferences"
+call make
+cd ..\
+copy OVIME\OVIMEUI.DLL Result\
+copy OVPreferences\OVPreferences.exe Result\
+del Result\*.obj
+del Result\*.exp
+del Result\*.lib
+del Result\*.pdb
+del Result\*.res
