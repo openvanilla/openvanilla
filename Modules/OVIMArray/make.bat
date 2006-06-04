1c91568e3afe236129db9982ea34e041a4cfc6ea
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-06-04 06:10:29 +0000

    Remove module dir from Win32 loader.

--- Modules/OVIMArray/make.bat
+++
@@ -1,2 +0,0 @@
-cl /I.\ /I..\..\..\..\Framework\Headers /I..\..\..\..\Modules\SharedHeaders /GR /EHsc /MT /FeOVIMArray.DLL /D WIN32 /LD *.cpp ..\..\..\..\Modules\SharedSource\*.cpp /link /DEF:..\..\OVModule.DEF
-pause
