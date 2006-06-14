d593d9168980a99f72a2c2854c3b7b8fd03b8655
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-06-14 09:40:05 +0000

    Add "depoloy-dependencies.bat" and modify corresponding actions in "deploy-debug.bat" and "deploy-release.bat" for deployment's convenience.

---
+++ Loaders/Win32/deploy-dependencies.bat
@@ -0,0 +1,4 @@
+copy %OV_DEPS%\libltdl3.dll %WINDIR%\system32\
+copy %OV_DEPS%\libiconv-2.dll %WINDIR%\system32\
+copy %OV_DEPS%\sqlite3.dll %WINDIR%\system32\
+copy %OV_DEPS%\tinyxml.dll %WINDIR%\system32\
--- Loaders/Win32/deploy-release.bat
+++ Loaders/Win32/deploy-release.bat
@@ -1,11 +1,8 @@
-copy .\AmphiVanilla\libltdl3.dll %WINDIR%\system32\
-copy .\AmphiVanilla\libiconv-2.dll %WINDIR%\system32\
-copy .\SQLite3\sqlite3.dll %WINDIR%\system32\
-copy .\tinyxml\Release\tinyxml.dll %WINDIR%\system32\
+call .\deploy-dependencies.bat
 copy .\OVIMEUI\Release\OVIMEUI.dll %WINDIR%\system32\
 copy .\OVIME\Release\ovime.ime %WINDIR%\system32\
 copy .\OVPreferences\Release\OVPreferences.exe %WINDIR%\OpenVanilla\
 copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
-copy .\Modules\OVIMTobacco\Release\OVIMTobacco.dll %WINDIR%\OpenVanilla\Modules\
 copy .\CSharpFormLibrary\bin\Release\CSharpFormLibrary.dll %WINDIR%\OpenVanilla\
+copy ..\..\Modules\OVIMTobacco\Release\OVIMTobacco.dll %WINDIR%\OpenVanilla\Modules\
 pause
