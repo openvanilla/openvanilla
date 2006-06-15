c277b290573f5b9b4408e52a91e1df1e8c0ef21a
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-06-15 10:31:44 +0000

    Remove Managed C++ part of OVIMEUI.

--- deploy-debug.bat
+++ deploy-debug.bat
@@ -3,6 +3,5 @@ copy .\OVIMEUI\Debug\OVIMEUI.dll %WINDIR%\system32\
 copy .\OVIME\Debug\ovime.ime %WINDIR%\system32\
 copy .\OVPreferences\Debug\OVPreferences.exe %WINDIR%\OpenVanilla\
 copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
-copy .\CSharpFormLibrary\bin\Debug\CSharpFormLibrary.dll %WINDIR%\OpenVanilla\
 copy ..\..\Modules\OVIMTobacco\Debug\OVIMTobacco.dll %WINDIR%\OpenVanilla\Modules\
 pause
