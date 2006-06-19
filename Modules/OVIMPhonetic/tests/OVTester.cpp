5182d12f8313689e376b61f95967ef7fa1985732
gugod <gugod@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-06-19 18:20:15 +0000

    r1912@gugod:  gugod | 2006-06-20 02:16:39 +0800  Use libtap to write this test. Which is very straightforward.  Grab libtap from http://jc.ngo.org.uk/trac-bin/trac.cgi/wiki/LibTap.
     Please notice, on OSX, it failed to run properly because pthread lock 
     always locks the process. So before you compile libtap, put the following
     line into src/tap.c:
     
     #undef HAVE_LIBPTHREAD
     
     At line 47, so that the LOCK and UNLOCK macro does nothing.
     
     Don't know if other platform suffers from this issue.

--- Modules/OVIMPhonetic/tests/OVTester.cpp
+++ Modules/OVIMPhonetic/tests/OVTester.cpp
@@ -1,83 +1,48 @@
+#ifdef     __cplusplus
+extern   "C"   {
+#endif
 
 #include <stdio.h>
 #include <stdarg.h>
 #include <dlfcn.h>
 #include <unistd.h>
 #include <sys/stat.h>
+
 #include <OpenVanilla/OpenVanilla.h>
 #include <OpenVanilla/OVLibrary.h>
 
+// libtap required. Grab it from
+// http://jc.ngo.org.uk/trac-bin/trac.cgi/wiki/LibTap
+#include <tap.h>
+
 #define OV_DEBUG
+#undef HAVE_LIBPTHREAD
 
-static int tests = 0;
-
-static char *OVIMlib = "../OVIMPhonetic.dylib";
-
-inline static void report(const char* state, const char* format,...) {
-  va_list args;
-  fprintf (stdout, "%3d : %5s - ", tests++, state);
-  va_start (args, format);
-  vfprintf (stdout, format, args);
-  va_end (args);
-  fprintf (stdout, "\n");
-}
-
-static int test_loadlib (char *lib) {
+int
+main(void) {
   void *libh;
-  char *reportstr;
+  char *OVIMlib = "../OVIMPhonetic.dylib";
   _OVGetLibraryVersion_t *g;
   _OVInitializeLibrary_t *i;
   _OVGetModuleFromLibrary_t *m;
 
-  printf("==> loadlib tests start\n\n");
+  plan_tests(5);
 
-  libh = dlopen( lib , RTLD_LAZY);
+  ok ( libh = dlopen(OVIMlib, RTLD_LAZY), "OVIMPhonetic lib loaded");
 
-  reportstr = "load library %s";
-  if (!libh) {
-    report("FAIL", reportstr, OVIMlib );
-    return 0;
-  } else {
-    report("OK", reportstr, OVIMlib );
-  }
+  ok (g = (_OVGetLibraryVersion_t*)dlsym(libh, "OVGetLibraryVersion"),
+      "symbol OVGetLibraryVersion is there");
 
-  g = (_OVGetLibraryVersion_t*)dlsym(libh, "OVGetLibraryVersion");
-  i = (_OVInitializeLibrary_t*)dlsym(libh, "OVInitializeLibrary");
-  m = (_OVGetModuleFromLibrary_t*)dlsym(libh, "OVGetModuleFromLibrary");
+  ok (i = (_OVInitializeLibrary_t*)dlsym(libh, "OVInitializeLibrary"),
+      "symbol OVInitializeLibrary is there");
 
-  reportstr = "g,i,m symbols are ready.";
-  if (!g || !i || !m) {
-    report("FAIL", reportstr);
-    return 0;
-  } else {
-    report("OK", reportstr);
-  }
+  ok (m = (_OVGetModuleFromLibrary_t*)dlsym(libh, "OVGetModuleFromLibrary"),
+      "symbol OVGetModuleFromLibrary is there");
+  ok (g() == OV_VERSION, "Version is up-to-date");
 
-  reportstr = "loadlib(): Version up-to-date (%s)";
-  if (g() < OV_VERSION) {
-    report("FAIL", reportstr, lib);
-    return 0;
-  } else {
-    report("OK", reportstr, lib);
-  }
-
-  printf("\n\n==> loadlib tests end.\n");
-  return 1;
+  return exit_status();
 }
 
-
-int main(void) {
-
-  printf("\n"
-         "---------------\n"
-         "OVTester Starts\n"
-         "---------------\n\n");
-
-  test_loadlib(OVIMlib);
-
-  printf("\n\n"
-         "-------------\n"
-         "OVTester Ends\n"
-         "-------------\n");
+#ifdef     __cplusplus
 }
-
+#endif
