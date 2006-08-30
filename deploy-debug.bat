ced643056027bc06103bbec34689d612f0fbf0aa
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-08-30 02:43:10 +0000

    Add OVIMGeneric to OVIME.sln and deploying batch files for convenience of debugging.

--- deploy-debug.bat
+++ deploy-debug.bat
@@ -4,5 +4,5 @@ copy .\OVIMEUIManaged\Debug\OVIMEUI.dll %WINDIR%\system32\
 copy .\OVIME\Debug\ovime.ime %WINDIR%\system32\
 copy .\OVPreferences\Debug\OVPreferences.exe %WINDIR%\OpenVanilla\
 copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
-copy ..\..\Modules\OVIMTobacco\Debug\OVIMTobacco.dll %WINDIR%\OpenVanilla\Modules\
+copy ..\..\Modules\OVIMGeneric\Debug\OVIMGeneric.dll %WINDIR%\OpenVanilla\Modules\
 pause
--- deploy-release.bat
+++ deploy-release.bat
@@ -4,5 +4,5 @@ copy .\OVIMEUIManaged\Release\OVIMEUI.dll %WINDIR%\system32\
 copy .\OVIME\Release\ovime.ime %WINDIR%\system32\
 copy .\OVPreferences\Release\OVPreferences.exe %WINDIR%\OpenVanilla\
 copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
-copy ..\..\Modules\OVIMTobacco\Release\OVIMTobacco.dll %WINDIR%\OpenVanilla\Modules\
+copy ..\..\Modules\OVIMGeneric\Release\OVIMGeneric.dll %WINDIR%\OpenVanilla\Modules\
 pause
