9f2fb7d78f608c1e445b1613579544c1dc9909ee
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-10-05 07:30:09 +0000

    Jaimie: update C# composition component

---
+++ CSharpFormLibrary/IMECompForm.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace CSharpFormLibrary
+{
+	/// <summary>
+	/// Summary description for IMECompForm.
+	/// </summary>
+	public class IMECompForm : System.Windows.Forms.Form
+	{
+		private System.Windows.Forms.TextBox textBox1;
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public IMECompForm()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+
+			//
+			// TODO: Add any constructor code after InitializeComponent call
+			//
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(0, 0);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(200, 22);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "Comp";
+			// 
+			// IMECompForm
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 15);
+			this.ClientSize = new System.Drawing.Size(200, 24);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "IMECompForm";
+			this.ShowInTaskbar = false;
+			this.Text = "IMECompForm";
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+		public void ShowNoActive()
+		{
+			UtilFuncs.SetVisibleNoActivate(this, true); // true to show. 
+		}
+		public void HideNoActive()
+		{
+			UtilFuncs.SetVisibleNoActivate(this, false); // false to hide.  
+		}
+		public void ClearComp()
+		{
+			this.textBox1.Clear();
+		}
+		public void SetLocation(int x, int y)
+		{
+			this.Location = new Point(x,y);
+		}
+		public void SetComp(string inputs)
+		{
+			//string[] a_inputs = inputs.Split(' ');
+			if(inputs==null) return;
+			this.textBox1.Text = inputs;
+		}
+	}
+}
