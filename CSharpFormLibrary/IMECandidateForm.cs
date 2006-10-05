748dd316cb3f5111827e3d1b81533ea8a2a9aedc
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-10-05 06:55:03 +0000

    Jaimie: update C# composition component - extract UtilsFuncs to UtilFuncs.cs

--- CSharpFormLibrary/IMECandidateForm.cs
+++ CSharpFormLibrary/IMECandidateForm.cs
@@ -216,38 +216,4 @@ namespace CSharpFormLibrary
 	}
 }
 
-class UtilFuncs 
-{ 
-	[ DllImport( "user32.dll" ) ] 
-	extern public static bool SetWindowPos( IntPtr hWnd, 
-		IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags );
- 
-	public const int HWND_TOPMOST = -1; // 0xffff 
-	public const int SWP_NOSIZE = 1; // 0x0001 
-	public const int SWP_NOMOVE = 2; // 0x0002 
-	public const int SWP_NOACTIVATE = 16; // 0x0010 
-	public const int SWP_SHOWWINDOW = 64; // 0x0040 
-  
-	public static void ShowWindowTopMost(IntPtr handle) 
-	{ 
-		SetWindowPos( handle, (IntPtr) HWND_TOPMOST, 0, 0, 0, 0, 
-			SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE | SWP_SHOWWINDOW ); 
-	}
- 
-	public static void SetVisibleNoActivate(Form form, bool visible) 
-	{ 
-		/*if ( visible) 
-		{
-			if(!form.Visible)
-			{				
-				ShowWindowTopMost(form.Handle);
-				form.Visible = visible;
-			}
-		}
-		else
-			form.Visible = visible;*/
-		if ( visible) 
-			ShowWindowTopMost(form.Handle);
-		form.Visible = visible;		
-	} 
-}
+
