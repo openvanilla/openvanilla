695f15184820a44964376590b7944cbb3a18c235
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-11-22 08:37:08 +0000

    1. Test MessageFilter but useless. 2. Test more different way to stop message propagation but failed. 3. Revise the UI look-n-feel.

--- Loaders/Win32/CSharpFormLibrary/IMEStatusForm.cs
+++ Loaders/Win32/CSharpFormLibrary/IMEStatusForm.cs
@@ -7,188 +7,33 @@ using System.Runtime.InteropServices;
 
 namespace CSharpFormLibrary
 {
-    public enum WindowsMessage : int
-    {
-        WM_NULL =           0x0000,
-        WM_CREATE =         0x0001,
-        WM_DESTROY =        0x0002,
-        WM_MOVE =           0x0003,
-        WM_SIZE =           0x0005,
-        WM_ACTIVATE =       0x0006,
-        WM_SETFOCUS =       0x0007,
-        WM_KILLFOCUS =      0x0008,
-        WM_ENABLE =         0x000A,
-        WM_SETREDRAW =      0x000B,
-        WM_SETTEXT =        0x000C,
-        WM_GETTEXT =        0x000D,
-        WM_GETTEXTLENGTH =  0x000E,
-        WM_PAINT =          0x000F,
-        WM_CLOSE =          0x0010,
-
-        WM_ERASEBKGND =     0x0014,
-
-        WM_SHOWWINDOW =     0x0018,
-
-        WM_SETCURSOR =      0x0020,
-        WM_MOUSEACTIVATE =  0x0021,
-
-        WM_WINDOWPOSCHANGING =  0x0046,
-        WM_WINDOWPOSCHANGED =   0x0047,
-
-        WM_NCCREATE =       0x0081,
-        WM_NCDESTROY =      0x0082,
-        WM_NCCALCSIZE =     0x0083,
-        WM_NCHITTEST =      0x0084,
-        WM_NCPAINT =        0x0085,
-        WM_NCACTIVATE =     0x0086,
-
-        WM_MENUSELECT =     0x011F,
-
-        WM_ENTERIDLE =          0x0121,
-        WM_MENURBUTTONUP =      0x0122,
-        WM_MENUDRAG =           0x0123,
-        WM_MENUGETOBJECT =      0x0124,
-        WM_UNINITMENUPOPUP =    0x0125,
-        WM_MENUCOMMAND =        0x0126,
-        WM_CHANGEUISTATE =      0x0127,
-        WM_UPDATEUISTATE =      0x0128,
-
-        WM_MOUSEMOVE =      0x0200,
-        WM_LBUTTONDOWN =    0x0201,
-        WM_LBUTTONUP =      0x0202,
-        WM_LBUTTONDBLCLK =  0x0203,
-        WM_RBUTTONDOWN =    0x0204,
-        WM_RBUTTONUP =      0x0205,
-        WM_RBUTTONDBLCLK =  0x0206,
-
-        WM_EXITMENULOOP =   0x0212,
-
-        WM_CAPTURECHANGED = 0x0215,
-
-        WM_IME_SETCONTEXT = 0x0281,
-        WM_IME_NOTIFY =     0x0282,
-
-        WM_MOUSEHOVER =     0x02A1,
-        WM_MOUSELEAVE =     0x02A3
-    };
-
-    public class IMEButton : System.Windows.Forms.Button
-    {
-        private const int WM_MOUSEACTIVATE = 0x0021;
-        private const int MA_ACTIVATE = 1;
-        private const int MA_ACTIVATEANDEAT = 2;
-        private const int MA_NOACTIVATE = 0x0003;
-        private const int MA_NOACTIVATEANDEAT = 0x0004;
-        private const int WM_SETFOCUS = 0x0007;
-        private const int WM_KILLFOCUS = 0x0008;
-        private const int WM_LBUTTONDOWN = 0x0201;
-        private const int WM_LBUTTONUP = 0x0202;
-        private const int WM_LBUTTONDBLCLK = 0x0203;
-        private const int WM_RBUTTONDOWN = 0x0204;
-        private const int WM_RBUTTONUP = 0x0205;
-        private const int WM_RBUTTONDBLCLK = 0x0206;
-
-        private static int msgCounter = 0;
-        private UInt64 m_AppHWnd;
-
-        public void DoPressEffect()
-        {
-            Color tempForeColor = this.ForeColor;
-            this.ForeColor = this.BackColor;
-            this.BackColor = tempForeColor;
-        }
-
-        public IMEButton() : base()
-        {
-            //this.SetStyle(ControlStyles.StandardClick, false);
-            //this.SetStyle(ControlStyles.EnableNotifyMessage, false);
-            //this.SetStyle(ControlStyles.UserMouse, false);
-        }
-
-        public UInt64 AppHWnd
-        {
-            get { return m_AppHWnd; }
-            set { m_AppHWnd = value; }
-        }
-
-        protected override void OnMouseUp(MouseEventArgs mevent)
-        {
-            base.OnMouseUp(mevent);
-            DoPressEffect();
-        }
-
-        protected override void WndProc(ref Message m)
-        {
-            msgCounter++;
-            System.Diagnostics.Debug.WriteLine(
-                "Button[" + msgCounter + "]: Msg->\t" +
-                Enum.GetName(typeof(WindowsMessage), m.Msg) +
-                "(0x" + m.Msg.ToString("X") + ")");
-
-            //if (m.Msg == WM_MOUSEACTIVATE)
-            if (m.Msg == WM_LBUTTONDOWN)
-            {
-                System.Diagnostics.Debug.WriteLine("==================");
-                System.Diagnostics.Debug.WriteLine("WM_MOUSEACTIVATE");
-                System.Diagnostics.Debug.WriteLine(
-                    "Button[" + msgCounter + "]: this.Handle->\t0x" + this.Handle.ToString("X"));
-                System.Diagnostics.Debug.WriteLine(
-                    "Button[" + msgCounter + "]: Parent.Handle->\t0x" + this.Parent.Handle.ToString("X"));
-                System.Diagnostics.Debug.WriteLine(
-                    "Button[" + msgCounter + "]: App.Handle->\t0x" + m_AppHWnd.ToString("X"));
-                System.Diagnostics.Debug.WriteLine(
-                    "Button[" + msgCounter + "]: m.HWnd->\t0x" + m.HWnd.ToString("X"));
-                System.Diagnostics.Debug.WriteLine(
-                    "Button[" + msgCounter + "]: LParam->\t0x" + m.LParam.ToString("X"));
-
-                System.Diagnostics.Debug.WriteLine(
-                    "Button[" + msgCounter + "]: WParam (before)->\t0x" + m.WParam.ToString("X"));
-                m.WParam = this.Handle;
-                //m.WParam = new IntPtr((long)m_AppHWnd);
-                //m.WParam = IntPtr.Zero;
-                System.Diagnostics.Debug.WriteLine(
-                    "Button[" + msgCounter + "]: WParam (after)->\t0x" + m.WParam.ToString("X"));
-
-                m.Result = (IntPtr)MA_NOACTIVATEANDEAT;
-                //m.Result = (IntPtr)MA_NOACTIVATE;
-
-                System.Diagnostics.Debug.WriteLine(
-                    "Button[" + msgCounter + "]: Result->\t0x" + m.Result.ToString("X"));
-
-                DoPressEffect();
-            } else            
-                base.WndProc(ref m);
-        }
-    }
-
 	/// <summary>
 	/// Summary description for IMEStatusForm.
 	/// </summary>
 	public class IMEStatusForm : System.Windows.Forms.Form
 	{
-		private IMEButton button1;
-		private IMEButton button2;
-		private IMEButton button3;
-		private IMEButton button6;
-		private System.Windows.Forms.ContextMenu contextMenu1;
-		private System.Windows.Forms.MenuItem menuItem1;
-		private System.Windows.Forms.MenuItem menuItem2;
-		private System.Windows.Forms.MenuItem menuItem3;
+        private const int MA_ACTIVATE = 1;
+        private const int MA_ACTIVATEANDEAT = 2;
+        private const int MA_NOACTIVATE = 0x0003;
+        private const int MA_NOACTIVATEANDEAT = 0x0004;
 
-		int currentMenu = 0;
+        private int msgCounter = 0;
+        private UInt64 m_AppHWnd;
+
+		private int currentMenu = 0;
 
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
 		private System.ComponentModel.Container components = null;
-		private const int WM_MOUSEACTIVATE = 0x0021;
-		private const int MA_ACTIVATE=1;		
-		private const int MA_ACTIVATEANDEAT = 2;
-		private const int MA_NOACTIVATE=0x0003;		
-		private const int MA_NOACTIVATEANDEAT = 0x0004;
-
-        private int msgCounter = 0;
-        private UInt64 m_AppHWnd;
+        private IMEButton button1;
+        private IMEButton button2;
+        private IMEButton button3;
+        private IMEButton button6;
+        private System.Windows.Forms.ContextMenu contextMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem3;
 
 		public IMEStatusForm()
 		{
@@ -196,29 +41,64 @@ namespace CSharpFormLibrary
 			// Required for Windows Form Designer support
 			//
 			InitializeComponent();
-			this.SetStyle(ControlStyles.StandardClick,false);
-			this.SetStyle(ControlStyles.EnableNotifyMessage,false);
-			this.SetStyle(ControlStyles.UserMouse,false);
+			this.SetStyle(ControlStyles.StandardClick, false);
+			this.SetStyle(ControlStyles.EnableNotifyMessage, false);
+			this.SetStyle(ControlStyles.UserMouse, false);
 			//
 			// TODO: Add any constructor code after InitializeComponent call
-			//            
-		}
+			//
+            //Application.AddMessageFilter(new MessageFilter());
+        }
+
+        #region IME-specific overridden methods
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                System.Diagnostics.Debug.WriteLine(
+                    "Form: CreateParams.ExStyle (before) =" + cp.ExStyle);
+                cp.ExStyle = 0x00000004; //WS_EX_NOPARENTNOTIFY
+                System.Diagnostics.Debug.WriteLine(
+                    "Form: CreateParams.ExStyle (after) =" + cp.ExStyle);
+                return cp;
+            }
+        }
 
 		protected override void WndProc(ref Message m) 
 		{
             msgCounter++;
             System.Diagnostics.Debug.WriteLine(
-                "Form[" + msgCounter + "]: Msg->\t" +
-                Enum.GetName(typeof(WindowsMessage), m.Msg) +
+                "Form[" + msgCounter + "]: (before base) Msg->\t" +
+                Enum.GetName(typeof(UtilFuncs.WindowsMessage), m.Msg) +
                 "(0x" + m.Msg.ToString("X") + ")");
 
-            if (m.Msg == WM_MOUSEACTIVATE)// && m.Result==(IntPtr) MA_ACTIVATE) 
-            {
-                //m.Result = (IntPtr)MA_NOACTIVATEANDEAT;				
+            base.WndProc(ref m);
 
-                m.Result = (IntPtr)MA_NOACTIVATE;
+            msgCounter++;
+            System.Diagnostics.Debug.WriteLine(
+                "Form[" + msgCounter + "]: (after base) Msg->\t" +
+                Enum.GetName(typeof(UtilFuncs.WindowsMessage), m.Msg) +
+                "(0x" + m.Msg.ToString("X") + ")");
+            if (m.Msg == (Int32)UtilFuncs.WindowsMessage.WM_PARENTNOTIFY)
+            {
+                System.Diagnostics.Debug.WriteLine("== Damn WM_PARENTNOTIFY! ==");
             }
-            else if (m.Msg == (Int32)WindowsMessage.WM_NCACTIVATE)
+            else if (m.Msg == (Int32)UtilFuncs.WindowsMessage.WM_MOUSEACTIVATE)// && m.Result==(IntPtr) MA_ACTIVATE) 
+            {
+                System.Diagnostics.Debug.WriteLine("== Damn WM_MOUSEACTIVATE! ==");
+
+                System.Diagnostics.Debug.WriteLine(
+                    "Form[" + msgCounter + "]: Result (before)->\t0x" + m.Result.ToString("X"));
+
+                //m.Result = (IntPtr)MA_NOACTIVATEANDEAT;
+                m.Result = (IntPtr)MA_NOACTIVATE;
+
+                System.Diagnostics.Debug.WriteLine(
+                    "Form[" + msgCounter + "]: Result (after)->\t0x" + m.Result.ToString("X"));
+                
+            }
+            else if (m.Msg == (Int32)UtilFuncs.WindowsMessage.WM_NCACTIVATE)
             {
                 System.Diagnostics.Debug.WriteLine("== Damn WM_NCACTIVATE! ==");
                 /*
@@ -238,10 +118,10 @@ namespace CSharpFormLibrary
                     "Form[" + msgCounter + "]: Result (after)->\t0x" + m.Result.ToString("X"));
                 */
             }
-            else if (m.Msg == (Int32)WindowsMessage.WM_ACTIVATE)
+            else if (m.Msg == (Int32)UtilFuncs.WindowsMessage.WM_ACTIVATE)
             {
                 System.Diagnostics.Debug.WriteLine("== Damn WM_ACTIVATE! ==");
-                ShowNoActive();
+                //ShowNoActive();
                 /*
                 System.Diagnostics.Debug.WriteLine(
                     "Form[" + msgCounter + "]: LParam (before)->\t0x" + m.LParam.ToString("X"));
@@ -262,11 +142,12 @@ namespace CSharpFormLibrary
                     "Form[" + msgCounter + "]: Result (after)->\t0x" + m.Result.ToString("X"));
                 */
             }
-            else
-                base.WndProc(ref m);
-		}
+            //else
+                //base.WndProc(ref m);
+        }
+        #endregion
 
-		/// <summary>
+        /// <summary>
 		/// Clean up any resources being used.
 		/// </summary>
 		protected override void Dispose( bool disposing )
@@ -302,7 +183,7 @@ namespace CSharpFormLibrary
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Desktop;
             //this.button1.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
             this.button1.Location = new System.Drawing.Point(4, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 21);
@@ -312,18 +193,15 @@ namespace CSharpFormLibrary
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.Control;
-            //this.button2.CausesValidation = false;
+            this.button2.BackColor = System.Drawing.SystemColors.Desktop;
             this.button2.ContextMenu = this.contextMenu1;
             //this.button2.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
             this.button2.Location = new System.Drawing.Point(84, 4);
             this.button2.Name = "button2";
-            //this.button2.Size = new System.Drawing.Size(40, 21);
+            this.button2.Size = new System.Drawing.Size(40, 21);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
-            //this.button2.AutoSize = true;
-            //this.button2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             //this.button2.UseVisualStyleBackColor = false;
             this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
             this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
@@ -354,7 +232,7 @@ namespace CSharpFormLibrary
             // 
             this.button3.BackColor = System.Drawing.SystemColors.Desktop;
             //this.button3.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
             this.button3.Location = new System.Drawing.Point(124, 4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(40, 21);
@@ -366,7 +244,7 @@ namespace CSharpFormLibrary
             // 
             this.button6.BackColor = System.Drawing.SystemColors.Desktop;
             //this.button6.Font = new System.Drawing.Font("PMingLiU", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button6.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button6.ForeColor = System.Drawing.Color.Transparent;
             this.button6.Location = new System.Drawing.Point(164, 4);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(40, 21);
@@ -396,6 +274,7 @@ namespace CSharpFormLibrary
 		}
 		#endregion
 
+        #region public methods
         public void SetAppHWnd(UInt64 HWND)
         {
             m_AppHWnd = HWND;
@@ -421,24 +300,18 @@ namespace CSharpFormLibrary
 
 		public void SetChiEng(bool isChinese)
 		{
-			if(isChinese)
-			{
-				this.button3.Text = "中";
-			}else
-				this.button3.Text = "英";
+			if(isChinese)   this.button3.Text = "中";
+			else            this.button3.Text = "英";
 		}
+
 		public void SetModString(string inputs)
 		{
 			if(inputs == null) return;
             
 			if(inputs.Length >10)
-			{
 				this.button2.Width = (inputs.Length)*10;
-			}
 			else
-			{
 				this.button2.Width = (inputs.Length)*2*10;
-			}
             
             this.button3.Left = this.button2.Right;
 			this.button6.Left = this.button3.Right;
@@ -461,10 +334,13 @@ namespace CSharpFormLibrary
 				this.menuItem2.Text = inputs;
 			if(currentMenu == 2)
 				this.menuItem3.Text = inputs;			
-			currentMenu++;			
-		}
+			currentMenu++;
+        }
+        #endregion
 
-		private Point mouseOffset;
+        #region private memebers
+
+        private Point mouseOffset;
 		private bool isMouseDown = false;
 
 		private void IMEStatusForm_MouseDown(object sender,System.Windows.Forms.MouseEventArgs e)
@@ -479,6 +355,7 @@ namespace CSharpFormLibrary
 				isMouseDown = true;
 			}
 		}
+
 		private void IMEStatusForm_MouseMove(object sender,
 			System.Windows.Forms.MouseEventArgs e)
 		{
@@ -512,17 +389,24 @@ namespace CSharpFormLibrary
             this.button2.ContextMenu.Show(button2, new System.Drawing.Point(0, button2.Bottom));
             System.Diagnostics.Debug.WriteLine("Context Menu Shown!");
         }
-	}
+        #endregion
+    }
 
+    #region Deprecated MessageFilter implementation
     /*
     public class MessageFilter : IMessageFilter
     {
         public bool PreFilterMessage(ref Message objMessage)
         {
-            if (objMessage.Msg == Convert.ToInt32(WindowsMessage.WM_MOUSEACTIVATE))
+            System.Diagnostics.Debug.WriteLine("PreFilterMessage");
+            if (objMessage.Msg == Convert.ToInt32(UtilFuncs.WindowsMessages.WM_MOUSEACTIVATE))
+            {
+                System.Diagnostics.Debug.WriteLine("WM_MOUSEACTIVATE");
                 return true;
+            }
             return false;
         }
     }
     */
+    #endregion
 }
--- Loaders/Win32/CSharpFormLibrary/UtilFuncs.cs
+++ Loaders/Win32/CSharpFormLibrary/UtilFuncs.cs
@@ -37,21 +37,86 @@ namespace CSharpFormLibrary
 			form.Visible = visible;
 		} 
 
-		public const uint WM_IME_SETCONTEXT	=	0x281;
 		public const ulong HWND_BROADCAST = 0xffff ;
-		
-			[DllImport("user32.dll")]
-			public static extern int SendMessage(
-				System.UInt64 hWnd,      // handle to destination window
-				uint Msg,       // message
-				long wParam,  // first message parameter
-				long lParam   // second message parameter
-				);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(
+            System.UInt64 hWnd,      // handle to destination window
+            uint Msg,       // message
+            long wParam,  // first message parameter
+            long lParam   // second message parameter
+            );
 
 		public static void GiveBackFocus(System.UInt64 hwnd)
 		{			
-			//long lngResult = SendMessage(hwnd, WM_IME_SETCONTEXT,1L, 0L ); 
-			long lngResult = SendMessage(hwnd, WM_IME_SETCONTEXT,1L, 0L ); 
+			long lngResult = SendMessage(hwnd, (Int32)WindowsMessage.WM_IME_SETCONTEXT, 1L, 0L); 
 		}
+
+        public enum WindowsMessage : int
+        {
+            WM_NULL = 0x0000,
+            WM_CREATE = 0x0001,
+            WM_DESTROY = 0x0002,
+            WM_MOVE = 0x0003,
+            WM_SIZE = 0x0005,
+            WM_ACTIVATE = 0x0006,
+            WM_SETFOCUS = 0x0007,
+            WM_KILLFOCUS = 0x0008,
+            WM_ENABLE = 0x000A,
+            WM_SETREDRAW = 0x000B,
+            WM_SETTEXT = 0x000C,
+            WM_GETTEXT = 0x000D,
+            WM_GETTEXTLENGTH = 0x000E,
+            WM_PAINT = 0x000F,
+            WM_CLOSE = 0x0010,
+
+            WM_ERASEBKGND = 0x0014,
+
+            WM_SHOWWINDOW = 0x0018,
+
+            WM_SETCURSOR = 0x0020,
+            WM_MOUSEACTIVATE = 0x0021,
+
+            WM_WINDOWPOSCHANGING = 0x0046,
+            WM_WINDOWPOSCHANGED = 0x0047,
+
+            WM_NCCREATE = 0x0081,
+            WM_NCDESTROY = 0x0082,
+            WM_NCCALCSIZE = 0x0083,
+            WM_NCHITTEST = 0x0084,
+            WM_NCPAINT = 0x0085,
+            WM_NCACTIVATE = 0x0086,
+
+            WM_MENUSELECT = 0x011F,
+
+            WM_ENTERIDLE = 0x0121,
+            WM_MENURBUTTONUP = 0x0122,
+            WM_MENUDRAG = 0x0123,
+            WM_MENUGETOBJECT = 0x0124,
+            WM_UNINITMENUPOPUP = 0x0125,
+            WM_MENUCOMMAND = 0x0126,
+            WM_CHANGEUISTATE = 0x0127,
+            WM_UPDATEUISTATE = 0x0128,
+
+            WM_MOUSEMOVE = 0x0200,
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_LBUTTONDBLCLK = 0x0203,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205,
+            WM_RBUTTONDBLCLK = 0x0206,
+
+            WM_PARENTNOTIFY = 0x0210,
+
+            WM_EXITMENULOOP = 0x0212,
+
+            WM_CAPTURECHANGED = 0x0215,
+
+            WM_IME_SETCONTEXT = 0x0281,
+            WM_IME_NOTIFY = 0x0282,
+
+            WM_MOUSEHOVER = 0x02A1,
+            WM_MOUSELEAVE = 0x02A3
+        }
 	}
 }
