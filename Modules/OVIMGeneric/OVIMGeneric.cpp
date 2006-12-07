6fb7b7cd7db57091b98bb06642c37e5e797345e2
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-12-07 17:29:34 +0000

    r1006@59-104-90-45:  b6s | 2006-12-08 01:21:28 +0800  Turn off the OV_DEBUG flag

--- Modules/OVIMGeneric/OVIMGeneric.cpp
+++ Modules/OVIMGeneric/OVIMGeneric.cpp
@@ -28,7 +28,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#define OV_DEBUG
+// #define OV_DEBUG
 
 #include "OVIMGeneric.h"
 #include "CIN-Defaults.h"
