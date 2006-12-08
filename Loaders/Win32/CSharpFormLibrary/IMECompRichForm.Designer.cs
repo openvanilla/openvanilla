efde2ffe33e53a97cae7638057eac128274c3147
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2006-12-08 09:54:12 +0000

    Jaimie: 把Composition window裡的TextBox改成RichTextBox

---
+++ Loaders/Win32/CSharpFormLibrary/IMECompRichForm.Designer.cs
@@ -0,0 +1,82 @@
+namespace CSharpFormLibrary
+{
+    partial class IMECompRichForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Multiline = false;
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(312, 31);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Test";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(11, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 2);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " ";
+            // 
+            // IMECompRichForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(312, 31);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "IMECompRichForm";
+            this.Opacity = 0.75;
+            this.ShowInTaskbar = false;
+            this.Text = "IMECompRichForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Loaders/Win32/CSharpFormLibrary/IMECompRichForm.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSharpFormLibrary
+{
+    public partial class IMECompRichForm : Form
+    {
+        private const int WM_MOUSEACTIVATE = 0x0021;
+        private const int MA_NOACTIVATE = 0x0003;
+        private const int MA_NOACTIVATEANDEAT = 0x0004;
+
+        //private int caretWidth = 0;
+        //just give the caret a small point;
+        private int caretWidth = 2;
+        private int CurrentAll = 0; //現在input字數的大小
+        private int currentAllCompStrLength = 0;
+        private Label label2;//composition window那塊圖的大小
+        private int singleCaretHightlight = 0;
+        
+        public IMECompRichForm()
+        {
+            InitializeComponent();
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_MOUSEACTIVATE)
+            {
+                m.Result = (IntPtr)MA_NOACTIVATEANDEAT;
+                return;
+            }
+            base.WndProc(ref m);
+        }
+        		public void ShowNoActive()
+		{
+			UtilFuncs.SetVisibleNoActivate(this, true); // true to show. 
+		}
+		public void HideNoActive()
+		{
+			UtilFuncs.SetVisibleNoActivate(this, false); // false to hide.  
+		}
+		public void ClearComp()
+		{
+			this.richTextBox1.Clear();
+		}
+		public void SetLocation(int x, int y)
+		{
+			this.Location = new Point(x,y);
+		}
+		public void SetComp(string inputs)
+		{
+
+			if(inputs==null) return;
+            //if (inputs == null || this.textBox1.Text == inputs) return;
+            /* *** Comments due to OVIME.VC8.sln <Jaimie> *************/
+            /* *** postpone this merge until fully upgrade to VC8 *****/
+            Graphics g = this.richTextBox1.CreateGraphics();
+            Size s;
+            Size proposedSize1 = new Size();
+            TextFormatFlags tff1;
+
+            //1st method for get the exact composition length
+#if true
+            System.Drawing.StringFormat format = new System.Drawing.StringFormat();
+
+            System.Drawing.RectangleF rect = new System.Drawing.RectangleF(0, 0, 1000, 1000);
+
+            System.Drawing.CharacterRange[] ranges = 
+             { new System.Drawing.CharacterRange(0, inputs.Length) };
+            System.Drawing.Region[] regions = new System.Drawing.Region[1];
+
+            format.SetMeasurableCharacterRanges(ranges);
+
+            regions = g.MeasureCharacterRanges(inputs, this.richTextBox1.Font, rect, format);
+            rect = regions[0].GetBounds(g);
+
+            //mark the next line for dynamic composition window length setting.
+            //this.Width = (int)(rect.Right + 1.0f);
+            //currentAllCompStrLength = (int)(rect.Right + 1.0f);
+            currentAllCompStrLength = (int)(rect.Right);
+#endif
+            CurrentAll = inputs.Length;
+            g.Dispose(); 
+            this.richTextBox1.Text = inputs;
+        }
+		public void SetCaretX(int x)
+		{
+
+            if (x > 0)
+            {
+                this.richTextBox1.Select(x - 1, 1);
+                this.richTextBox1.SelectionBackColor = System.Drawing.Color.Black;
+                this.richTextBox1.SelectionColor = Color.White;
+                //this.richTextBox1.Width = this.richTextBox1.Width;
+                this.label1.Left = ((currentAllCompStrLength) * (x) / CurrentAll);
+            }
+            else
+            {
+                this.label1.Left = 0;
+            }
+
+
+            
+		}
+
+    }
+}
