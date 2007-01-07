757c303b153c2bac54ca1ef4d582e33eb012c837
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-01-07 12:02:50 +0000

    Revise deploy-installer.bat for convenience.

--- Loaders/Win32/deploy-installer.bat
+++ Loaders/Win32/deploy-installer.bat
@@ -1,13 +1,15 @@
+mkdir .\Installer\system32
+mkdir .\Installer\OpenVanilla
+mkdir .\Installer\OpenVanilla\Modules
 copy "%OV_DEPS%\libltdl3.dll" .\Installer\system32\
 copy "%OV_DEPS%\libiconv-2.dll" .\Installer\system32\
 copy "%OV_DEPS%\sqlite3.dll" .\Installer\system32\
 copy "%OV_DEPS%\tinyxml.dll" .\Installer\system32\
-copy .\CSharpFormLibrary\bin\Release\CSharpFormLibrary.dll .\Installer\OpenVanilla\
+copy ".\CSharpFormLibrary\bin\Release\CSharpFormLibrary.dll" .\Installer\OpenVanilla\
 copy .\OVIMEUIManaged\Release\OVIMEUI.dll .\Installer\system32\
-copy .\OVIME\Release\ovime.ime .\Installer\system32\
+copy .\OVIME\Release\OVIME.ime .\Installer\system32\
 copy .\OVPreferences\Release\OVPreferences.exe .\Installer\OpenVanilla\
 copy .\OVPreferences\Release\OVPreferences.exe.manifest .\Installer\OpenVanilla\
 xcopy /e .\OVPreferences\Release\zh_TW .\Installer\OpenVanilla\zh_TW\
-copy .\Modules\OVIMTobacco\Release\OVIMTobacco.dll .\Installer\OpenVanilla\Modules\
-copy .\Modules\OVIMRomanNew\Release\OVIMRomanNew.dll .\Installer\OpenVanilla\Modules\
+copy "..\..\..\branches\0.7.2-Module-OVIMTobacco-Old\OVIMTobaccoOld\Release\OVIMTobaccoOld.dll" ".\Installer\OpenVanilla\Modules\OVIMTobacco.dll"
 pause
