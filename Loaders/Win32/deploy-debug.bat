972e8eeb9ad56d60d61da657085a20bd0f5319e5
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-01-23 21:41:58 +0000

    Removes iconv from deploy-installer.bat. Revises deploy-debug.bat and deploy-release.bat for convenience.

--- Loaders/Win32/deploy-debug.bat
+++ Loaders/Win32/deploy-debug.bat
@@ -1,10 +1,10 @@
 call .\deploy-dependencies.bat
 copy .\CSharpFormLibrary\bin\Debug\CSharpFormLibrary.dll %WINDIR%\OpenVanilla\
 copy .\OVIMEUIManaged\Debug\OVIMEUI.dll %WINDIR%\system32\
-copy .\OVIME\Debug\ovime.ime %WINDIR%\system32\
-copy .\OVPreferences\Debug\OVPreferences.exe %WINDIR%\OpenVanilla\
-copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
-copy ..\..\Modules\OVIMGeneric\Debug\OVIMGeneric.dll %WINDIR%\OpenVanilla\Modules\
+copy .\OVIME\Debug\OVIME.ime %WINDIR%\system32\
+REM copy .\OVPreferences\Debug\OVPreferences.exe %WINDIR%\OpenVanilla\
+REM copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
+REM copy ..\..\Modules\OVIMGeneric\Debug\OVIMGeneric.dll %WINDIR%\OpenVanilla\Modules\
 copy "..\..\..\branches\0.7.2-Module-OVIMTobacco-Old\OVIMTobaccoOld\Debug\OVIMTobaccoOld.dll" "%WINDIR%\OpenVanilla\Modules\OVIMTobacco.dll"
-del "%APPDATA%\OpenVanilla\orz.txt"
+REM del "%APPDATA%\OpenVanilla\orz.txt"
 pause
--- Loaders/Win32/deploy-release.bat
+++ Loaders/Win32/deploy-release.bat
@@ -1,9 +1,9 @@
 call .\deploy-dependencies.bat
 copy .\CSharpFormLibrary\bin\Release\CSharpFormLibrary.dll %WINDIR%\OpenVanilla\
 copy .\OVIMEUIManaged\Release\OVIMEUI.dll %WINDIR%\system32\
-copy .\OVIME\Release\ovime.ime %WINDIR%\system32\
-copy .\OVPreferences\Release\OVPreferences.exe %WINDIR%\OpenVanilla\
-copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
-copy ..\..\Modules\OVIMGeneric\Release\OVIMGeneric.dll %WINDIR%\OpenVanilla\Modules\
+copy .\OVIME\Release\OVIME.ime %WINDIR%\system32\
+REM copy .\OVPreferences\Release\OVPreferences.exe %WINDIR%\OpenVanilla\
+REM copy .\OVPreferences\Release\OVPreferences.exe.manifest %WINDIR%\OpenVanilla\
+REM copy ..\..\Modules\OVIMGeneric\Release\OVIMGeneric.dll %WINDIR%\OpenVanilla\Modules\
 copy "..\..\..\branches\0.7.2-Module-OVIMTobacco-Old\OVIMTobaccoOld\Release\OVIMTobaccoOld.dll" "%WINDIR%\OpenVanilla\Modules\OVIMTobacco.dll"
 pause
