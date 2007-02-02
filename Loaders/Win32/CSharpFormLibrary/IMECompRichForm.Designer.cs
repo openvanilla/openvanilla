593d1b0d3eb532d68afcd6ad3c4aed52e9e63f31
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-02-02 10:03:36 +0000

    James: 用 .NET 2.0 的 TextRenderer 手動畫 comp 上面的字(包括字根變色)和 caret. 較 RichTextBox快約一秒。

--- Loaders/Win32/CSharpFormLibrary/IMECompRichForm.Designer.cs
+++ Loaders/Win32/CSharpFormLibrary/IMECompRichForm.Designer.cs
@@ -28,55 +28,70 @@ namespace CSharpFormLibrary
         /// </summary>
         private void InitializeComponent()
         {
-            //this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox1 = new AdvRichTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();        
+            this.components = new System.ComponentModel.Container();
+            //this.richTextBox1 = new System.Windows.Forms.TextBox();
+            //this.label1 = new System.Windows.Forms.Label();
+            //this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
             // 
             // label1
-            // 
+            /*// 
             this.label1.BackColor = System.Drawing.SystemColors.WindowText;
-            this.label1.Font = new System.Drawing.Font("PMingLiU", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));           
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(4, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(0);
             this.label1.Name = "label1";
-            this.label1.Bounds= new System.Drawing.Rectangle (4,0,1,22);
+            this.label1.Size = new System.Drawing.Size(1, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = " ";
-            this.label1.Parent = this;
+            this.label1.Visible = false;*/
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+ /*           this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)));
             this.richTextBox1.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.richTextBox1.Bounds = new System.Drawing.Rectangle(5, 0, 915, 22);
+            this.richTextBox1.Location = new System.Drawing.Point(5, 0);
             this.richTextBox1.Margin = new System.Windows.Forms.Padding(0);
             this.richTextBox1.Multiline = false;
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            //this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            //this.richTextBox1.SelectionAlignment = CSharpFormLibrary.TextAlign.Left;
+            this.richTextBox1.Size = new System.Drawing.Size(915, 22);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             this.richTextBox1.WordWrap = false;
-            this.richTextBox1.Parent = this;
+            this.richTextBox1.Visible = false; 
+        */ 
+            // 
+            // timer1
+            // 
+            //this.timer1.Tick += new System.EventHandler(this.timer2_Tick);
+            //this.timer1.Interval = 1000;            
             // 
             // IMECompRichForm
-            // 
+            //             
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            formInitWidth = 149;
-            this.ClientSize = new System.Drawing.Size(formInitWidth, 19);
-            this.ControlBox = false;            
+            this.ClientSize = new System.Drawing.Size(formInitWidth, 22);
+            this.ControlBox = false;
+            //this.Controls.Add(this.label1); 
+            //this.Controls.Add(this.richTextBox1);                      
             this.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximumSize = new System.Drawing.Size(600, 25);
+            //this.MaximumSize = new System.Drawing.Size(600, 25);
             this.Name = "IMECompRichForm";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.ResumeLayout(false);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.IMECompRichForm_Paint);
+
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label1;        
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox richTextBox1;
+       
     }
 }
--- Loaders/Win32/CSharpFormLibrary/IMECompRichForm.cs
+++ Loaders/Win32/CSharpFormLibrary/IMECompRichForm.cs
@@ -3,32 +3,120 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace CSharpFormLibrary
 {
     public partial class IMECompRichForm : Form
-    {
+    {        
         private const int WM_MOUSEACTIVATE = 0x0021;
         private const int MA_NOACTIVATE = 0x0003;
-        private const int MA_NOACTIVATEANDEAT = 0x0004;
-
-        private int formInitWidth;
-        private int caretPosX = 0;
-        private int caretPosIndex = 0;
+        private const int MA_NOACTIVATEANDEAT = 0x0004;        
+        private const int WM_NCACTIVATE = 0x0086;
+        private int formInitWidth=199;
         private int compSelStart = 0;
         private int compSelEnd = 0;
+        private int m_caretX = 0;
+        private string m_text="";
+        private int caretIndex = 0;
+
+        public int CaretX
+        {
+            get { return m_caretX; }
+            set { m_caretX = value; }
+        }
+
+        public string Buf
+        {
+            get { return m_text; }
+            set { m_text = value; }
+        }
         
         public IMECompRichForm()
         {
             InitializeComponent();
+            
             this.SetStyle(
                 ControlStyles.UserPaint |
                 ControlStyles.AllPaintingInWmPaint |
                 ControlStyles.OptimizedDoubleBuffer, true);
+             
             Application.EnableVisualStyles();
         }
+      
+
+        private void IMECompRichForm_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            
+            Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);            
+            Size proposedSize = new Size(int.MaxValue, int.MaxValue);
+            TextFormatFlags flags = TextFormatFlags.NoPadding;
+            Size size = TextRenderer.MeasureText(e.Graphics, Buf, Font, proposedSize, flags);
+            Size sizeText = TextRenderer.MeasureText(e.Graphics, Buf.Substring(0, caretIndex), Font, proposedSize,flags);
+            
+            int offsetX = 2;
+            int offsetY = 3;
+            //SizeF sizeF = e.Graphics.MeasureString(Buf.Substring(0, caretIndex), Font);
+                
+            //resize
+            if (this.Width - 50 < (int)size.Width)
+                this.Width += 100;            
+
+            //draw backcolor
+            /*b = new System.Drawing.SolidBrush(SystemColors.ButtonFace);
+            e.Graphics.FillRectangle(b, this.Bounds);
+            b.Dispose();*/
+            
+            //draw string
+            //Brush b;
+            //b = new SolidBrush(Color.Black);
+            TextRenderer.DrawText(e.Graphics, Buf, Font, new Point(offsetX, offsetY), Color.Black, flags);
+            //TextRenderer.DrawText(e.Graphics, Buf, Font, new Point(offsetX, offsetY), Color.Yellow, flags);
+
+            if (compSelEnd-compSelStart > 0)
+            {
+                //Debug.WriteLine("(" + compSelStart + "," + compSelEnd + ")");
+                if (compSelStart == 0)
+                    TextRenderer.DrawText(e.Graphics, Buf.Substring(compSelStart, compSelEnd - compSelStart), Font, new Point(offsetX, offsetY), Color.DarkKhaki, flags);
+                else
+                {
+                    Size sizeSelectionText = TextRenderer.MeasureText(e.Graphics, Buf.Substring(0,compSelStart), Font, proposedSize, flags);
+                    TextRenderer.DrawText(e.Graphics, Buf.Substring(compSelStart, compSelEnd - compSelStart), Font, new Point(sizeSelectionText.Width + offsetX, offsetY), Color.DarkKhaki, flags);
+                }
+            }
+            
+            
+            //draw border
+            ControlPaint.DrawBorder(
+                e.Graphics, ClientRectangle,
+                    Color.LightGray, ButtonBorderStyle.Outset);
+             
+            //save caret  for c++ using  and draw it                    
+            CaretX = (int)sizeText.Width;                                    
+            Pen p = new Pen(Color.Black, 1);
+            e.Graphics.DrawLine(p, sizeText.Width + offsetX, offsetY, sizeText.Width + offsetX, sizeText.Height + offsetY);
+            p.Dispose();
+            
+        }
+
+        /*private void MeasureStringMin(PaintEventArgs e)
+        {
+            // Measure string.
+            SizeF stringSize = new SizeF();
+            stringSize = e.Graphics.MeasureString(Buf.Substring(0, CaretX), this.richTextBox1.Font);
+
+            // Draw rectangle representing size of string.
+            e.Graphics.DrawRectangle(new Pen(Color.Red, 1), 0.0F, 0.0F, stringSize.Width, stringSize.Height);
+
+            // Draw string to screen.
+            e.Graphics.DrawString(Buf.Substring(0, CaretX), this.richTextBox1.Font, Brushes.Black, new PointF(0, 0));
+            System.Diagnostics.Debug.WriteLine("Buf=" + Buf);
+            System.Diagnostics.Debug.WriteLine("Buf sub=" + Buf.Substring(0, CaretX));
+            System.Diagnostics.Debug.WriteLine("size=" + stringSize.Width.ToString());
+        }*/
 
         protected override void WndProc(ref Message m)
         {
@@ -37,26 +125,48 @@ namespace CSharpFormLibrary
                 m.Result = (IntPtr)MA_NOACTIVATEANDEAT;
                 return;
             }
-            base.WndProc(ref m);
+            /*if (m.Msg == WM_NCACTIVATE)
+            {
+                if (m.WParam == (IntPtr)1)
+                {
+                    m.WParam = (IntPtr)0;
+                    System.Diagnostics.Debug.WriteLine("WM_NCACTIVATE w=1");
+                    //m.Result = (IntPtr)MA_NOACTIVATEANDEAT;
+                    //return;
+                    base.WndProc(ref m);
+                }
+                else
+                {
+                    m.WParam = (IntPtr)1;
+                    base.WndProc(ref m);
+                }
+            }
+            else */
+             base.WndProc(ref m);
         }
 
         public void ShowNoActive()
-		{
+		{            
+            //this.richTextBox1.Text = Buf;
+            this.Refresh();
+            //this.timer1.Enabled = true;
             if(!this.Visible)
 			    UtilFuncs.SetVisibleNoActivate(this, true); // true to show. 
 		}
 
 		public void HideNoActive()
 		{
+            //this.timer1.Enabled = false;
             if (this.Visible)
 			    UtilFuncs.SetVisibleNoActivate(this, false); // false to hide.  
 		}
 
 		public void ClearComp()
-		{
-			this.richTextBox1.Text="";
-            compSelStart = 0;
-            compSelEnd = 0;
+		{	
+            //this.richTextBox1.Text="";
+            Buf = "";
+            //compSelStart = 0;
+            //compSelEnd = 0;
             this.Width = formInitWidth;
 		}
 
@@ -68,12 +178,16 @@ namespace CSharpFormLibrary
 		public void SetComp(string inputs)
 		{
 			if(inputs==null) return;
-            this.richTextBox1.Text = inputs;
+            //先設定至 Buf -> 給 onPaint 算 Caret
+            Buf = inputs;
+            
+            //CaretX = caretIndex;
+            //this.richTextBox1.Text = inputs;
         }
 
         public int GetCaretPosX()
         {
-            return caretPosX;
+            return CaretX;
         }
 
 		public void SetCaretX(int x)
@@ -82,18 +196,21 @@ namespace CSharpFormLibrary
                 new StringBuilder(this.richTextBox1.Text);
             sb.Insert(x, "︳");
             this.richTextBox1.Text = sb.ToString();*/
-
-            caretPosIndex = x;
-            Point pt =             
-                this.richTextBox1.GetPositionFromCharIndex(caretPosIndex);
-            caretPosX = pt.X + 6;
-            this.label1.Left = caretPosX;
             
-            Point pt2 = 
+            /*Point pt =             
+                this.richTextBox1.GetPositionFromCharIndex(x);
+            this.label1.Left  = pt.X + 6;            
+            System.Diagnostics.Debug.WriteLine("X=" + x.ToString());
+            System.Diagnostics.Debug.WriteLine("labelX=" + label1.Left.ToString());*/
+
+            this.caretIndex = x;
+            
+
+            /*Point pt2 = 
                 this.richTextBox1.GetPositionFromCharIndex(
                     this.richTextBox1.Text.Length);
             if (this.Width - this.richTextBox1.Bounds.Left < pt2.X)
-                this.Width = pt2.X + 100;
+                this.Width = pt2.X + 100;*/
             /*
             if (compSelStart > 0 && compSelStart == compSelEnd) //已經組字
                 this.richTextBox1.Select(compSelStart - 1, 1);
@@ -116,5 +233,21 @@ namespace CSharpFormLibrary
         {
             this.Dispose(true);
         }
+   
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {            
+            if (label1.Visible == true)
+            {
+                label1.Visible = false;
+                System.Diagnostics.Debug.WriteLine("label1.Visible=false");
+            }
+            else
+            {
+                label1.Visible = true;
+                System.Diagnostics.Debug.WriteLine("label1.Visible=true");
+            }
+        }
+
     }
 }
