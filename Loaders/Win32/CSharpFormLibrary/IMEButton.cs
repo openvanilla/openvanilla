a3e6337a32d7fc798a17f60569eae38f886f4e7c
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-02-13 09:52:29 +0000

    James: 修好 IMEButton 被按下去按鈕沒有改變的問題。

--- Loaders/Win32/CSharpFormLibrary/IMEButton.cs
+++ Loaders/Win32/CSharpFormLibrary/IMEButton.cs
@@ -48,27 +48,27 @@ namespace CSharpFormLibrary
         private void UpdateAppearance()
         {
             if (m_wasMouseDown)
-            {
+            {                
                 m_colorTop = Color.DimGray;
                 m_colorMiddle = Color.DimGray;
                 m_colorBottom = Color.Black;
                 m_colorBorder = Color.Gray;
                 m_colorText = Color.White;
                 m_buttonBorderStyle = ButtonBorderStyle.Inset;
-
                 m_wasButtonPressed = true;
+                this.Refresh();
             }
             else
             {
-                if(m_wasButtonPressed) {
+                if(m_wasButtonPressed) {                    
                     m_colorTop = Color.LightGray;
                     m_colorMiddle = Color.DimGray;
                     m_colorBottom = Color.Black;
                     m_colorBorder = Color.Gray;
                     m_colorText = Color.White;
                     m_buttonBorderStyle = ButtonBorderStyle.Outset;
+                    this.Refresh();
                 }
-
                 m_wasButtonPressed = false;
             }
         }
@@ -109,25 +109,26 @@ namespace CSharpFormLibrary
 
         protected void MyOnMouseDown()
         {
-            m_wasMouseDown = true;
+            m_wasMouseDown = true;            
             UpdateAppearance();
+            System.Diagnostics.Debug.WriteLine("mousedown");
         }
 
         protected override void OnMouseUp(MouseEventArgs mevent)
         {
             base.OnMouseUp(mevent);
-            m_wasMouseDown = false;
+            m_wasMouseDown = false;            
             UpdateAppearance();
+            System.Diagnostics.Debug.WriteLine("mouseup");
         }
 
         protected override void WndProc(ref Message m)
         {
-            base.WndProc(ref m);
-
+            base.WndProc(ref m);            
             if (m.Msg == (Int32)UtilFuncs.WindowsMessage.WM_MOUSEACTIVATE)
             {
-                m.Result = (IntPtr)MA_NOACTIVATEANDEAT;
-                MyOnMouseDown();
+                m.Result = (IntPtr)MA_NOACTIVATEANDEAT;                
+                MyOnMouseDown();                
             }
         }
     }
