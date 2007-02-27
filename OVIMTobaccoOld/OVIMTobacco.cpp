9aba0e40282e63376f0e4bc0f24089a2c133c7ee
b6s <b6s@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-02-27 15:01:52 +0000

    Fixes a bug of SQL escape character '%' in QueryForKey.

--- OVIMTobaccoOld/OVIMTobacco.cpp
+++ OVIMTobaccoOld/OVIMTobacco.cpp
@@ -1,6 +1,6 @@
 // OVIMTobacco.cpp
 
-//#define OV_DEBUG
+#define OV_DEBUG
 
 #ifndef WIN32
 	#include <OpenVanilla/OpenVanilla.h>
@@ -1086,6 +1086,7 @@ const char *QueryForKey(SQLite3 *db, const char *tbl, const char *key) {
     char *ep=keyescaped;
     while (*kp) {
         if (*kp=='\'') { *ep++='\''; *ep++='\''; }
+		if (*kp=='%') { *ep++='%'; *ep++='%'; }
         else *ep++=*kp;
         kp++;
     }
