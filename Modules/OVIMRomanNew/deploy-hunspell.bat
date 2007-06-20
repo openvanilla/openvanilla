1241b8e455d09376b21a493e85a2302e88bed9d7
barabbas <barabbas@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-06-20 07:15:25 +0000

    Moves hunspell deployment steps from trunk/Loaders/Win32/deploy-dependencies.bat to trunk/Modules/OVIMRomanNew/deploy-hunspell.bat Adds deploy-hunspell.bat to the post-build events of OVIMRomanNew.vcproj Revises README for Win32 building and deployment of OVIMRomanNew

---
+++ Modules/OVIMRomanNew/deploy-hunspell.bat
@@ -0,0 +1,4 @@
+copy "%OV_DEPS%\hunspell\hunspelldll.dll" %WINDIR%\system32\
+mkdir "%WINDIR%\OpenVanilla\Modules\OVIMRomanNew"
+copy en_US.aff "%WINDIR%\OpenVanilla\Modules\OVIMRomanNew"
+copy en_US.dic "%WINDIR%\OpenVanilla\Modules\OVIMRomanNew"
