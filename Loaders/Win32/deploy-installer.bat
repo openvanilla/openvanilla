e54a13b76083b60fd26e606e150ba0d4f1543e0b
barabbas <barabbas@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-07-26 16:07:51 +0000

    Progresses OV-Win32 to 0.7.2.4.

--- Loaders/Win32/deploy-installer.bat
+++ Loaders/Win32/deploy-installer.bat
@@ -1,6 +1,8 @@
-mkdir .\Installer\system32
-mkdir .\Installer\OpenVanilla
-mkdir .\Installer\OpenVanilla\Modules
+mkdir ".\Installer\system32"
+mkdir ".\Installer\OpenVanilla"
+mkdir ".\Installer\OpenVanilla\Modules"
+mkdir ".\Installer\OpenVanilla\Modules\OVIMGeneric"
+mkdir ".\Installer\OpenVanilla\Modules\OVIMRomanNew"
 copy "%OV_DEPS%\libltdl3.dll" .\Installer\system32\
 copy "%OV_DEPS%\libiconv-2.dll" .\Installer\system32\
 copy "%OV_DEPS%\sqlite3.dll" .\Installer\system32\
@@ -13,6 +15,8 @@ copy .\OVIME\Release\OVIME.ime .\Installer\system32\
 copy .\OVPreferences\Release\OVPreferences.exe .\Installer\OpenVanilla\
 copy .\OVPreferences\Release\OVPreferences.exe.manifest .\Installer\OpenVanilla\
 xcopy /e .\OVPreferences\Release\zh_TW .\Installer\OpenVanilla\zh_TW\
-copy "..\..\..\branches\0.7.2-Module-OVIMTobacco-Old\OVIMTobaccoOld\Release\OVIMTobaccoOld.dll" ".\Installer\OpenVanilla\Modules\OVIMTobacco.dll"
+REM copy "..\..\..\branches\0.7.2-Module-OVIMTobacco-Old\OVIMTobaccoOld\Release\OVIMTobaccoOld.dll" ".\Installer\OpenVanilla\Modules\OVIMTobacco.dll"
 copy "..\..\Modules\OVIMGeneric\Release\OVIMGeneric.dll" ".\Installer\OpenVanilla\Modules\"
+copy "..\..\Modules\OVIMRomanNew\Release\OVIMRomanNew.dll" ".\Installer\OpenVanilla\Modules\"
+copy "..\..\Modules\OVIMRomanNew\en_US.*" ".\Installer\OpenVanilla\Modules\OVIMRomanNew\"
 pause
