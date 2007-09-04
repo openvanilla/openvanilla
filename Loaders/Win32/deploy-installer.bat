37645dcd36a1602af22725f9af9d2fd51966fff0
barabbas <barabbas@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-09-04 14:38:47 +0000

    Revises deploy-installer.bat as OVPreferences and Hunspell changes.

--- Loaders/Win32/deploy-installer.bat
+++ Loaders/Win32/deploy-installer.bat
@@ -2,21 +2,21 @@ mkdir ".\Installer\system32"
 mkdir ".\Installer\OpenVanilla"
 mkdir ".\Installer\OpenVanilla\Modules"
 mkdir ".\Installer\OpenVanilla\Modules\OVIMGeneric"
-mkdir ".\Installer\OpenVanilla\Modules\OVIMRomanNew"
 mkdir ".\Installer\OpenVanilla\Resources"
 copy "%OV_DEPS%\libltdl3.dll" .\Installer\system32\
 copy "%OV_DEPS%\libiconv-2.dll" .\Installer\system32\
 copy "%OV_DEPS%\sqlite3.dll" .\Installer\system32\
 copy "%OV_DEPS%\tinyxml.dll" .\Installer\system32\
-copy "%OV_DEPS%\hunspell\hunspelldll.dll" .\Installer\system32\
+copy "%OV_DEPS%\hunspell\hunspell.dll" .\Installer\system32\
 copy ".\CSharpFormLibrary\bin\Release\CSharpFormLibrary.dll" .\Installer\OpenVanilla\
 copy "..\..\Utilities\GacUtil\bin\Release\GacUtil.exe" .\Installer\OpenVanilla\
 copy .\OVIMEUIManaged\Release\OVIMEUI.dll .\Installer\system32\
 copy .\OVIME\Release\OVIME.ime .\Installer\system32\
-copy .\OVPreference.CS2\bin\Release\OVPreference.CS2.exe .\Installer\OpenVanilla\
-copy .\OVPreference.CS2\bin\Release\Resources\*.resources .\Installer\OpenVanilla\Resources
+copy .\OVPreferences\bin\Release\OVPreferences.exe .\Installer\OpenVanilla\
+copy .\OVPreferences\bin\Release\Resources\*.resources .\Installer\OpenVanilla\Resources
 REM copy "..\..\..\branches\0.7.2-Module-OVIMTobacco-Old\OVIMTobaccoOld\Release\OVIMTobaccoOld.dll" ".\Installer\OpenVanilla\Modules\OVIMTobacco.dll"
 copy "..\..\Modules\OVIMGeneric\Release\OVIMGeneric.dll" ".\Installer\OpenVanilla\Modules\"
 copy "..\..\Modules\OVIMRomanNew\Release\OVIMRomanNew.dll" ".\Installer\OpenVanilla\Modules\"
-copy "..\..\Modules\OVIMRomanNew\en_US.*" ".\Installer\OpenVanilla\Modules\OVIMRomanNew\"
+cd "..\..\Modules\OVIMRomanNew"
+call "deploy-hunspell.bat"
 pause
