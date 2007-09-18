deee5d106d3eb7b170149cac460ceb7dbf319613
barabbas <barabbas@da5ee8da-a019-0410-8a94-01093a00bd73>
2007-09-18 19:41:22 +0000

    Advances win32 to 0.7.2.7 with Tobacco.

--- Loaders/Win32/deploy-dependencies.bat
+++ Loaders/Win32/deploy-dependencies.bat
@@ -1,5 +1,4 @@
 copy "%OV_DEPS%\libltdl3.dll" %WINDIR%\system32\
 copy "%OV_DEPS%\libiconv-2.dll" %WINDIR%\system32\
 copy "%OV_DEPS%\sqlite3.dll" %WINDIR%\system32\
-REM copy "%OV_DEPS%\tinyxml.dll" %WINDIR%\system32\
-REM copy "%OV_DEPS%\hunspell\hunspelldll.dll" %WINDIR%\system32\
+pause
--- Loaders/Win32/deploy-installer.bat
+++ Loaders/Win32/deploy-installer.bat
@@ -2,11 +2,12 @@ mkdir ".\Installer\system32"
 mkdir ".\Installer\OpenVanilla"
 mkdir ".\Installer\OpenVanilla\Modules"
 mkdir ".\Installer\OpenVanilla\Modules\OVIMGeneric"
+mkdir ".\Installer\OpenVanilla\Modules\OVIMRomanNew"
+mkdir ".\Installer\OpenVanilla\Modules\OVIMTobacco"
 mkdir ".\Installer\OpenVanilla\Resources"
 copy "%OV_DEPS%\libltdl3.dll" .\Installer\system32\
 copy "%OV_DEPS%\libiconv-2.dll" .\Installer\system32\
 copy "%OV_DEPS%\sqlite3.dll" .\Installer\system32\
-copy "%OV_DEPS%\tinyxml.dll" .\Installer\system32\
 copy "%OV_DEPS%\hunspell\hunspell.dll" .\Installer\system32\
 copy ".\CSharpFormLibrary\bin\Release\CSharpFormLibrary.dll" .\Installer\OpenVanilla\
 copy "..\..\Utilities\GacUtil\bin\Release\GacUtil.exe" .\Installer\OpenVanilla\
@@ -14,9 +15,13 @@ copy .\OVIMEUIManaged\Release\OVIMEUI.dll .\Installer\system32\
 copy .\OVIME\Release\OVIME.ime .\Installer\system32\
 copy .\OVPreferences\bin\Release\OVPreferences.exe .\Installer\OpenVanilla\
 copy .\OVPreferences\bin\Release\Resources\*.resources .\Installer\OpenVanilla\Resources
-REM copy "..\..\..\branches\0.7.2-Module-OVIMTobacco-Old\OVIMTobaccoOld\Release\OVIMTobaccoOld.dll" ".\Installer\OpenVanilla\Modules\OVIMTobacco.dll"
 copy "..\..\Modules\OVIMGeneric\Release\OVIMGeneric.dll" ".\Installer\OpenVanilla\Modules\"
+copy "..\..\Modules\SharedData\dayi3-patched.cin" ".\Installer\OpenVanilla\Modules\OVIMGeneric\"
+copy "..\..\Modules\SharedData\ehq-symbols.cin" ".\Installer\OpenVanilla\Modules\OVIMGeneric\"
 copy "..\..\Modules\OVIMRomanNew\Release\OVIMRomanNew.dll" ".\Installer\OpenVanilla\Modules\"
-cd "..\..\Modules\OVIMRomanNew"
-call "deploy-hunspell.bat"
+copy "..\..\Modules\OVIMRomanNew\en_US.aff" ".\Installer\OpenVanilla\Modules\OVIMRomanNew\"
+copy "..\..\Modules\OVIMRomanNew\en_US.dic" ".\Installer\OpenVanilla\Modules\OVIMRomanNew\"
+copy "..\..\Modules\OVIMTobacco\Release\OVIMTobacco.dll" ".\Installer\OpenVanilla\Modules\"
+copy "..\..\Modules\OVIMTobacco\imtables.db" ".\Installer\OpenVanilla\Modules\OVIMTobacco\"
+copy "..\..\Modules\OVIMTobacco\tsi.db" ".\Installer\OpenVanilla\Modules\OVIMTobacco\"
 pause
