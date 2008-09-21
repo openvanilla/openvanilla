588ec54939d1e9807b0dba2ce8ebb7cf77d442ec
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2008-09-21 13:25:20 +0000

    Removes all previously OV 0.8.x in-module project files and Makefiles.

--- Modules/OVIMPOJ-Holo/make.bat
+++
@@ -1,2 +0,0 @@
-cl /I..\..\Framework\Headers /I..\SharedHeaders /I.\ /EHsc /GR /MT /D"WIN32" /FeOVIMPOJ-Holo.DLL /LD OVIMPOJ-Holo.cpp POJ-Holo.cpp ..\SharedSource\*.cpp /link /DEF:..\..\Experiments\Win32\OVModule.def
-pause
