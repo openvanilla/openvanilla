35c9e3ddea9abfcb03dd88f13a02df60b9cddc94
zonble <zonble@da5ee8da-a019-0410-8a94-01093a00bd73>
2008-09-21 12:39:06 +0000

    Removes unused files.

--- Utilities/OVFilterWidget/OVFilterWidget.wdgtproj/project.wdgt/zh_CN.lproj/localizedStrings.js
+++
@@ -1,8 +0,0 @@
-var localizedStrings = new Object;
-
-localizedStrings["Done"] = unescape('%u5B8C%u6210');
-localizedStrings['Output Filter Widget'] = unescape('%u6587%u5B57%u8FC7%u6EE4%u8F6C%u6362 Widget');
-localizedStrings['Start Converting!'] = unescape('%u5F00%u59CB%u8F6C%u6362%uFF01');
-localizedStrings[unescape('OpenVanilla is an Input Method Framework%2C it contains not only input method modules but also output filter modules. For further information%2C please visit http://openvanilla.org/ .')] = unescape('OpenVanilla %u662F%u4E00%u5957%u6587%u5B57%u8F93%u5165%u73AF%u5883%uFF0C%u9664%u4E86%u8F93%u5165%u6CD5%u4E4B%u5916%uFF0C%u8FD8%u5305%u542B%u5404%u79CD%u6587%u5B57%u8F6C%u6362%u8FC7%u6EE4%u529F%u80FD%uFF0C%u4EE5%u53CA%u4E2D%u6587%u5904%u7406%u6240%u9700%u8981%u7684%u5404%u79CD%u5DE5%u5177%u3002%u60A8%u53EF%u4EE5%u5728%u5B98%u65B9%u7F51%u7AD9%u4E0A%uFF0C%u627E%u5230%u66F4%u591A%u76F8%u5173%u8D44%u8BAF%uFF1A http://openvanilla.org/ .');
-localizedStrings['Please input text here...'] = unescape('%u8BF7%u5728%u6B64%u8F93%u5165%u60A8%u8981%u8F6C%u6362%u7684%u6587%u5B57%u2026');
-localizedStrings['Converted result goes here...'] = unescape('%u5728%u6B64%u4F1A%u51FA%u73B0%u6587%u5B57%u7684%u8F6C%u6362%u7ED3%u679C%u2026');
--- Utilities/OVSQLiteEditor/README.txt
+++
@@ -1,7 +0,0 @@
-For Win32, download http://www.ch-werner.de/javasqlite/javasqlite-20060714-win32.zip
-and put them into C:/Program Files/Java/.../
-
-For Mac OS X, follow the instructions of http://www.io.com/~bolsinga/archives/2006.html#e216
-
-For Linux, download http://www.ch-werner.de/javasqlite/javasqlite-20060714.tar.gz
-and follow the instructions of http://www.ch-werner.de/javasqlite/
