f769512f58bd20b7eb9e9b23b1ee293ce81a197f
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2008-09-21 12:46:10 +0000

    Removes all non-core modules from 0.9 branch

--- Modules/OVIMRomanNew/deploy-hunspell.bat
+++
@@ -1,4 +0,0 @@
-copy "%OV_DEPS%\hunspell\libhunspell.dll" %WINDIR%\system32\
-mkdir "C:\OpenVanilla\Modules\OVIMRomanNew"
-copy en_US.aff "C:\OpenVanilla\Modules\OVIMRomanNew\"
-copy en_US.dic "C:\OpenVanilla\Modules\OVIMRomanNew\"
