ecb4b3692a4c781d3a6d9668133a59a9e06abcdd
lukhnos <lukhnos@da5ee8da-a019-0410-8a94-01093a00bd73>
2008-09-21 12:12:49 +0000

    Removes Experiments

--- Experiments/VisualBasic/OVIMPOJ-Holo/make.bat
+++
@@ -1,2 +0,0 @@
-cl /I..\..\..\Headers /I.\ /I..\ /EHsc /GR /MT /D"WIN32" /FeOVIMPOJ-Holo.DLL /LD ..\OVIMPOJ-Holo.cpp ..\POJ-Holo.cpp ..\..\..\Source\*.cpp /link /DEF:..\..\Win32\OVModule.def
-pause
--- Experiments/Web/OV/t/view_JSON.t
+++
@@ -1,6 +0,0 @@
-use strict;
-use warnings;
-use Test::More tests => 1;
-
-BEGIN { use_ok 'OV::View::JSON' }
-
