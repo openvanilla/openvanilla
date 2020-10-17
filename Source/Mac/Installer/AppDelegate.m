621b04ab684bab995402a5d2385a15176e515e7d
Lukhnos Liu <lukhnos@lukhnos.org>
2020-10-16 21:40:29 -0700

    Carry over McBopomofo changes for the latest macOS
    * Fix vertical candidate window style
    * Supports Caps Lock toggle
    * Make sure Installer can see the uninstalled app bundle unmounted

--- Source/Mac/Installer/AppDelegate.m
+++ Source/Mac/Installer/AppDelegate.m
@@ -306,11 +306,23 @@ NSModalResponse RunAlertPanel(NSString *title, NSString *message, NSString *butt
     entryCount = getfsstat(bufs, entryCount * entrySize, MNT_NOWAIT);
     for (int i = 0; i < entryCount; i++) {
         if (!strcmp(bundleAbsPath, bufs[i].f_mntfromname)) {
+            free(bufs);
+
+            // getfsstat() may return us a cached result, and so we need to get the stat of the mounted fs.
+            // If statfs() returns an error, the mounted fs is already gone.
+            struct statfs stat;
+            int checkResult = statfs(bundleAbsPath, &stat);
+            if (checkResult != 0) {
+                // Meaning the app's bundle is not mounted, that is it's not translocated.
+                // It also means that the app is not loaded.
+                return NO;
+            }
+
             return YES;
         }
     }
+    free(bufs);
     return NO;
-
 }
 
 - (void)resetArrayModuleSettings
