e33cb4d937bac37a7c6e8af2e3ec3b2a5c253cd4
zonble <zonble@gmail.com>
2023-12-19 22:24:05 +0800

    Removes unused code.

--- Source/Mac/OVModuleManager/OVModuleManager.mm
+++ Source/Mac/OVModuleManager/OVModuleManager.mm
@@ -38,10 +38,6 @@
 
 @import TooltipUI;
 @import VXHanConvert;
-//@import OVIMTableBased;
-//@import OVIMArray;
-//@import OVIMBig5Code;
-//@import OVAFAssociatedPhrases;
 
 @interface OVCanInstall ()
 @property (readwrite, assign) BOOL willOverrideBuiltInTable;
