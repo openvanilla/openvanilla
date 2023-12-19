b815eacf6221e5585661b065abfa7a21835133e9
zonble <zonble@gmail.com>
2023-12-19 22:19:09 +0800

    Fixes localization.

--- Source/Mac/Preferences/PreferencesWindowController.swift
+++ Source/Mac/Preferences/PreferencesWindowController.swift
@@ -29,6 +29,11 @@ private let kGeneralSettingIdentifier = "GeneralSettingIdentifier"
 private let kAssociatedPhrasesSettingIdentifier = "org.openvanilla.OVAFAssociatedPhrases"
 private let kAddInputMethodIdentifier = "AddInputMethodIdentifier"
 
+final class Dummy: NSObject {
+    @objc func title() -> String { "" }
+    @objc func setTitle(_ title:String) {}
+}
+
 typealias PreferencesItem = (identifier: String,
                              localizedName: String,
                              viewController: BasePreferencesViewController)
@@ -74,11 +79,11 @@ class PreferencesWindowController: NSWindowController {
             }
             if let view = view as? NSMatrix {
                 for cell in view.cells {
-                    localizableObjects[NSValue(nonretainedObject: view)] = cell.title
+                    localizableObjects[NSValue(nonretainedObject: cell)] = cell.title
                 }
                 return
             }
-            let selector = Selector("title")
+            let selector = #selector(Dummy.title)
             if view.responds(to: selector) {
                 if let title = view.perform(selector).takeRetainedValue() as? String {
                     localizableObjects[NSValue(nonretainedObject: view)] = title
@@ -117,9 +122,9 @@ class PreferencesWindowController: NSWindowController {
     func reload(_ notification: Notification?) {
         items.removeAll()
         tableView.deselectAll(self)
-        items.append((kGeneralSettingIdentifier, "General Settings", generalPreferencesViewController))
-        items.append((kAssociatedPhrasesSettingIdentifier, "Associated Phrases", associatedPhrasesPreferencesViewController))
-        
+        items.append((kGeneralSettingIdentifier, NSLocalizedString("General Settings", comment:""), generalPreferencesViewController))
+        items.append((kAssociatedPhrasesSettingIdentifier, NSLocalizedString("Associated Phrases", comment:""), associatedPhrasesPreferencesViewController))
+
         let identifiers = OVModuleManager.default().inputMethodIdentifiers
         for moduleIdentifier in identifiers {
             if OVModuleManager.default().isTableBasedInputMethodIdentifier(moduleIdentifier) {
@@ -133,15 +138,15 @@ class PreferencesWindowController: NSWindowController {
                       OVModuleManager.default().localizedInputMethodName(arrayModulePreferencesViewController.moduleIdentifier ?? ""),
                       arrayModulePreferencesViewController))
         
-        items.append((kAddInputMethodIdentifier, "Add New Input Method", addTableBasedInputMethodViewController))
-        
+        items.append((kAddInputMethodIdentifier, NSLocalizedString("Add New Input Method", comment:""), addTableBasedInputMethodViewController))
+
         tableView.reloadData()
         tableView.selectRowIndexes(IndexSet(integer: 0), byExtendingSelection: false)
     }
     
     private func updateLocalization() {
         for value in localizableObjects.keys {
-            guard let view = value.nonretainedObjectValue as? NSView,
+            guard let viewOrCell = value.nonretainedObjectValue as? NSObject,
                   let dictKey = localizableObjects[value]
             else {
                 continue
@@ -149,15 +154,20 @@ class PreferencesWindowController: NSWindowController {
             
             let text = NSLocalizedString(dictKey, comment: "")
             
-            if let view = view as? NSTextField {
+            if let view = viewOrCell as? NSTextField {
                 if !view.isEditable {
                     view.stringValue = text
                     continue
                 }
             }
-            let selector = Selector("setTitle:")
-            if view.responds(to: selector) {
-                view.perform(selector, with: text)
+            if let cell = viewOrCell as? NSCell {
+                cell.title = text
+                continue
+            }
+
+            let selector = #selector(Dummy.setTitle(_:))
+            if viewOrCell.responds(to: selector) {
+                viewOrCell.perform(selector, with: text)
             }
         }
     }
