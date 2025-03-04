7f0e38c25e611918c75515ee613da2939053ecba
zonble <zonble@gmail.com>
2025-03-04 22:55:13 +0800

    Adds CinManager.

---
+++ Source/Mac/CinManager/CinManager.swift
@@ -0,0 +1,169 @@
+//
+// CinManager.swift
+//
+// Copyright (c) 2004-2012 Lukhnos Liu (lukhnos at openvanilla dot org)
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import Foundation
+
+private let CinRepositoryBaseURL =
+"https://raw.githubusercontent.com/chinese-opendesktop/cin-tables/refs/heads/master"
+
+struct CinTable {
+    var filename: String
+    var url: URL
+    var name: String
+    var shortDescription: String
+}
+
+struct CinTableGroup {
+    var name: String
+    var tables: [CinTable]
+
+    mutating func insert(_ table: CinTable) {
+        tables.append(table)
+    }
+}
+
+fileprivate struct CinReadMeParser {
+    static func parse(text: String) -> [CinTableGroup]  {
+        let lines = text.split(separator: "\n")
+        let groupRegex = try! NSRegularExpression(pattern: "Language\\s=\\s\"(.*),(.*)\"")
+        let tableRegex = try! NSRegularExpression(pattern: "(\\w+)\\.cin,\"(.*)\",\"(.*)\"")
+
+        var groups: [CinTableGroup] = []
+        var currentGroup: CinTableGroup?
+
+        for line in lines {
+            if let range = groupRegex.firstMatch(in: String(line), range: NSRange(location: 0, length: line.utf16.count)) {
+                print("range \(range)")
+                let groupNameRange = range.range(at: 1)
+                let groupContentRange = range.range(at: 2)
+
+                let groupName = String(line[line.index(line.startIndex, offsetBy: groupNameRange.lowerBound)..<line.index(line.startIndex, offsetBy: groupNameRange.upperBound)])
+
+                if let currentGroup = currentGroup {
+                    groups.append(currentGroup)
+                }
+
+                currentGroup = CinTableGroup(name: groupName, tables: [])
+                continue
+            }
+
+            if let range = tableRegex.firstMatch(in: String(line), range: NSRange(location: 0, length: line.utf16.count)) {
+                print("range \(range)")
+                let filenameRange = range.range(at: 1)
+                let filename = String(line[line.index(line.startIndex, offsetBy: filenameRange.lowerBound)..<line.index(line.startIndex, offsetBy: filenameRange.upperBound)]) + ".cin"
+                let url  = CinRepositoryBaseURL + "/" + filename
+                let nameRange = range.range(at: 2)
+                let name = String(line[line.index(line.startIndex, offsetBy: nameRange.lowerBound)..<line.index(line.startIndex, offsetBy: nameRange.upperBound)])
+                let shortDescriptionRange = range.range(at: 3)
+                let shortDescription = String(line[line.index(line.startIndex, offsetBy: shortDescriptionRange.lowerBound)..<line.index(line.startIndex, offsetBy: shortDescriptionRange.upperBound)])
+                currentGroup?.insert(CinTable(filename: filename, url: URL(string: url)!, name: name, shortDescription: shortDescription))
+            }
+        }
+        if let currentGroup = currentGroup {
+            groups.append(currentGroup)
+        }
+        return groups
+    }
+}
+
+@MainActor
+class CinManager: Sendable {
+    static let shared = CinManager()
+
+    protocol Delegate: AnyObject {
+        func cinManager(_ manager: CinManager, didUpdate state: State)
+    }
+
+    enum State {
+        case initial
+        case failed(error: Error)
+        case loading
+        case loaded(tableGroups: [CinTableGroup])
+        case loadedButLoading(tableGroups: [CinTableGroup])
+    }
+
+    enum CinManagerError: Error, LocalizedError {
+        case noData
+
+        var errorDescription: String? {
+            "No data found."
+        }
+    }
+
+    weak var delegate: Delegate?
+
+    var state = State.initial {
+        didSet {
+            self.delegate?.cinManager(self, didUpdate: state)
+        }
+    }
+
+    func load() {
+        let readmeURLString = "README.md"
+        guard let url = URL(string: CinRepositoryBaseURL + "/" + readmeURLString) else {
+            return
+        }
+
+        switch state {
+        case .loading, .loadedButLoading:
+            return
+        case .initial, .failed:
+            self.state = .loading
+        case let .loaded(tableGroups):
+            self.state = .loadedButLoading(tableGroups: tableGroups)
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            if let error {
+                DispatchQueue.main.async {
+                    if case let .loadedButLoading(tabkeGroup) = self?.state {
+                        self?.state = .loaded(tableGroups: tabkeGroup)
+                    } else {
+                        self?.state = .failed(error: error)
+                    }
+                }
+                return
+            }
+            guard let data = data, let string = String(data: data, encoding: .utf8) else {
+                DispatchQueue.main.async {
+                    if case let .loadedButLoading(tabkeGroup) = self?.state {
+                        self?.state = .loaded(tableGroups: tabkeGroup)
+                    } else {
+                        self?.state = .failed(error: CinManagerError.noData)
+                    }
+                }
+                return
+            }
+            let result = CinReadMeParser.parse(text: string)
+            DispatchQueue.main.async {
+                self?.state = .loaded(tableGroups: result)
+            }
+        }
+        task.resume()
+    }
+
+}
