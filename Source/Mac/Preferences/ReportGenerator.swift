afac024516a4187f3a5a0698c4e7d42d4e1855db
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2025-11-14 16:23:04 +0800

    Polish preference report output

--- Source/Mac/Preferences/ReportGenerator.swift
+++ Source/Mac/Preferences/ReportGenerator.swift
@@ -1,4 +1,4 @@
-// Copyright (c) 2004-2012 Lukhnos Liu (lukhnos at openvanilla dot org)
+// Copyright (c) 2004-2025 Lukhnos Liu (lukhnos at openvanilla dot org)
 //
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
@@ -26,57 +26,99 @@ import InfoCollector
 import ModuleManager
 import OpenVanillaImpl
 
+extension Bool {
+    fileprivate var yesNo: String { self ? "Y" : "N" }
+}
+
 private class OpenVanillaPreferenceCollector {
-    static func genreate() -> String {
+    static func generate() -> String {
         var report = "- OpenVanilla Input Methods\n"
         let tables = OVModuleManager.default.inputMethodIdentifiers
         for table in tables {
             report += "  - \(table)\n"
             let layout = OVModuleManager.default.alphanumericKeyboardLayout(
                 forInputMethod: table)
-            report += OVModuleManager.default.isCustomTableBasedInputMethod(table) ?
-                "    - Cutom Input Method\n" :
-                "    - Built-in Input Method\n"
+            report +=
+                OVModuleManager.default.isCustomTableBasedInputMethod(table)
+                ? "    - Custom Input Method\n" : "    - Built-in Input Method\n"
             report += "    - Alphanumeric Keyboard: \(layout)\n"
             if table.hasPrefix("org.openvanilla.OVIMTableBased") {
-                report += "    - Clear reading at composition error: " + (PreferencesHelper
-                    .boolValue(moduleIdentifier: table, forKey: "ClearReadingBufferAtCompositionError") ? "Y" : "N") + "\n"
-                report += "    - Compose while typing: " + (PreferencesHelper
-                    .boolValue(moduleIdentifier: table, forKey: "ComposeWhileTyping") ? "Y" : "N")  + "\n"
-                report += "    - Should compose at maximum radicala length: " + (PreferencesHelper
-                    .boolValue(moduleIdentifier: table, forKey: "ShouldComposeAtMaximumRadicalLength") ? "Y" : "N")  + "\n"
-                report += "    - Special code prompt: " + (PreferencesHelper
-                    .boolValue(moduleIdentifier: table, forKey: "SpecialCodePrompt") ? "Y" : "N")  + "\n"
-                report += "    - Use space as first candidate selection key: \(PreferencesHelper.unsignedIntegerValue(moduleIdentifier: table, forKey: "UseSpaceAsFirstCandidateSelectionKey") ?? 0)\n"
-                report += "    - Maximum radical length: \(PreferencesHelper.unsignedIntegerValue(moduleIdentifier: table, forKey: "MaximumRadicalLength") ?? 0)\n"
-            }
-            else if table == "org.openvanilla.OVIMArray" {
-                report += "    - Special code prompt: " + (PreferencesHelper
-                    .boolValue(moduleIdentifier: table, forKey: "SpecialCodePrompt") ? "Y" : "N")  + "\n"
-                report += "    - Quick mode enaabled: " + (PreferencesHelper
-                    .boolValue(moduleIdentifier: table, forKey: "QuickMode") ? "Y" : "N")  + "\n"
+                report +=
+                    "    - Clear reading at composition error: "
+                    + (PreferencesHelper
+                        .boolValue(
+                            moduleIdentifier: table, forKey: "ClearReadingBufferAtCompositionError"
+                        )
+                        .yesNo) + "\n"
+                report +=
+                    "    - Compose while typing: "
+                    + (PreferencesHelper
+                        .boolValue(moduleIdentifier: table, forKey: "ComposeWhileTyping")
+                        .yesNo) + "\n"
+                report +=
+                    "    - Should compose at maximum radical length: "
+                    + (PreferencesHelper
+                        .boolValue(
+                            moduleIdentifier: table, forKey: "ShouldComposeAtMaximumRadicalLength")
+                        ? "Y" : "N") + "\n"
+                report +=
+                    "    - Special code prompt: "
+                    + (PreferencesHelper
+                        .boolValue(moduleIdentifier: table, forKey: "SpecialCodePrompt")
+                        ? "Y" : "N") + "\n"
+                report +=
+                    "    - Use space as first candidate selection key: \(PreferencesHelper.unsignedIntegerValue(moduleIdentifier: table, forKey: "UseSpaceAsFirstCandidateSelectionKey") ?? 0)\n"
+                report +=
+                    "    - Maximum radical length: \(PreferencesHelper.unsignedIntegerValue(moduleIdentifier: table, forKey: "MaximumRadicalLength") ?? 0)\n"
+            } else if table == "org.openvanilla.OVIMArray" {
+                report +=
+                    "    - Special code prompt: "
+                    + (PreferencesHelper
+                        .boolValue(moduleIdentifier: table, forKey: "SpecialCodePrompt")
+                        .yesNo) + "\n"
+                report +=
+                    "    - Quick mode enabled: "
+                    + (PreferencesHelper
+                        .boolValue(moduleIdentifier: table, forKey: "QuickMode")
+                        .yesNo) + "\n"
 
             }
         }
-        report += "- Shared Alphanumeric Keyboard Layout: " + OVModuleManager.default.sharedAlphanumericKeyboardLayoutIdentifier + "\n"
-        report += "- Traditional to Simpified Chinese Filter Enabled: " + (OVModuleManager.default.traditionalToSimplifiedChineseFilterEnabled ? "Y" : "N") + "\n"
-        report += "- Simpified to Traditional Chinese Filter Enabled: " + (OVModuleManager.default.simplifiedToTraditionalChineseFilterEnabled ? "Y" : "N") + "\n"
-        report += "- Associaed Phrase Around Filter Enabled: " + (OVModuleManager.default.associatedPhrasesAroundFilterEnabled ? "Y" : "N") + "\n"
+        report +=
+            "- Shared Alphanumeric Keyboard Layout: "
+            + OVModuleManager.default.sharedAlphanumericKeyboardLayoutIdentifier + "\n"
+        report +=
+            "- Traditional to Simpified Chinese Filter Enabled: "
+            + (OVModuleManager.default.traditionalToSimplifiedChineseFilterEnabled.yesNo)
+            + "\n"
+        report +=
+            "- Simplified to Traditional Chinese Filter Enabled: "
+            + (OVModuleManager.default.simplifiedToTraditionalChineseFilterEnabled.yesNo)
+            + "\n"
+        report +=
+            "- Associated Phrase Around Filter Enabled: "
+            + (OVModuleManager.default.associatedPhrasesAroundFilterEnabled.yesNo) + "\n"
 
         let userDefaults = UserDefaults.standard
-        report += "- Candidate List Text Size: \(userDefaults.integer(forKey: OVCandidateListTextSizeKey))\n"
-        report += "- Candidate List Style Name: \(userDefaults.string(forKey: OVCandidateListStyleNameKey))\n"
-        report += "- Failback to Alphanumeric Keyboard Layout on Shift: \(userDefaults.bool(forKey: OVFallbackToAlphanumericKeyboardLayoutOnShiftKey) ? "Y" : "N")" + "\n"
-        report += "- Make Sound Feedback on Input Error: \(userDefaults.bool(forKey: OVMakeSoundFeedbackOnInputErrorKey) ? "Y" : "N")" + "\n"
+        report +=
+            "- Candidate List Text Size: \(userDefaults.integer(forKey: OVCandidateListTextSizeKey))\n"
+        report +=
+            "- Candidate List Style Name: \(userDefaults.string(forKey: OVCandidateListStyleNameKey) ?? "Not Set")\n"
+        report +=
+            "- Fallback to Alphanumeric Keyboard Layout on Shift: \(userDefaults.bool(forKey: OVFallbackToAlphanumericKeyboardLayoutOnShiftKey).yesNo)"
+            + "\n"
+        report +=
+            "- Make Sound Feedback on Input Error: \(userDefaults.bool(forKey: OVMakeSoundFeedbackOnInputErrorKey) ? "Y" : "N")"
+            + "\n"
         return report
     }
 }
 
 class ReportGenerator {
-    static func generate() async -> String  {
+    static func generate() async -> String {
         var report = ""
         report += await InfoCollector.generate()
-        report += OpenVanillaPreferenceCollector.genreate()
+        report += OpenVanillaPreferenceCollector.generate()
         return report
     }
 
